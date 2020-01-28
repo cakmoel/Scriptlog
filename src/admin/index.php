c80bd74e38baf35069bd7a22967929f0ea748d66
cakmoel <alanmoehammad@gmail.com>
2020-01-28 10:32:12 +0700

    append MIMEType Testing

--- src/admin/index.php
+++ src/admin/index.php
@@ -2,7 +2,7 @@
 /**
  * File index.php
  * 
- * @category admin\index.php
+ * @category admin\index.php file
  * @author   M.Noermoehammad 
  * @license  https://opensource.org/licenses/MIT MIT License
  * 
@@ -30,14 +30,13 @@ if (!$isUserLoggedIn) {
    header("Location: login.php");
    exit();
    
-    
 } 
 
 $user_id = isset($_COOKIE['cookie_user_id']) ? $_COOKIE['cookie_user_id'] : $_SESSION['user_id'];
 $user_email = isset($_COOKIE['cookie_user_email']) ? $_COOKIE['cookie_user_email'] : $_SESSION['user_email'];
 $user_level = isset($_COOKIE['cookie_user_level']) ? $_COOKIE['cookie_user_level'] : $_SESSION['user_level'];
 $user_login = isset($_COOKIE['cookie_user_login']) ? $_COOKIE['cookie_user_login'] : $_SESSION['user_login'];
-$user_session = isset($_COOKIE['cookie_user_session']) ? $_COOKIE['cookie_user_session'] : $_SESSION['user_session'];
+$user_session = user_info($authenticator, $user_login)['user_session'];
     
 // BreadCrumbs
 $breadCrumbs = isset($_GET['load']) ? htmlentities(sanitize_urls($_GET['load']), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : http_response_code();
@@ -51,15 +50,21 @@ $currentURL = APP_PROTOCOL . '://'. APP_HOSTNAME . dirname($_SERVER['PHP_SELF'])
 // Allowed query
 $allowedQuery = array('dashboard', 'posts', 'medialib',
                       'pages', 'topics', 'comments', 'templates', 
-                      'menu', 'menu-child', 'users', 'settings', 'plugins', 'logout');    
+                      'menu', 'menu-child', 'users', 'settings', 
+                      'plugins', 'logout', '403', '404');    
 
 // retrieve plugin actived -- for administrator
 $plugin_navigation = setplugin($user_level, 'private');
 
 include 'admin-layout.php';
+
 admin_header($stylePath, $breadCrumbs, $allowedQuery);
+
 include 'navigation.php';
+
 include 'request.php';
+
 admin_footer($currentURL, $ubench);
+
 ob_end_flush();
     
--- tests/index.php
+++ tests/index.php
@@ -1,2 +1,6 @@
 <?php
 
+//require __DIR__ . '/simpletest/autorun.php';
+
+require __DIR__ . '/../lib/main-dev.php';
+
