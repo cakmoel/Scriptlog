7c708213b4029e53eec7a02c47496f92f7250d57
cakmoel <alanmoehammad@gmail.com>
2020-01-28 10:11:12 +0700

    update false authentication on request.php

--- src/admin/request.php
+++ src/admin/request.php
@@ -4,11 +4,11 @@ $load = null;
 
 try {
 
-    if (isset($_GET['load']) && $_GET['load'] != null) {
+    if (isset($_GET['load']) || array_key_exists('load', $_GET)) {
      
         $load = htmlentities(strip_tags(strtolower(basename($_GET['load']))), ENT_QUOTES|ENT_SUBSTITUTE, 'UTF-8');
         $load = filter_var($load, FILTER_SANITIZE_URL);
-        
+
         // checking if the string contains parent directory
         if (strstr($_GET['load'], '../') !== false) {
             
@@ -16,7 +16,14 @@ try {
             throw new AppException("Directory traversal attempt!");
             
         }
-        
+
+        if (strpos($_GET['load'], '..')) {
+
+            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            throw new AppException("Directory traversal attempt!");
+
+        }
+
         // checking remote file inclusions
         if (strstr($_GET['load'], 'file://') !== false) {
             
@@ -25,16 +32,34 @@ try {
             
         }
     
+        if (strstr($_GET['load'], 'http://') !== false) {
+            
+            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            throw new AppException("Remote file inclusion attempt!");
+            
+        }
+
         if ((!is_readable(dirname(dirname(__FILE__)) .DS. APP_ADMIN .DS."{$load}.php")) 
-           || (empty($load)) || (!in_array($load, $allowedQuery, true)) ) {
+            || (empty($load)) || (!in_array($load, $allowedQuery, true)) ) {
         
             header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
             throw new AppException("404 - Page requested not found");
         
         } else {
         
-           include __DIR__ . DS . $load .'.php';
-        
+           if (false === $authenticator -> userAccessControl()) {
+
+               header($_SERVER['SERVER_PROTOCOL']." 400 Bad Request");
+               header("Status: 400 Bad Request");
+               header("Retry-After: 300");
+               die("Application not response to bad request. Please try again later.");
+
+           } else {
+               
+               include __DIR__ . DS . $load .'.php';
+
+           }
+           
         }
     
     } else {
