aafce401e41e13253faca0a5d39a25c79ced661c
cakmoel <alanmoehammad@gmail.com>
2020-02-06 15:56:42 +0700

    revamp basic session on site

--- src/lib/utility/start-session-on-site.php
+++ src/lib/utility/start-session-on-site.php
@@ -10,31 +10,28 @@ function start_session_on_site()
   $life_time = 600;
 
   $session_name = session_name();
-
+  
   if (isset($_COOKIE[$session_name])) {
 
-    $session_id = $_COOKIE[$session_name];
+   $session_id = $_COOKIE[$session_name];
 
-  } elseif (isset($_GET[$session_name])) {
+ } elseif (isset($_GET[$session_name])) {
 
-     $session_id = $_GET[$session_name];
+    $session_id = $_GET[$session_name];
 
-  } else {
+ } else {
 
-     return session_start();
+   return turn_on_session($life_time, $session_name);
+   
+ }
 
-     return setcookie($session_name,session_id(),time()+$life_time);
+ if (!preg_match('/^[a-zA-Z0-9,\-]{22,40}$/', $session_id)) {
 
-  }
+   return false;
 
-  if (!preg_match('/^[a-zA-Z0-9,\-]{22,40}$/', $session_id)) {
+} 
 
-     return false;
-
-  }
-
-  return session_start();
-
-  return setcookie($session_name, session_id(), time()+$life_time);
+return turn_on_session($life_time, $session_name);
 
 }
+
