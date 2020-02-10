7113360d4a7011f9210e0e25db0b95fda40d9ca4
cakmoel <alanmoehammad@gmail.com>
2020-02-10 17:03:44 +0700

    update route-request function

--- src/lib/utility/route-request.php
+++ src/lib/utility/route-request.php
@@ -8,11 +8,41 @@
  * @return mixed
  * 
  */
-function route_request()
+function route_request(array $config)
 {
   
   $dispatcher = new Dispatcher();
   
-  return $dispatcher -> dispatch();
+  $connection = db_connect($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['name']);
+
+  if ((check_table($connection, 'tbl_users') == true) || (check_table($connection, 'tbl_user_token') == true)
+  || (check_table($connection, 'tbl_topics') == true) || (check_table($connection, 'tbl_themes') == true)
+  || (check_table($connection, 'tbl_settings') == true) || (check_table($connection, 'tbl_posts') == true)
+  || (check_table($connection, 'tbl_post_topic') == true) || (check_table($connection, 'tbl_plugin') == true)
+  || (check_table($connection, 'tbl_menu_child') == true) || (check_table($connection, 'tbl_menu') == true)
+  || (check_table($connection, 'tbl_mediameta') == true) || (check_table($connection, 'tbl_media') == true)
+  || (check_table($connection, 'tbl_comments') == true) || (check_table($connection, 'tbl_comment_reply') == true)) {
+
+    if ((is_dir(__DIR__ . '/../../install/')) && (file_exists(__DIR__ . '/../../install/install.php'))) {
+
+       header($_SERVER['SERVER_PROTOCOL']." 200 Found");
+       header("Status: 200 Found");
+       header("Location: ".APP_PROTOCOL . '://' . APP_HOSTNAME . dirname($_SERVER['PHP_SELF']) . DS . 'install/install.php');
+       exit();
+
+    } else {
+
+      header($_SERVER['SERVER_PROTOCOL']." 410 Gone");
+      header("Status: 410 Gone");
+      header("Retry-After: 300");
+      exit("The content has been permanently deleted from server");
+
+    }
+    
+  } else {
+    
+    return $dispatcher -> dispatch();
+
+  }
 
 }
