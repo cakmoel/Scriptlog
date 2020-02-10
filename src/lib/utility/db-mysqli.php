06dc0364f5739edd4d7e1071567fdae5f3cf0318
cakmoel <alanmoehammad@gmail.com>
2020-02-10 17:04:17 +0700

    update db mysql improved function

--- src/lib/utility/db-mysqli.php
+++ src/lib/utility/db-mysqli.php
@@ -13,13 +13,15 @@
 function db_connect($host, $user, $passwd, $dbname)
 {
 
-  mysqli_driver(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
   
   $database_connection = mysqli_connect($host, $user, $passwd, $dbname);
 
-  if(mysqli_connect_errno($database_connection)) {
-    scriptlog_error('Failed to connect to MySQL '.mysqli_connect_error(), E_USER_ERROR);
+  if(mysqli_connect_errno()) {
+
+    scriptlog_error('Failed to connect to MySQL '.mysqli_connect_error(), E_USER_NOTICE);
     exit();
+
   }
 
   return $database_connection;
@@ -38,7 +40,7 @@ function table_exists($connection, $table, $counter = 0)
   if ($connection) {
     $counter++;
 
-    $check = mysqli_query("SHOW TABLES LIKE '".$table."'");
+    $check = mysqli_query($connection, "SHOW TABLES LIKE '".$table."'");
 
     if($check !== false) {
 
@@ -59,12 +61,12 @@ function table_exists($connection, $table, $counter = 0)
 }
 
 // check whether database table does exist 
-function check_table($connection, $dbname)
+function check_table($connection, $table)
 {
   
   $install = false;
 
-  if(!table_exists($link, $table)) {
+  if(!table_exists($connection, $table)) {
       
     $install = true;
 
@@ -78,7 +80,6 @@ function check_table($connection, $dbname)
 
 }
 
-// 
 function db_query($connection, $query_type, $table_name, $SQL)
 {
   if (mysqli_query($connection, $SQL)) {
@@ -88,3 +89,5 @@ function db_query($connection, $query_type, $table_name, $SQL)
   }
 
 }
+
+
