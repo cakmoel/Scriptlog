52f72459e08ae5998b7966666d39397774496a62
cakmoel <alanmoehammad@gmail.com>
2020-02-21 15:54:32 +0700

    update file authorizer.php on admin directory

--- src/admin/authorizer.php
+++ src/admin/authorizer.php
@@ -6,55 +6,50 @@
  * @category checking whether cookies or session exists or not
  * @author   Vincy vincy@gmail.com
  * @see     https://phppot.com/php/secure-remember-me-for-login-using-php-session-and-cookies/
+ * @see     https://stackoverflow.com/questions/1846202/php-how-to-generate-a-random-unique-alphanumeric-string
  * 
  */
-$current_date = date("Y-m-d H:i:s", time()); 
 
-$cookie_expired_time = time() + (30 * 24 * 60 * 60); // Expired cookies for 1 month
+$current_date = date("Y-m-d H:i:s", time()); 
 
 $isUserLoggedIn = false;
 
+// Check if loggedin session and redirect if session exists
 if (!empty($_SESSION['user_id'])) {
 
     $isUserLoggedIn = true;
 
-} elseif (!empty($_COOKIE['cookie_user_email']) && !empty($_COOKIE['random_pwd']) 
-          && !empty($_COOKIE['random_selector'])) {
+} elseif ((!empty($_COOKIE['cookie_user_login'])) && (!empty($_COOKIE['cookie_user_email'])) 
+         && (!empty($_COOKIE['random_pwd'])) && (!empty($_COOKIE['random_selector']))) {  // Check if loggedin session exists
 
     $isPasswordVerified = false;
     $isSelectorVerified = false;
     $isExpiredDateVerified = false;
-    $isAgentVerified = false;
-
+    
     // retrieve user token info
-    $token_info = $authenticator -> findTokenByUserEmail($_COOKIE['cookie_user_email'], 0);
+    $token_info = $authenticator -> findTokenByLogin($_COOKIE['cookie_user_login'], 0);
 
+     // Validate random password cookie with database
     if (password_verify($_COOKIE['random_pwd'], $token_info['pwd_hash'])) {
         $isPasswordVerified = true;
     }
 
+    // Validate random selector cookie with database
     if (password_verify($_COOKIE['random_selector'], $token_info['selector_hash'])) {
         $isSelectorVerified = true;
     }
 
+    // check cookie expiration by date
     if ($token_info['expired_date'] >= $current_date) {
         $isExpiredDateVerified = true;
     }
 
-    if ((isset($_SESSION['agent'])) 
-       || ($_SESSION['agent'] == sha1($_SERVER['HTTP_ACCEPT_CHARSET'].
-                                      $_SERVER['HTTP_ACCEPT_ENCODING'].
-                                      $_SERVER['HTTP_ACCEPT_LANGUAGE'].
-                                      $_SERVER['HTTP_USER_AGENT']))) {
-	
-        $isAgentVerified = true;
-         
-    }
+    /** 
+     * Redirect if all cookie based validation retuens true
+     * Else, mark the token as expired and clear cookies
+     */
 
-    if (!empty($token_info['ID']) && $isPasswordVerified 
-        && $isSelectorVerified 
-        && $isExpiredDateVerified
-        && $isAgentVerified) {
+    if ((!empty($token_info['ID'])) && $isPasswordVerified && $isSelectorVerified && $isExpiredDateVerified ) {
 
         $isUserLoggedIn = true;
 
@@ -66,7 +61,7 @@ if (!empty($_SESSION['user_id'])) {
              
          }
 
-         $authenticator -> removeCookies();
+         $authenticator -> clearAuthCookies();
          
     } 
 
