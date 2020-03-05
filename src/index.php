85abec6e7661ae9298f1b4498a700cca703bc999
cakmoel <alanmoehammad@gmail.com>
2020-03-06 01:51:25 +0700

    update front-controller - index.php

--- src/index.php
+++ src/index.php
@@ -3,14 +3,13 @@
  * index.php file
  * 
  * @category index.php file designed as front controller
- * @package  SCRIPTLOG
  * @author   M.Noermoehammad
  * @license  https://opensource.org/licenses/MIT MIT License
  * @version  1.0
  * 
  */
-require __DIR__ . '/lib/main-dev.php'; 
+require __DIR__ . '/lib/main.php'; 
 
-route_request($config);
+route_request();
 
 
