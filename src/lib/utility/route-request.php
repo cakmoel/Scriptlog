ea02eab6ae047a3544d716acef7198749a3e1854
cakmoel <alanmoehammad@gmail.com>
2020-03-11 07:29:31 +0700

    update route request function

--- src/lib/utility/route-request.php
+++ src/lib/utility/route-request.php
@@ -13,35 +13,28 @@ function route_request()
   
   $dispatcher = new Dispatcher();
   
-  if ((check_table('tbl_users') == false) || (check_table('tbl_user_token') == false)
-      || (check_table('tbl_topics') == false) || (check_table('tbl_themes') == false)
-      || (check_table('tbl_settings') == false) || (check_table('tbl_posts') == false)
-      || (check_table('tbl_post_topic') == false) || (check_table('tbl_plugin') == false)
-      || (check_table('tbl_menu_child') == false) || (check_table('tbl_menu') == false)
-      || (check_table('tbl_mediameta') == false) || (check_table('tbl_media') == false)
-      || (check_table('tbl_comments') == false) || (check_table('tbl_comment_reply') == false)
-      || (check_table('tbl_login_attempt') == false)) {
+if (check_table()) {
 
-      return $dispatcher -> dispatch();
-    
-  } else {
-    
-    if ((is_dir(__DIR__ . '/../../install/')) && (file_exists(__DIR__ . '/../../install/install.php'))) {
+     return $dispatcher -> dispatch();
 
-      header($_SERVER['SERVER_PROTOCOL']." 200 Found");
-      header("Status: 200 Found");
-      header("Location: ".APP_PROTOCOL . '://' . APP_HOSTNAME . dirname($_SERVER['PHP_SELF']) . DS . 'install/install.php');
-      exit();
+} else {
 
-    } else {
+   if ((is_dir(__DIR__ . '/../../install/')) && (file_exists(__DIR__ . '/../../install/install.php'))) {
 
-     header($_SERVER['SERVER_PROTOCOL']." 410 Gone");
-     header("Status: 410 Gone");
-     header("Retry-After: 300");
-     exit("The content has been permanently deleted from server");
-   
-    }
+       header($_SERVER['SERVER_PROTOCOL']." 200 Found");
+       header("Status: 200 Found");
+       header("Location: ".APP_PROTOCOL . '://' . APP_HOSTNAME . dirname($_SERVER['PHP_SELF']) . DS . 'install/install.php');
+       exit();
 
-  }
+   } else {
+
+       header($_SERVER['SERVER_PROTOCOL']." 410 Gone");
+       header("Status: 410 Gone");
+       header("Retry-After: 300");
+       exit("The content has been permanently deleted from server");
+
+   }
+
+}
 
 }
