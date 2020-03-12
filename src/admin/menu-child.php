d63b3882e4e846d0c8c986c2490aea5fcb011d9b
cakmoel <alanmoehammad@gmail.com>
2020-03-12 08:04:44 +0700

    update submenu

--- src/admin/menu-child.php
+++ src/admin/menu-child.php
@@ -1,46 +1,112 @@
 <?php if (!defined('SCRIPTLOG')) die("Direct Access Not Allowed!");
 
 $action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action'])) : "";
-$subMenuId = isset($_GET['subMenuId']) ? abs((int)$_GET['subMenuId']) : 0;
+$subMenuId = isset($_GET['Id']) ? intval($_GET['Id']) : 0;
 $menuChildDao = new MenuChildDao();
-$validator = new FormValidator();
 $menuChildEvent = new MenuChildEvent($menuChildDao, $validator, $sanitizer);
 $menuChildApp = new MenuChildApp($menuChildEvent);
     
-    switch ($action) {
+switch ($action) {
     
-        case ActionConst::NEWSUBMENU:
+    case ActionConst::NEWSUBMENU:
             # Add New Menu
+        if (false === $authenticator->userAccessControl(ActionConst::NAVIGATION)) {
+
+            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+
+        } else {
+
+            if ((!check_integer($subMenuId)) && (gettype($subMenuId, "integer"))) {
+
+                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                throw new AppException("Invalid ID data type!");
+
+            }
+
             if ($subMenuId == 0) {
     
                 $menuChildApp -> insert();
-    
-            }
-    
-            break;
         
-        case ActionConst::EDITSUBMENU:
+            } else {
+
+                direct_page('index.php?load=dashboard', 302);
+
+            }
+
+        }
     
+        break;
+        
+    case ActionConst::EDITSUBMENU:
+    
+        if ((!check_integer($subMenuId)) && (gettype($subMenuId) !== "integer")) {
+
+            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            throw new AppException("Invalid ID data type!");
+
+        }
+
+        if (false === $authenticator->userAccessControl(ActionConst::NAVIGATION)) {
+
+            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+
+        } else {
+
             if ($menuChildDao -> checkMenuChildId($subMenuId, $sanitizer)) {
     
-                $menuChildApp -> update($subMenuId);
-    
+                $menuChildApp -> update(settype($subMenuId, "integer"));
+        
             } else {
-    
-                direct_page('index.php?load=menu&error=menuNotFound', 404);
-    
+        
+                direct_page('index.php?load=menu-child&error=submenuNotFound', 404);
+        
             }
+
+        }
+        
+        break;
     
-            break;
+    case ActionConst::DELETESUBMENU:
     
-        case ActionConst::DELETESUBMENU:
-    
-            $menuChildApp -> remove($menuId);
-    
-        default:
+        if ((!check_integer($subMenuId)) && (gettype($subMenuId) !== "integer")) {
+
+            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            throw new AppException("Invalid ID data type!");
+
+        } 
+
+        if (false === $authenticator->userAccessControl(ActionConst::NAVIGATION)) {
+
+            direct_page('index.php?load=403&forbideen='.forbidden_id(), 403);
+
+        } else {
+
+            if ($menuChildDao->checkMenuChildId($subMenuId, $sanitizer)) {
+
+                $menuChildApp -> remove(settype($subMenuIdId, "integer"));
+                
+            } else {
+
+                direct_page('index.php?load=menu-child&error=submenuNotFound', 404);
+
+            }
             
+        }
+        
+        break;
+
+    default:
+            
+        if (false === $authenticator->userAccessControl(ActionConst::NAVIGATION)) {
+
+            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+
+        } else {
+
             $menuChildApp -> listItems();
-    
-            break;
+
+        }
             
-    }
+        break;
+            
+}
