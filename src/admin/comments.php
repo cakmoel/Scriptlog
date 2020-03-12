04d98668615cf0e8a7bf641832e9a92f6ac7d0e9
cakmoel <alanmoehammad@gmail.com>
2020-03-12 07:58:53 +0700

    update comment

--- src/admin/comments.php
+++ src/admin/comments.php
@@ -1,37 +1,75 @@
 <?php if (!defined('SCRIPTLOG')) die("Direct Access Not Allowed");
 
 $action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action'])) : "";  
-$commentId = isset($_GET['Id']) ? abs((int)$_GET['Id']) : 0;
+$commentId = isset($_GET['Id']) ? intval($_GET['Id']) : 0;
 $commentDao = new CommentDao();
-$validator = new FormValidator();
 $commentEvent = new CommentEvent($commentDao, $validator, $sanitizer);
 $commentApp = new CommentApp($commentEvent);
     
     switch ($action) {
         
         case ActionConst::EDITCOMMENT:
-        
-            if ($commentDao -> checkCommentId($commentId, $sanitizer)) {
-                
-                $commentApp -> update($commentId);
-                
+            
+            if (false === $authenticator->userAccessControl(Action)) {
+
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+
             } else {
+
+                if ($commentDao -> checkCommentId($commentId, $sanitizer)) {
                 
-                direct_page('index.php?load=comments&error=commentNotFound', 404);
+                    $commentApp -> update(settype($commentId, "integer"));
+                    
+                } else {
+                    
+                    direct_page('index.php?load=comments&error=commentNotFound', 404);
+                    
+                }
                 
             }
-            
+        
             break;
      
         case ActionConst::DELETECOMMENT:
             
-            $commentApp -> remove($commentId);
+            if (false === $authenticator->userAccessControl(ActionConst::COMMENTS)) {
+
+                direct_page('index.php?load=403&');
+                
+            } else {
+
+                if ((!check_integer($commentId)) && (gettype($commentId) !== "integer")) {
+
+                    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                    throw new AppException("Invalid ID data type!");
+
+                }
+
+                if ($commentDao->checkCommentId($commentId, $sanitizer)) {
+
+                    $commentApp -> remove(settype($commentId, "integer"));
+
+                } else {
+
+                    direct_page('index.php?load=comments&error=commentNotFound', 404);
+
+                }
+
+            }
             
             break;
             
         default:
             
-            $commentApp -> listItems();
+            if (false === $authenticator->userAccessControl(ActionConst::COMMENTS)) {
+
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+
+            } else {
+
+                $commentApp -> listItems();
+
+            }
             
         break;
         
