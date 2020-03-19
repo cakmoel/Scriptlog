ae833165c0a639066488b98cba925da5e0df93b3
cakmoel <alanmoehammad@gmail.com>
2020-03-20 06:55:29 +0700

    update regenerate function

--- src/lib/utility/regenerate-session.php
+++ src/lib/utility/regenerate-session.php
@@ -4,7 +4,6 @@
  * regenerate new session ID
  * Session ID must be regenerated when
  * User logged in
- * User logged out
  * Certain period has passed
  * 
  * @return void
@@ -31,4 +30,76 @@ function regenerate_session()
 
  session_regenerate_id();
 
+}
+
+function get_session_data($session_name = 'PHPSESSID', $session_save_handler = 'files')
+{
+
+ $session_data = array();
+
+ if (array_key_exists($session_name, $_COOKIE)) {
+
+    $session_id = $_COOKIE[$session_name];
+
+    $old_session_id = session_id();
+
+    session_write_close();
+
+    $old_session_save_handler = ini_get('session.save_handler');
+
+    ini_set('session.save_handler', $session_save_handler);
+
+    $old_session_name = session_name($session_name);
+
+    session_id($session_id);
+
+    session_start();
+
+    $session_data = $_SESSION;
+
+    session_write_close();
+
+    ini_set('session.save_handler', $old_session_save_handler);
+
+    session_name($old_session_name);
+
+    session_id($old_session_id);
+
+    session_start();
+
+ }
+
+ return $session_data;
+
+}
+
+function clear_duplicate_cookies()
+{
+
+ if (headers_sent()) {
+
+    return;
+
+ }
+
+ $cookies = array();
+
+ foreach (headers_list() as $header) {
+
+    if (strpos($header, 'Set-Cookie:') === 0) {
+
+        $cookies[] = $header;
+
+    }
+
+ }
+
+ header_remove('Set-Cookie');
+
+ foreach (array_unique($cookies) as $cookie) {
+
+    header($cookie, false);
+
+ }
+ 
 }
