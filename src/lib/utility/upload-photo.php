f3b02643eb3024c5e903239e4923bf3f2a4191fa
cakmoel <alanmoehammad@gmail.com>
2020-04-23 06:14:54 +0700

    revamping function to handle image upload

--- src/lib/utility/upload-photo.php
+++ src/lib/utility/upload-photo.php
@@ -1,96 +1,149 @@
 <?php
 /**
- * Upload photo
+ * Intervention Image
  * 
- * @category Function upload_photo
- * @param string $file_name
- * @param integer $width
- * @param integer $height
- * @param string $mode
+ * @category Package
+ * @author  Oliver Vogel the author of Intervention Image 
+ * @see http://image.intervention.io/
+ * @see https://anchetawern.github.io/blog/2016/02/18/using-the-intervention-image-library-in-php/
+ * @see https://www.tutmecode.com/php/create-thumbnail-from-big-size-image-in-php-or-laravel/
+ * @license MIT
  * 
  */
-function upload_photo($file_location, $file_size, $file_type, $width, $height, $mode, $file_name)
+
+use Intervention\Image\ImageManager;
+use Intervention\Image\ImageManagerStatic as Image;
+
+/**
+ * Photo instance function
+ * // create an image manager instance with favored driver
+ * @return object
+ * 
+ */
+function photo_instance()
 {
-    
-    // picture directory
-    $image_path = __DIR__ . '/../../public/files/pictures/';
-    $image_path_thumb = __DIR__ . '/../../public/files/pictures/thumbs/';
-    $image_uploaded = $image_path.$file_name;
-    
-    // save picture from resources
-    
-    if ($file_size > APP_FILE_SIZE) {
-        
-        move_uploaded_file($file_location, $image_uploaded);
-        
-        // resize picture
-        $resizeImage = new Resize($image_uploaded);
-        $resizeImage ->resizeImage($width, $height, $mode);
-        $resizeImage ->saveImage($image_uploaded, 100);
-               
-    } else {
-        
-        move_uploaded_file($file_location, $image_uploaded);
-        
-    }
-    
-    // checking file type
-    $img_source = null;
-    
-    if ($file_type == "image/jpeg") {
-        
-        $img_source = imagecreatefromjpeg($image_uploaded);
-        
-    } elseif ($file_type == "image/png") {
-        
-        $img_source = imagecreatefrompng($image_uploaded);
-        
-    } elseif ($file_type == "image/gif") {
-        
-        $img_source = imagecreatefromgif($image_uploaded);
-        
-    } elseif($file_type == "image/webp") {
+  // Currently you can choose between gd and imagick
+  $manager = new ImageManager(['driver' => 'gd']);
 
-        $img_source = imagecreatefromwebp($image_uploaded);
+  return $manager;
 
-    }
-    
-    $source_width = imagesx($img_source);
-    $source_height = imagesy($img_source);
-    
-    // set picture's size
-    $set_width = 320;
-    $set_height = (($set_width/$source_width) * $source_height);
-    
-    // process
-    $img_processed = imagecreatetruecolor($set_width, $set_height);
-    imagecopyresampled($img_processed, $img_source, 0, 0, 0, 0, $set_width, $set_height, $source_width, $source_height);
-    
-    // save picture's thumbnail
-    if ($file_type == "image/jpeg") {
+}
 
-        header('Content-Type: image/jpeg');
-        imagejpeg($img_processed, $image_path_thumb . "thumb_" . $file_name);
-        
-    } elseif ($file_type == "image/png") {
-        
-        header('Content-Type: image/png');
-        imagepng($img_processed, $image_path_thumb . "thumb_" . $file_name);
-        
-    } elseif ($file_type == "image/gif") {
-        
-        header('Content-Type: image/gif');
-        imagegif($img_processed, $image_path_thumb . "thumb_" . $file_name);
-        
-    } elseif ($file_type == "image/webp") {
+/**
+ * Upload photo
+ *
+ * @param string $file_location
+ * @param string $file_size
+ * @param string $file_type
+ * @param string $file_name
+ * @return void
+ * 
+ */
+function upload_photo($file_location, $file_size, $file_type, $file_name)
+{
 
-        header('Content-Type: image/webp');
-        imagewebp($img_processed, $image_path_thumb . "thumb_" . $file_name);
+ // picture directory
+ $image_path = __DIR__ . '/../../public/files/pictures/';
+ $image_path_thumb = __DIR__ . '/../../public/files/pictures/thumbs/';
+ $image_uploaded = $image_path.$file_name;
+ 
+ list($current_width, $current_height) = getimagesize($file_location);
 
-    } 
-    
-    // Delete Picture in computer's memory
-    imagedestroy($img_source);
-    imagedestroy($img_processed);
-    
+   // crop origname
+if (false === set_regular_photo($current_width, $current_height, $file_location, $image_uploaded ) ) {
+
+    scriptlog_error("Error creating medium size of picture", E_USER_WARNING);
+
+}
+
+// crop to medium size
+if( false === set_medium_photo($current_width, $current_height, $image_uploaded, $image_path_thumb, $file_name ) ) {
+
+   scriptlog_error("Error creating medium size of picture", E_USER_WARNING);
+
+}
+
+// crop to smaller size
+if( false === set_small_photo($current_width, $current_height, $image_uploaded, $image_path_thumb, $file_name ) ) {
+
+ scriptlog_error("Error creating smaller size of picture", E_USER_WARNING);
+
+}
+
+}
+
+// setting regular size of picture
+function set_regular_photo( $current_width, $current_height, $file_location, $file_path_uploaded ) {
+
+$regular_size = 770;
+
+if ($current_width <= 0 || $current_height <= 0) {
+
+  return false;
+
+}
+
+$regular_scaled = min($regular_size/$current_width, $regular_size/$current_height);
+$new_width = ceil($regular_scaled*$current_width);
+$new_height = ceil($regular_scaled*$current_height);
+
+$regular_photo = photo_instance()->make($file_location);
+$regular_photo->resize($new_width, $new_height);
+if( $regular_photo->save($file_path_uploaded, 70)) {
+
+    return true;
+}
+
+}
+
+// setting medium size of picture
+function set_medium_photo( $current_width, $current_height, $file_path_uploaded, $file_path_thumb, $file_name )
+{
+
+$medium_size = 640;
+
+if ($current_width <= 0 || $current_height <= 0) {
+
+    return false;
+
+}
+
+$medium_scaled = min($medium_size/$current_width, $medium_size/$current_height);
+$new_width = ceil($medium_scaled*$current_width);
+$new_height = ceil($medium_scaled*$current_height);
+
+$medium_photo = photo_instance()->make($file_path_uploaded);
+$medium_photo->fit($new_width, $new_height);
+if( $medium_photo->save($file_path_thumb .'medium_'.$file_name, 60 ) ) {
+
+   return true;
+
+}
+
+}
+
+// setting smaller size of picture
+function set_small_photo( $current_width, $current_height, $file_path_uploaded, $file_path_thumb, $file_name )
+{
+
+$small_size = 320;
+
+if ($current_width <= 0 || $current_height <= 0) {
+
+    return false;
+
+}
+
+$small_scaled = min($small_size/$current_width, $small_size/$current_height);
+$new_width = ceil($small_scaled*$current_width);
+$new_height = ceil($small_scaled*$current_height);
+
+$small_photo = photo_instance()->make($file_path_uploaded);
+$small_photo->fit($new_width, $new_height);
+if( $small_photo->save($file_path_thumb.'small_'.$file_name, 60 ) ) {
+
+  return true;
+
+}
+
 }
