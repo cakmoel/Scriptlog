c0bb83efcbf9daf5cbd659e4d213224ce600c089
cakmoel <alanmoehammad@gmail.com>
2020-05-28 09:43:23 +0700

    append function to check domain validation

---
+++ src/lib/utility/is-valid-domain.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * a function to validate the domain name
+ * @see https://www.w3schools.in/php-script/check-domain-name-is-valid/
+ * @param string $url
+ * @return boolean
+ * 
+ */
+function is_valid_domain($url)
+{
+
+ $validation = FALSE;
+    
+ $urlparts = parse_url(filter_var($url, FILTER_SANITIZE_URL));
+    
+  if(!isset($urlparts['host'])){
+        
+    $urlparts['host'] = $urlparts['path'];
+
+  }
+
+  if($urlparts['host']!=''){
+    
+    if (!isset($urlparts['scheme'])){
+         
+        $urlparts['scheme'] = 'http';
+    
+    }
+    
+    if(checkdnsrr($urlparts['host'], 'A') && in_array($urlparts['scheme'],array('http','https')) && ip2long($urlparts['host']) === FALSE){ 
+         
+       $urlparts['host'] = preg_replace('/^www\./', '', $urlparts['host']);
+       $url = $urlparts['scheme'].'://'.$urlparts['host']. "/";            
+         
+        if (filter_var($url, FILTER_VALIDATE_URL) !== false && @get_headers($url)) {
+             $validation = TRUE;
+        }
+
+    }
+
+  }
+
+  if(!$validation){
+
+    return false;
+
+  } else {
+
+    return true;
+
+  }
+
+}
+
+function domain_name()
+{
+  $domain = (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : "";
+
+  return $domain;
+}
