50c9af34510ddbb46f0c79c2f9fc1908cc2595c4
cakmoel <alanmoehammad@gmail.com>
2020-05-28 10:41:03 +0700

    revamp authorizer.php in admin dir

--- src/admin/authorizer.php
+++ src/admin/authorizer.php
@@ -7,61 +7,104 @@
  * @author   Vincy vincy@gmail.com
  * @see     https://phppot.com/php/secure-remember-me-for-login-using-php-session-and-cookies/
  * @see     https://stackoverflow.com/questions/1846202/php-how-to-generate-a-random-unique-alphanumeric-string
- * 
+ * @see     https://paragonie.com/blog/2015/04/secure-authentication-php-with-long-term-persistence
  */
 
 $current_date = date("Y-m-d H:i:s", time()); 
-
-$isUserLoggedIn = false;
+$loggedIn = false;
 
 // Check if loggedin session and redirect if session exists
-if (!empty($_SESSION['user_id'])) {
+if (!empty($_SESSION['scriptlog_session_id'])) {
 
-    $isUserLoggedIn = true;
+    $loggedIn = true;
 
-} elseif ((!empty($_COOKIE['cookie_user_login'])) && (!empty($_COOKIE['cookie_user_email'])) 
-         && (!empty($_COOKIE['random_pwd'])) && (!empty($_COOKIE['random_selector']))) {  // Check if loggedin session exists
+} elseif ((!empty($_COOKIE['scriptlog_cookie_login'])) && (!empty($_COOKIE['scriptlog_validator'])) && (!empty($_COOKIE['scriptlog_selector']))) {  
 
-    $isPasswordVerified = false;
-    $isSelectorVerified = false;
-    $isExpiredDateVerified = false;
+    $validator_verified = false;
+    $selector_verified = false;
+    $expired_verified = false;
     
     // retrieve user token info
-    $token_info = $authenticator -> findTokenByLogin($_COOKIE['cookie_user_login'], 0);
+    $token_info = $authenticator -> findTokenByLogin($_COOKIE['scriptlog_cookie_login'], 0);
 
-     // Validate random password cookie with database
-    if (password_verify($_COOKIE['random_pwd'], $token_info['pwd_hash'])) {
-        $isPasswordVerified = true;
+    $expected_validator = crypt($_COOKIE['scriptlog_validator'], $token_info['pwd_hash']);
+    $correct_validator = crypt($_COOKIE['scriptlog_validator'], $token_info['pwd_hash']);
+
+    $expected_selector = crypt($_COOKIE['scriptlog_selector'], $token_info['selector_hash']);
+    $correct_selector = crypt($_COOKIE['scriptlog_selector'], $token_info['selector_hash']);
+
+    if(!function_exists('hash_equals')) {
+
+         if(timing_safe_equals($expected_validator, $correct_validator) == 0) {
+
+              // validate random validator cookie with database
+              if(password_verify($_COOKIE['scriptlog_validator'], $token_info['pwd_hash'])) {
+
+                  $validator_verified = true;
+
+              }
+
+         }
+
+         if(timing_safe_equals($expected_selector, $correct_selector) == 0) {
+
+              // Validate random selector cookie with database
+              if(password_verify($_COOKIE['scriptlog_selector'], $token_info['selector_hash'])) {
+
+                  $selector_verified = true;
+
+              }
+
+         }
+
+    } else {
+
+         if(hash_equals($expected_validator, $correct_validator)) {
+
+              if(password_verify($_COOKIE['scriptlog_validator'], $token_info['pwd_hash'])) {
+
+                 $validator_verified = true;
+
+              }
+
+         }
+
+         if(hash_equals($expected_selector, $correct_selector)) {
+
+             if(password_verify($_COOKIE['scriptlog_selector'], $token_info['selector_hash'])) {
+
+                 $selector_verified = true;
+
+             }
+
+         }
+
     }
-
-    // Validate random selector cookie with database
-    if (password_verify($_COOKIE['random_selector'], $token_info['selector_hash'])) {
-        $isSelectorVerified = true;
-    }
-
+    
     // check cookie expiration by date
     if ($token_info['expired_date'] >= $current_date) {
-        $isExpiredDateVerified = true;
+        $expired_verified = true;
     }
 
     /** 
-     * Redirect if all cookie based validation retuens true
+     * Redirect if all cookies based validation return true
      * Else, mark the token as expired and clear cookies
      */
 
-    if ((!empty($token_info['ID'])) && $isPasswordVerified && $isSelectorVerified && $isExpiredDateVerified ) {
+    if ((!empty($token_info['ID'])) && $validator_verified && $selector_verified && $expired_verified ) {
 
-        $isUserLoggedIn = true;
+        $loggedIn = true;
 
     } else {
 
          if (!empty($token_info['ID'])) {
 
              $userToken -> updateTokenExpired($token_info['ID']);
-             
+
          }
 
-         $authenticator -> clearAuthCookies();
+         // clear cookies
+         $authenticator -> clearAuthCookies($token_info['user_login']);
          
     } 
 
