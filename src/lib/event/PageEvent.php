335fe6ebc4509498c0d849b0c4dbf69d184b366a
cakmoel <alanmoehammad@gmail.com>
2020-05-30 21:13:34 +0700

    revamp pageEvent

--- src/lib/event/PageEvent.php
+++ src/lib/event/PageEvent.php
@@ -21,6 +21,13 @@ class PageEvent
   private $pageId;
  
   /**
+   * page's image
+   * @var integer
+   * 
+   */
+  private $page_image;
+
+  /**
    * Author
    * 
    * @var string
@@ -93,6 +100,12 @@ class PageEvent
    */
   private $comment_status;
 
+  private $pageDao;
+
+  private $validator;
+
+  private $sanitizer;
+
   /**
    * Initialize or instanstiate of class propertis
    */
@@ -111,7 +124,23 @@ class PageEvent
    */
   public function setPageId($pageId)
   {
-   $this->pageId = $pageId;
+    $this->pageId = $pageId;
+  }
+
+  public function setPageAuthor($author)
+  {
+    $this->author = $author;
+  }
+
+/**
+ * set page's image
+ *
+ * @param number|interger $post_image
+ * 
+ */
+  public function setPageImage($page_image)
+  {
+    $this->page_image = $page_image;
   }
 
   /**
@@ -122,7 +151,7 @@ class PageEvent
    */
   public function setPageTitle($title)
   {
-    $this->title = $title;
+    $this->title = prevent_injection($title);
   }
 
   /**
@@ -144,7 +173,7 @@ class PageEvent
  */
   public function setPageContent($content)
   {
-    $this->content = prevent_injection($content);
+    $this->content = purify_dirty_html($content);
   }
 
   /**
@@ -155,7 +184,7 @@ class PageEvent
    */
   public function setMetaDesc($meta_desc)
   {
-    $this->meta_desc = $meta_desc;
+    $this->meta_desc = prevent_injection($meta_desc);
   }
 
   /**
@@ -166,7 +195,7 @@ class PageEvent
    */
   public function setMetaKeys($meta_keys)
   {
-    $this->meta_key = $meta_keys;
+    $this->meta_key = prevent_injection($meta_keys);
   }
   
   /**
@@ -180,6 +209,13 @@ class PageEvent
     $this->page_status = $page_status;
   }
   
+  /**
+   * Set post type
+   *
+   * @param string $post_type
+   * @return void
+   * 
+   */
   public function setPostType($post_type)
   {
     $this->post_type = $post_type;    
@@ -202,36 +238,20 @@ class PageEvent
   
   public function addPage()
   {
-    $upload_path = __DIR__ . '/../../public/files/pictures/';
-    $image_uploader =  new ImageUploader('image', $upload_path);
      
     $this->validator->sanitize($this->author, 'int');
     $this->validator->sanitize($this->title, 'string');
-    $this->validator->sanitize($this->meta_desc, 'string');
-    $this->validator->sanitize($this->meta_key, 'string');
+    $this->validator->sanitize($this->post_image, 'int');
+   
+    if ((!empty($this->meta_desc)) || (!empty($this->meta_key))) {
+      $this->validator->sanitize($this->meta_desc, 'string');
+      $this->validator->sanitize($this->meta_key, 'string');
+    }
     
-    if ($image_uploader -> isImageUploaded()) {
-       
-        return $this->pageDao->createPage([
-            'post_author' => $this->author,
-            'post_date' => date("Y-m-d H:i:s"),
-            'post_title' => $this->title,
-            'post_slug' => $this->slug,
-            'post_content' => $this->content,
-            'post_summary' => $this->meta_desc,
-            'post_keyword' => $this->meta_key,
-            'post_status' => $this->page_status,
-            'post_type' => $this->post_type,
-            'comment_status' => $this->comment_status
-        ]);
-        
-    } else {
-     
-       $newFileName = $image_uploader -> renameImage();
-       $uploadImagePost = $image_uploader -> uploadImage('post', $newFileName, 770, 400, 'crop');
-       
-       return $this->pageDao->createPage([
-           'post_image' => $newFileName,
+    if (!empty($this->post_image)) {
+
+      return $this->pageDao->createPage([
+           'media_id' => $this->post_image,
            'post_author' => $this->author,
            'post_date' => date("Y-m-d H:i:s"),
            'post_title' => $this->title,
@@ -242,24 +262,40 @@ class PageEvent
            'post_status' => $this->page_status,
            'post_type' => $this->post_type,
            'comment_status' => $this->comment_status
-       ]);
+      ]);
         
+    } else {
+
+      return $this->pageDao->createPage([
+          'post_author' => $this->author,
+          'post_date' => date("Y-m-d H:i:s"),
+          'post_title' => $this->title,
+          'post_slug' => $this->slug,
+          'post_content' => $this->content,
+          'post_summary' => $this->meta_desc,
+          'post_keyword' => $this->meta_key,
+          'post_status' => $this->page_status,
+          'post_type' => $this->post_type,
+          'comment_status' => $this->comment_status
+      ]);
+       
     }
-    
+  
   }
   
   public function modifyPage()
   {
-    $upload_path = __DIR__ . '/../../public/files/pictures/';
-    $image_uploader =  new ImageUploader('image', $upload_path);
     
     $this->validator->sanitize($this->pageId, 'int');
     $this->validator->sanitize($this->author, 'int');
     $this->validator->sanitize($this->title, 'string');
-    $this->validator->sanitize($this->meta_desc, 'string');
-    $this->validator->sanitize($this->meta_key, 'string');
+   
+    if( ( !empty( $this->meta_desc ) )  || ( !empty( $this->meta_key ) ) ) {
+        $this->validator->sanitize($this->meta_desc, 'string');
+        $this->validator->sanitize($this->meta_key, 'string');
+    }
     
-    if ($image_uploader -> isImageUploaded()) {
+    if(empty($this->post_image)) {
         
       return $this->pageDao->updatePage($this->sanitizer, [
           'post_author' => $this->author,
@@ -269,26 +305,23 @@ class PageEvent
           'post_content' => $this->content,
           'post_summary' => $this->meta_desc,
           'post_keyword' => $this->meta_key,
-          'post_status' => $this->page_status,
           'post_type' => $this->post_type,
+          'post_status' => $this->page_status,
           'comment_status' => $this->comment_status
       ], $this->pageId);
       
     } else {
         
-       $newFileName = $image_uploader -> renameImage();
-       $uploadImagePost = $image_uploader -> uploadImage('post', $newFileName, 770, 400, 'crop');
-       
        return $this->pageDao->updatePage($this->sanitizer, [
-           'post_image' => $newFileName,
+           'post_image' => $this->post_image,
            'date_modified' => date("Y-m-d H:i:s"),
            'post_title' => $this->title,
            'post_slug' => $this->slug,
            'post_content' => $this->content,
            'post_summary' => $this->meta_desc,
            'post_keyword' => $this->meta_key,
-           'post_status' => $this->page_status,
            'post_type' => $this->post_type,
+           'post_status' => $this->page_status,
            'comment_status' => $this->comment_status
        ], $this->pageId);
        
@@ -298,18 +331,25 @@ class PageEvent
   
   public function removePage()
   {
+    
     $this->validator->sanitize($this->pageId, 'int');
      
     if (!$data_page = $this->pageDao->findPageById($this->pageId, $this->post_type, $this->sanitizer)) {
         direct_page('index.php?load=pages&error=pageNotFound', 404);
     }
     
-    $page_image = $data_page['post_image'];
+    $media_id = $data_page['media_id'];
+
+    $medialib = new MediaDao();
+    $media_data = $medialib->findMediaById((int)$media_id, $this->sanitizer);
+    $page_image = basename($media_data['media_filename']);
+
     if ($page_image !== '') {
         
-        if (is_readable(__DIR__ . '/../public/files/pictures/'.$page_image)) {
-            unlink(__DIR__ . '/../public/files/pictures'.$page_image);
-            unlink(__DIR__ . '/../public/files/pictures/thumbs/thumbs_'.$page_image);
+        if (is_readable(__DIR__ . '/../../public/files/pictures/'.$page_image)) {
+            unlink(__DIR__ . '/../../public/files/pictures/'.$page_image);
+            unlink(__DIR__ . '/../../public/files/pictures/thumbs/medium_'.$page_image);
+            unlink(__DIR__ . '/../../public/files/pictures/thumbs/small_'.$page_image);
         }
         
         return $this->pageDao->deletePage($this->pageId, $this->sanitizer, $this->post_type);
@@ -330,6 +370,23 @@ class PageEvent
   public function commentStatusDropDown($selected = "")
   {
     return $this->pageDao->dropDownCommentStatus($selected);
+  }
+  
+  public function pageAuthorId()
+  {
+
+    if(isset($_COOKIE['scriptlog_cookie_id'])) {
+
+      return $_COOKIE['scriptlog_cookie_id'];
+
+    }
+
+    if(isset(Session::getInstance()->scriptlog_session_id)) {
+
+       return Session::getInstance()->scriptlog_session_id;
+
+    }
+
   }
   
   public function totalPages($data = null)
