df281339066bb81e8b96670adbbfb69a354a29ff
cakmoel <alanmoehammad@gmail.com>
2020-05-30 21:34:31 +0700

    update edit-page.php

--- src/admin/ui/pages/edit-page.php
+++ src/admin/ui/pages/edit-page.php
@@ -51,84 +51,45 @@ echo "Error saving data. Please try again." . $saveError;
 endif;
 ?>
 
-<form method="post" action="index.php?load=pages&action=<?=(isset($formAction)) ? $formAction : null; ?>&pageId=<?=(isset($formData['ID'])) ? $formData['ID'] : 0; ?>" 
-role="form" enctype="multipart/form-data" autocomplete="off">
-<input type="hidden" name="page_id" value="<?=(isset($pageData['ID'])) ? $pageData['ID'] : 0; ?>" />
-<input type="hidden" name="MAX_FILE_SIZE" value="697856" />
+<?php 
+$action = isset($formAction) ? $formAction : nul;
+$page_id = isset($pageData['ID']) ? (int)$pageData['ID'] : 0;
+?>
+<form method="post" action="<?=generate_request('index.php', 'post', ['posts', $action, $page_id])['link']; ?>" role="form" enctype="multipart/form-data" autocomplete="off">
+<input type="hidden" name="page_id" value="<?= $page_id; ?>" />
+<input type="hidden" name="MAX_FILE_SIZE" value="<?=APP_FILE_SIZE; ?>" >
 
 <div class="box-body">
 <div class="form-group">
 <label>Title (required)</label>
 <input type="text" class="form-control" name="post_title" placeholder="Enter title here" value="
-<?=(isset($pageData)) ? htmlspecialchars($pageData['post_title']) : ""; ?>
-<?=(isset($formData['post_title'])) ? htmlspecialchars($formData['post_title'], ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8") : ""; ?>" required>
+<?=(isset($pageData)) ? safe_html($pageData['post_title']) : ""; ?>
+<?=(isset($formData['post_title'])) ? safe_html($formData['post_title']) : ""; ?>" maxlength="200" required>
 </div>
 
-<?php 
-if (isset($pageData['post_image'])) :
-?>
-<div class="form-group">
-<?php 
-$image = '../public/files/pictures/'.$pageData['post_image'];
-$imageThumb = '../public/files/pictures/thumbs/thumb_'.$pageData['post_image'];
-
-if (!is_readable($imageThumb)) :
-    $imageThumb = '../public/files/pictures/thumbs/nophoto.jpg';
-endif;
-
-if (is_readable($image)) :
-?>
-<br><a href="<?php echo $image; ?>"><img src="<?php  echo $imageThumb; ?>" class="img-responsive pad"></a><br> 
-<label>change picture :</label> 
-<input type="file" name="image" id="file" accept="image/*" onchange="loadFile(event)" maxlength="512" />
-<img id="output" class="img-responsive pad" />
-<p class="help-block">Maximum file size: <?= format_size_unit(697856); ?></p>
-<?php 
-else :
-?>
-<br><img src="<?php echo $imageThumb; ?>" class="img-responsive pad"><br> 
-<label>change picture :</label> 
-<input type="file" name="image" id="file" accept="image/*" onchange="loadFile(event)"  maxlength="512" />
-<img id="output" class="img-responsive pad" />
-<p class="help-block">Maximum file size: <?= format_size_unit(697856); ?></p>
-<?php 
-endif;
-?>
-</div>
-<?php else : ?>
-<div class="form-group">
-<label>Upload Picture :</label> 
-<input type="file" name="image" id="file" accept="image/*" onchange="loadFile(event)"  maxlength="512" />
-<img id="output" class="img-responsive pad" />
-<p class="help-block">Maximum file size: <?= format_size_unit(697856); ?></p>
-</div>
-<?php 
-endif;
-?>
+<?=(isset($medialibs)) ? $medialibs : "Media Not Found"; ?>
 
 <div class="form-group">
 <label>Meta Description</label>
-<textarea class="form-control" name="post_summary" rows="3" maxlength="500" >
-<?=(isset($pageData['post_summary'])) ? $pageData['post_summary'] : ""; ?>
-<?=(isset($formData['post_summary'])) ? htmlspecialchars($formData['post_summary'], ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8") : ""; ?>
+<textarea class="form-control" name="post_summary" rows="3" maxlength="320" >
+<?=(isset($pageData['post_summary'])) ? safe_html($pageData['post_summary']) : ""; ?>
+<?=(isset($formData['post_summary'])) ? safe_html($formData['post_summary']) : ""; ?>
 </textarea>
 </div>
 
 <div class="form-group">
 <label>Meta Keywords</label>
 <textarea class="form-control" name="post_keyword" rows="3" maxlength="200" >
-<?=(isset($pageData['post_keyword'])) ? $pageData['post_keyword'] : ""; ?>
-<?=(isset($formData['post_keyword'])) ? htmlspecialchars($formData['post_keyword'], ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8") : ""; ?>
+<?=(isset($pageData['post_keyword'])) ? safe_html($pageData['post_keyword']) : ""; ?>
+<?=(isset($formData['post_keyword'])) ? safe_html($formData['post_keyword']) : ""; ?>
 </textarea>
 </div>
 
 <div class="form-group">
 <label>Content (required)</label>
-<textarea class="textarea" placeholder="Place some text here"
-style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;" 
-name="post_content"  maxlength="10000" required>
-<?=(isset($pageData['post_content'])) ? $pageData['post_content'] : ""; ?>
-<?=(isset($formData['post_content'])) ? htmlspecialchars($formData['post_content'], ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8") : ""; ?>
+<textarea class="textarea" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;" name="post_content" maxlength="50000" required>
+<?=(isset($pageData['post_content'])) ? safe_html($pageData['post_content']) : ""; ?>
+<?=(isset($formData['post_content'])) ? safe_html($formData['post_content']) : ""; ?>
 </textarea>
 </div>
 
