73b7483f4746cdc7261b054740592ba064785f3f
cakmoel <alanmoehammad@gmail.com>
2020-06-05 09:08:20 +0700

    revamp medialib link function

--- src/lib/utility/medialib-link.php
+++ src/lib/utility/medialib-link.php
@@ -8,6 +8,9 @@
  * @return string
  * 
  */
+
+use Intervention\Image\ImageManagerStatic as Image;
+
 function medialib_link($media_type, $media_filename)
 {
  
@@ -16,25 +19,42 @@ function medialib_link($media_type, $media_filename)
       case "image/jpeg":
       case "image/gif":
       case "image/png":
+      case "image/webp":
 
-        $image_link = app_url().APP_IMAGE.'thumbs/thumb_'.rawurlencode(basename($media_filename));
+        $image_link = app_url().DS.APP_IMAGE_THUMB.'medium_'.rawurlencode($media_filename);
 
         return $image_link;
           
         break;
-
-      case 'audio/mp3':
+        
+      case 'audio/wav':
       case 'audio/ogg':
       case 'audio/mpeg':
         
-        $audio_link = app_url().APP_AUDIO.rawurlencode(basename($media_filename));
+        $audio_link = app_url().DS.APP_AUDIO.rawurlencode(basename($media_filename));
 
         return $audio_link;
 
         break;
       
+      case 'video/mp4':
+      case 'video/webm':
+      case 'video/ogg':
+        
+        $video_link = app_url().DS.APP_VIDEO.rawurlencode(basename($media_filename));
+
+        return $video_link;
+
+        break;
+      
       default:
-          # code...
-          break;
+          
+        $media_link = "#";
+
+        return $media_link;
+
+        break;
+
   }
+  
 }
