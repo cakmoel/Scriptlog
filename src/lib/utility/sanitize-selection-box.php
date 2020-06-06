1a83c9c088fe6780c0eb9ff00dc6e7083cd8ce37
cakmoel <alanmoehammad@gmail.com>
2020-06-06 21:26:56 +0700

    update function sanitize_selection_box()

--- src/lib/utility/sanitize-selection-box.php
+++ src/lib/utility/sanitize-selection-box.php
@@ -40,6 +40,36 @@ function sanitize_selection_box($values, $white_args)
  
             break;
 
+        case 'media_access':
+
+            if (empty($values['media_access']) || !in_array($values['media_access'], array_keys($white_args))) {
+
+                return false;
+
+            }
+
+            break;
+
+        case 'media_target':
+
+            if (empty($values['media_target']) || !in_array($values['media_target'], array_keys($white_args))) {
+
+                return false;
+
+            }
+
+            break;
+
+        case 'media_status':
+
+            if (empty($values['media_status']) || !in_array($values['media_status'], $white_args)) {
+
+                return false;
+
+            }
+
+            break;
+
         default:
 
             return true;
