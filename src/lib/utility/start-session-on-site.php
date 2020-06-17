2918f915bed2787d752d4b80a31bd84371866b9c
cakmoel <alanmoehammad@gmail.com>
2020-06-17 10:26:24 +0700

    revamp start_session_on_site function

--- src/lib/utility/start-session-on-site.php
+++ src/lib/utility/start-session-on-site.php
@@ -2,7 +2,9 @@
 /**
  * Function start session on site
  * 
+ * @see https://www.php.net/manual/en/ref.session.php
  * @see https://github.com/GoogleChromeLabs/samesite-examples/blob/master/php.md 
+ * @see https://stackoverflow.com/questions/36877/how-do-you-set-up-use-httponly-cookies-in-php
  * @see https://stackoverflow.com/a/46971326/2308553 
  * @category function 
  * @uses turn_on_session()
@@ -12,7 +14,7 @@
 function start_session_on_site()
 {
   
-  $life_time = time() + Authentication::COOKIE_EXPIRE;
+  $life_time = Authentication::COOKIE_EXPIRE;
 
   $current_cookie_params = session_get_cookie_params();
 
@@ -23,25 +25,25 @@ function start_session_on_site()
         'path' => '/',
         'domain' => $current_cookie_params["domain"],
         'secure' => $current_cookie_params["secure"],
-        'httponly' => $current_cookie_params["httponly"],
+        'httponly' => 1,
         'samesite' => 'Lax'
       ]);
 
-    } else { 
+  } else { 
 
       session_set_cookie_params(
         $life_time,
         '/; samesite=Lax',
         $current_cookie_params["domain"],
         $current_cookie_params["secure"],
-        $current_cookie_params["httponly"]
+        1
       );
 
-    }
+  }
 
-  $session_name = session_name('scl');
+ $session_name = session_name('scriptlog');
   
-  if (isset($_COOKIE[$session_name])) {
+ if (isset($_COOKIE[$session_name])) {
 
    $session_id = $_COOKIE[$session_name];
 
@@ -51,17 +53,31 @@ function start_session_on_site()
 
  } else {
 
-   return turn_on_session($life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], $current_cookie_params["httponly"]);
+   return turn_on_session($life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], true);
    
  }
 
- if (!preg_match('/^[a-zA-Z0-9,\-]{22,40}$/', $session_id)) {
+ if(!session_valid_id($session_id)) {
 
    return false;
 
-} 
+ }
 
-return turn_on_session($life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], $current_cookie_params["httponly"]);
+return turn_on_session($life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], true);
 
 }
+
+/**
+ * session_valid_id function
+ * 
+ * @see https://www.php.net/manual/en/function.session-id.php
+ * @param string $session_id
+ * @return void
+ * 
+ */
+function session_valid_id($session_id)
+{
+   return preg_match('/^[-,a-zA-Z0-9]{1,128}$/', $session_id) > 0;
+}
+
 
