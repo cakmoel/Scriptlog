f49113ee6d256db54606ca1c2bb68b475dc2f992
cakmoel <alanmoehammad@gmail.com>
2020-06-19 08:06:31 +0700

    update logout

--- src/admin/logout.php
+++ src/admin/logout.php
@@ -1,4 +1,21 @@
 <?php if (!defined('SCRIPTLOG')) die("Direct Access Not Allowed!");
 
-$authenticator -> logout();
+$ip_address = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : get_ip_address();
+$user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "";
+
+if(isset($_GET['load']) && $_GET['load'] == basename('logout')) {
+
+    if(Session::getInstance()->scriptlog_session_ip !== $ip_address || Session::getInstance()->scriptlog_session_agent !== $user_agent) {
+
+        session_destroy();
+        session_regenerate_id();
+        Session::getInstance()->scriptlog_session_agent = $user_agent;
+        Session::getInstance()->scriptlog_session_ip = $ip_address;
+        
+    }
+
+    $authenticator -> logout();
+    
+}
+
 
