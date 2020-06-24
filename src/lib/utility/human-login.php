0f64b6b64326d3a8187676b1a935a7678caa02dc
cakmoel <alanmoehammad@gmail.com>
2020-06-24 21:36:04 +0700

    revamp human_login function

--- src/lib/utility/human-login.php
+++ src/lib/utility/human-login.php
@@ -1,92 +1,5 @@
 <?php
 /**
- * Human login id function
- * a function to generate random numbers for loginId
- *
- * @category Function
- * @author M.Noermoehammad
- * @return void
- * 
- */
-
-function human_login_id()
-{
- 
- $random_int = ircmaxell_generator_numbers(0, 999);
- 
- $_SESSION['human_login_id'] = $random_int;
- 
- return $random_int;
-
-}
-
-/**
- * Verify human login id function
- * a function to verify human_login_id()
- * 
- * @param [type] $loginId
- * @return void
- */
-function verify_human_login_id($loginId)
-{
-// check if a session is started and a token is transmitted, if not return an error
-if(!isset($_SESSION['human_login_id'])) { 
-     
-   return false;
-   
- }
-  
- // check if the form is sent with token in it
- if(!isset($loginId)) {
-   
-    return false;
-
- }
-   
- // compare the tokens against each other if they are still the same
- if ($_SESSION['human_login_id'] !== $loginId) {
-       
-     return false;
-   
- }
-  
- return true;
-
-}
-
-/**
- * Human login request
- * generate login query string parameters
- *
- * @param string $base
- * @param array $data
- * @return mixed
- * 
- */
-function human_login_request($base, array $data)
-{
-
-$html = [];
-
-$action = (is_array($data) && array_key_exists(0, $data)) ? rawurlencode($data[0]) : '';
-$id = (is_array($data) && array_key_exists(1, $data)) ? urlencode($data[1]) : null;
-$uniqueKey =  (is_array($data) && array_key_exists(2, $data)) ? urlencode($data[2]) : null;
-
-$query_data = array(
-
-   'action' => $action,
-   'Id' => abs((int)$id),
-   'uniqueKey'=> sanitize_urls($uniqueKey),
-
-);
-
-$html['doLogin'] = build_query($base, $query_data);
-
-return $html;
-
-}
-
-/**
  * Safe human login
  *
  * @param string $ip
@@ -96,7 +9,7 @@ return $html;
  * @return void
  * 
  */
-function safe_human_login($ip, $loginId, $uniqueKey, array $values)
+function safe_human_login(array $values)
 {
 
 if( check_form_request($values, ['login', 'user_pass', 'scriptpot_name', 'scriptpot_email', 'captcha_code', 'remember', 'csrf', 'LogIn']) == false )  {
@@ -105,20 +18,6 @@ if( check_form_request($values, ['login', 'user_pass', 'scriptpot_name', 'script
      header('Status: 413 Payload Too Large');
      header('Retry-After: 3600');
      die("413 Payload Too Large");
-
-}
-
-if(false === verify_human_login_id($loginId)) {
-
-   http_response_code(400);
-   scriptlog_error("Sorry, unpleasant attempt request");
-
-}
-
-if(!isset($uniqueKey) && ($uniqueKey !== md5(app_key().$ip))) {
-
-   http_response_code(400);
-   scriptlog_error("Sorry, unpleasant attempt request");
 
 }
 
@@ -135,7 +34,7 @@ if(!isset($uniqueKey) && ($uniqueKey !== md5(app_key().$ip))) {
  * @return void
  * 
  */
-function processing_human_login($authenticator, $ip, $loginId, $uniqueKey, $errors, array $values)
+function processing_human_login($ip, $authenticator, $errors, array $values)
 {
 
 $login = (isset($values['login'])) ? prevent_injection($values['login']) : null;
@@ -147,7 +46,6 @@ $is_authenticated = false;
 $captcha = true;
 
 // validate form
-safe_human_login($ip, $loginId, $uniqueKey, $values);
 $valid = !empty($csrf) && verify_form_token('login_form', $csrf);
 
 if(!$valid) {
