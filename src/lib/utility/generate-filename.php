2f1b2a5d0eb1fb415ddbffc58ad515757160641e
cakmoel <alanmoehammad@gmail.com>
2020-07-14 01:52:10 +0700

    fix bug undefined offset 0 extension-filename on generate_filename function

--- src/lib/utility/generate-filename.php
+++ src/lib/utility/generate-filename.php
@@ -9,10 +9,14 @@
 function generate_filename($file_name)
 {
  
- $file_info = pathinfo($file_name);
- $name = $file_info['filename'];
- $file_extension = $file_info['extension'];
+ $file_info = pathinfo($file_name, PATHINFO_EXTENSION | PATHINFO_FILENAME);
+ 
+ $name = isset($file_info['filename']) ? $file_info['filename'] : null;
+    
+ $file_extension = isset($file_info['extension']) ? $file_info['extension'] : null;
+
  $tmp = str_replace(array('.',' '), array('',''), microtime());
+ 
  $new_filename = rename_file(md5($name.$tmp)).'-'.date('Ymd').'.'.$file_extension;
 
  return (array('new_filename'=>$new_filename, 'file_extension'=>$file_extension));
