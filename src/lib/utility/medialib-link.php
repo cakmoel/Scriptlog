8a98845b0e29efbf275ff24f171c4a368bd0ff4c
cakmoel <alanmoehammad@gmail.com>
2020-08-17 09:41:45 +0700

    update medialib-link function

--- src/lib/utility/medialib-link.php
+++ src/lib/utility/medialib-link.php
@@ -8,31 +8,35 @@
  * @return string
  * 
  */
-
-use Intervention\Image\ImageManagerStatic as Image;
-
 function medialib_link($media_type, $media_filename)
 {
  
+  if (!preg_match('/^(?:[a-z0-9_-]|\.(?!\.))+$/iD', $media_filename)) {
+
+     scriptlog_error("Bad filename", E_USER_WARNING);
+
+  }
+
   switch ($media_type) {
 
       case "image/jpeg":
+      case "image/jpg":  
       case "image/gif":
       case "image/png":
       case "image/webp":
 
-        $image_dir =  __DIR__ . '/../../public/files/pictures/thumbs/medium_'.$media_filename;
+        $image_dir =  __DIR__ . '/../../'.APP_IMAGE_MEDIUM.'medium_'.$media_filename;
 
         $file_basename = substr($media_filename, 0, strripos($media_filename, '.'));
 
         if(is_readable($image_dir)) {
 
-          $image_link = app_url().DS.APP_IMAGE_THUMB.'medium_'.rawurlencode($media_filename);
-
+          $image_link = app_url().DS.APP_IMAGE_MEDIUM.'medium_'.rawurlencode(basename($media_filename));
+          
         } else {
 
-          $image_link = app_url().DS.APP_IMAGE_THUMB.'medium_'.rawurlencode($file_basename.'.webp');
-
+          $image_link = app_url().DS.APP_IMAGE_MEDIUM.'medium_'.rawurlencode(basename($file_basename.'.webp'));
+          
         }
 
         return $image_link;
@@ -58,7 +62,15 @@ function medialib_link($media_type, $media_filename)
         return $video_link;
 
         break;
-      
+        
+      case "application/pdf":
+
+           $pdf_link = app_url().DS.APP_DOCUMENT.rawurlencode(basename($media_filename));
+
+           return $pdf_link;
+
+        break;
+
       default:
           
         $media_link = "#";
