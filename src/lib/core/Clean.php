3e31111924a756bc60d945eb36ae1943c256e3ed
cakmoel <alanmoehammad@gmail.com>
2020-08-20 14:24:45 +0700

    update protection against cross-site scripting(XSS) part 2

---
+++ src/lib/core/Clean.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Clean class.
+ *
+ * LICENSE: The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Detroit Studio s.r.o.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * Usage:
+ * If you want to clean simple string use cleanInput() function.
+ *  $cleanString = Clean::cleanInput($dirtyString);
+ * If you want to clean array with various values use cleanArray() function.
+ *  $cleanArray = Clean::cleanArray($dirtyArray);
+ *   
+ * @author     Ľuboš Beran
+ * @copyright  (c) 2012-2014 Detroit Studio s.r.o.
+ * @web        http://www.detroit.sk
+ * @license    MIT
+ * @version    1.2
+ */
+
+class Clean
+{
+
+  /** 
+   * Function to escape all possible XSS attacks from string.
+   *    
+   * @param string $data String to escape
+   * @param bool $addslashes If we want to use "addslashes"
+   * @return string $data Clean string
+   */
+  public static function cleanInput($data,$addslashes = false)
+  {
+    // Fix &entity\n;
+    $data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+    $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+    $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+    $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+
+    // Remove any attribute starting with "on" or xmlns
+    $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+
+    // Remove javascript: and vbscript: protocols
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+
+    // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+
+    // Remove namespaced elements (we do not need them)
+    $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+    if($addslashes){
+      $data = addslashes($data);
+    }
+    do
+    {
+      // Remove really unwanted tags
+      $old_data = $data;
+      $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+    }
+    while ($old_data !== $data);
+
+    // we are done...
+    return $data;
+  }
+
+  /**
+   * Function to clean multi-dimensional array of posted data.
+   * Usage: Clean::cleanArray($_POST); (for example)
+   *
+   * @param array $data Array to escape
+   * @param bool $addslashes Set true, if you can use addslashes function - extended security
+   * @return array
+   */
+  public static function cleanArray($data = array(),$addslashes = false)
+  {
+    $iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($data));
+    //clean array
+    foreach($iterator as $key => $value){
+      $data[$key] = self::cleanInput($value,$addslashes);
+    }
+    return $data;
+  }
+}
