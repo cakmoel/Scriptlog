d1f6fe38f52b0a0a4e21daef3f9727b9e1e72fbc
cakmoel <alanmoehammad@gmail.com>
2020-08-29 02:03:42 +0700

    update Class ThemeApp

--- src/lib/app/ThemeApp.php
+++ src/lib/app/ThemeApp.php
@@ -64,11 +64,13 @@ class ThemeApp extends BaseApp
 
     if (isset($_POST['themeFormSubmit'])) {
 
-      $theme_title = (isset($_POST['theme_title']) ? preg_replace('/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/', '', $_POST['theme_title']) : "");
-      $theme_desc = (isset($_POST['theme_description']) ? prevent_injection($_POST['theme_description']) : "");
-      $theme_designer = (isset($_POST['theme_designer']) ? preg_replace('/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/', '', $_POST['theme_designer']) : "");
-      $theme_dir = (isset($_POST['theme_directory']) ? $_POST['theme_directory'] : "");
-
+      $filters = [
+        'theme_title' => FILTER_SANITIZE_STRING, 
+        'theme_description' => FILTER_SANITIZE_FULL_SPECIAL_CHARS, 
+        'theme_designer' => FILTER_SANITIZE_STRING, 
+        'theme_directory' => FILTER_SANITIZE_STRING
+      ];
+      
       try {
         
         if (!csrf_check_token('csrfToken', $_POST, 60*10)) {
@@ -78,12 +80,12 @@ class ThemeApp extends BaseApp
           
         }
 
-        if (empty($theme_title) || empty($theme_designer) || empty($theme_dir)) {
+        if (empty($_POST['theme_title']) || empty($_POST['theme_designer']) || empty($_POST['theme_directory'])) {
           $checkError = false;
           array_push($errors, "All columns required must be filled");
         }
 
-        if ($this->themeEvent->isThemeExists($theme_title) === true) {
+        if ($this->themeEvent->isThemeExists(distill_post_request($filters)['theme_title']) === true) {
           $checkError = false;
           array_push($errors, "Sorry, you have installed this theme before.");
         }
@@ -92,7 +94,7 @@ class ThemeApp extends BaseApp
 
           $this->setView('edit-template');
           $this->setPageTitle('Add New Theme');
-          $this->setFormAction('newTheme');
+          $this->setFormAction(ActionConst::NEWTHEME);
           $this->view->set('pageTitle', $this->getPageTitle());
           $this->view->set('formAction', $this->getFormAction());
           $this->view->set('errors', $errors);
@@ -101,10 +103,10 @@ class ThemeApp extends BaseApp
 
         } else {
 
-          $this->themeEvent->setThemeTitle($theme_title);
-          $this->themeEvent->setThemeDescription($theme_desc);
-          $this->themeEvent->setThemeDesigner($theme_designer);
-          $this->themeEvent->setThemeDirectory($theme_dir);
+          $this->themeEvent->setThemeTitle(prevent_injection(distill_post_request($filters)['theme_title']));
+          $this->themeEvent->setThemeDescription(purify_dirty_html(distill_post_request($filters)['theme_description']));
+          $this->themeEvent->setThemeDesigner(prevent_injection(distill_post_request($filters)['theme_designer']));
+          $this->themeEvent->setThemeDirectory(prevent_injection(distill_post_request($filters)['theme_directory']));
           $this->themeEvent->addTheme();
           direct_page('index.php?load=templates&status=themeAdded', 200);
 
@@ -122,7 +124,7 @@ class ThemeApp extends BaseApp
        
        $this->setView('edit-template');
        $this->setPageTitle('Add New Theme');
-       $this->setFormAction('newTheme');
+       $this->setFormAction(ActionConst::NEWTHEME);
        $this->view->set('pageTitle', $this->getPageTitle());
        $this->view->set('formAction', $this->getFormAction());
        $this->view->set('csrfToken', csrf_generate_token('csrfToken'));
@@ -140,58 +142,127 @@ class ThemeApp extends BaseApp
 
     if (isset($_POST['themeFormSubmit'])) {
 
-      $file_name = (isset($_FILES['zip_file']['name']) ? $_FILES['zip_file']['name']  : "");
-      $file_size = (isset($_FILES['zip_file']['size']) ? $_FILES['zip_file']['size'] : null);
-      $file_location = (isset($_FILES['zip_file']['tmp_name']) ? $_FILES['zip_file']['tmp_name'] : "");
-      $max_filesize = $_POST['MAX_FILE_SIZE'];
-
+      $file_name = isset($_FILES['zip_file']['name']) ? $_FILES['zip_file']['name']  : null;
+      $file_size = isset($_FILES['zip_file']['size']) ? $_FILES['zip_file']['size'] : null;
+      $file_location = isset($_FILES['zip_file']['tmp_name']) ? $_FILES['zip_file']['tmp_name'] : null;
+      $file_type = isset($_FILES['zip_file']['type']) ? $_FILES['zip_file']['type'] : null;
+      $file_error = isset($_FILES['zip_file']['error']) ? $_FILES['zip_file']['error'] : null;
+      
       $theme_title = current(explode(".",$file_name));
       $extension = [];
       $split = explode(".",$file_name);
       $extension = (array_key_exists(1, $split) ? $split[1] : null);
       $validate_format = (strtolower($extension) == 'zip' ? true : false);
-      $theme_dir = "public/themes/{$theme_title}";
+      $theme_dir = $theme_title;
+      $theme_ini = null;
 
       try {
 
         if (!csrf_check_token('csrfToken', $_POST, 60*10)) {
          
           header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-          throw new AppException("Sorry, unpleasant attempt detected!");
+          $checkError = false;
+          array_push($errors, "Sorry, unpleasant attempt detected!");
           
         }
 
-        if ((empty($file_location))) {
-           
-          $checkError = false;
-          array_push($errors, "Please upload your selected file");
-
-        }
-
-        if (!$validate_format) {
+        if (!isset($file_error) || is_array($file_error)) {
 
           $checkError = false;
-          array_push($errors, "Invalid file format.Make sure you have a .zip format");
+          array_push($errors, "Invalid paramenter");
           
         }
+  
+        switch ($file_error) {
+  
+          case UPLOAD_ERR_OK:
+              break;
+          case UPLOAD_ERR_NO_FILE:
+             
+             $checkError = false;
+             array_push($errors, "No file uploaded");
+  
+             break;
+         
+          case UPLOAD_ERR_INI_SIZE:
+          case UPLOAD_ERR_FORM_SIZE:
+            
+            $checkError = false;
+            array_push($errors, "Exceeded filesize limit");
+  
+            break;
+  
+          default:
+              
+            $checkError = false;
+            array_push($errors, "Unknown errors");
+            
+            break;
+            
+        }
 
-        if (!(is_writable('../public/themes/'))) {
+        if ($file_size > scriptlog_upload_filesize()) {
+
+            $checkError = false;
+            array_push($errors, "Exceeded file size limit. Maximum file size is. ".format_size_unit(scriptlog_upload_filesize()));
+
+        }
+
+        if (false === check_file_name($file_location)) {
+
+           $checkError = false;
+           array_push($errors, "file name is not valid");
+
+        }
+
+        if (true === check_file_length($file_location)) {
+
+           $checkError = false;
+           array_push($errors, "file name is too long");
+
+        }
+
+
+        if (!(is_writable(__DIR__ . '/../../'. APP_THEME))) {
           
           $checkError = false;
           array_push($errors, "Permission denied.");
 
-        } elseif ((is_dir('../public/themes/'.$theme_title.'/')) || (is_readable('../public/themes/'.$theme_title.'/theme.ini'))) {
+        } elseif ((is_dir(__DIR__ . '/../../'.APP_THEME.$theme_title.'/')) || (is_readable(__DIR__ .'/../../'.APP_THEME.$theme_title.'/theme.ini'))) {
 
           $checkError = false;
           array_push($errors, "Sorry, you have installed this theme before.");
 
         }
 
+         // upload file
+        if (is_uploaded_file($file_location)) {
+
+          if ((!$validate_format) || false === check_mime_type(array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed'), $file_location)) {
+
+             $checkError = false;
+             array_push($errors,  "Invalid file format.Make sure you have a .zip format");
+            
+          } else {
+
+            # upload theme 
+            upload_theme(basename($file_name), $file_location, ["..", ".git", ".svn", "composer.json", "composer.lock", "framework_config.yaml", ".php", ".html", ".phtml", ".php5", ".php4", ".pl", ".py", ".sh", ".htaccess"]);
+           
+            if (file_exists(APP_ROOT.'public/themes/'.$theme_title.'/theme.ini')) {
+
+              $theme_ini = parse_ini_file(APP_ROOT.'public/themes/'.$theme_title.'/theme.ini');
+ 
+            }
+               
+          }
+           
+        }
+
         if (!$checkError) {
            
           $this->setView('install-template');
           $this->setPageTitle('Upload Theme');
-          $this->setFormAction('installTheme');
+          $this->setFormAction(ActionConst::INSTALLTHEME);
           $this->view->set('pageTitle', $this->getPageTitle());
           $this->view->set('formAction', $this->getFormAction());
           $this->view->set('errors', $errors);
@@ -199,18 +270,12 @@ class ThemeApp extends BaseApp
 
         } else {
 
-           # upload theme and populate database
-           upload_theme($file_name, $file_location, $max_filesize, ["..", ".git", ".svn", "composer.json", "composer.lock", "framework_config.yaml", ".php", ".html", ".phtml", ".php5", ".php4", ".pl", ".py", ".sh", ".htaccess"]);
-           
-           if (file_exists(APP_ROOT.'public/themes/'.$theme_title.'/theme.ini'))
-              $theme_ini = parse_ini_file(APP_ROOT.'public/themes/'.$theme_title.'/theme.ini');
-              
-              $this->themeEvent->setThemeTitle($theme_ini['theme_name']);
-              $this->themeEvent->setThemeDescription($theme_ini['theme_description']);
-              $this->themeEvent->setThemeDesigner($theme_ini['theme_designer']);
-              $this->themeEvent->setThemeDirectory($theme_dir);
-              $this->themeEvent->addTheme();
-              direct_page('index.php?load=templates&status=themeAdded', 200);
+            $this->themeEvent->setThemeTitle($theme_ini['theme_name']);
+            $this->themeEvent->setThemeDescription($theme_ini['theme_description']);
+            $this->themeEvent->setThemeDesigner($theme_ini['theme_designer']);
+            $this->themeEvent->setThemeDirectory($theme_dir);
+            $this->themeEvent->addTheme();
+            direct_page('index.php?load=templates&status=themeAdded', 200);
               
         }
 
@@ -226,7 +291,7 @@ class ThemeApp extends BaseApp
 
       $this->setView('install-template');
       $this->setPageTitle('Upload Theme');
-      $this->setFormAction('installTheme');
+      $this->setFormAction(ActionConst::INSTALLTHEME);
       $this->view->set('pageTitle', $this->getPageTitle());
       $this->view->set('formAction', $this->getFormAction());
       $this->view->set('csrfToken', csrf_generate_token('csrfToken'));
@@ -261,7 +326,6 @@ class ThemeApp extends BaseApp
       $theme_desc = (isset($_POST['theme_description']) ? prevent_injection($_POST['theme_description']) : "");
       $theme_designer = (isset($_POST['theme_designer']) ? preg_replace('/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/', '', $_POST['theme_designer']) : "");
       $theme_dir = (isset($_POST['theme_directory']) ? $_POST['theme_directory'] : "");
-      $theme_status = $_POST['theme_status'];
       $theme_id = abs((int)$_POST['theme_id']);
 
       try {
@@ -296,7 +360,6 @@ class ThemeApp extends BaseApp
           $this->themeEvent->setThemeDescription($theme_desc);
           $this->themeEvent->setThemeDesigner($theme_designer);
           $this->themeEvent->setThemeDirectory($theme_dir);
-          $this->themeEvent->setThemeStatus($theme_status);
           $this->themeEvent->modifyTheme();
           direct_page('index.php?load=templates&status=themeUpdated', 200);
 
