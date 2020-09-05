4b4c1cd48c08951464e2957f9c246c9041b32c50
cakmoel <alanmoehammad@gmail.com>
2020-09-05 07:24:32 +0700

    udpate class TopicDao

--- src/lib/dao/TopicDao.php
+++ src/lib/dao/TopicDao.php
@@ -170,6 +170,27 @@ class TopicDao extends Dao
      
  }
  
+/**
+ * showAllActiveTopics
+ * retrieve all active topics and display it on sidebar themes
+ * 
+ * @return array
+ * 
+ */
+ public function showAllActiveTopics()
+ {
+  
+  $sql = "SELECT ID, topic_title, topic_slug, topic_status FROM  tbl_topics 
+          WHERE topic_status = 'Y' ORDER BY ID ";
+
+  $this->setSQL($sql);
+
+  $active_topics = $this->findAll();
+
+  return (empty($active_topics)) ?: $active_topics;
+
+ }
+
  /**
   * Set topic
   * post category
