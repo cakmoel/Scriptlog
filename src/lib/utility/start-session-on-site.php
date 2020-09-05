0a03e61faf835fb7bda04d6fa8d04c46a8e4358e
cakmoel <alanmoehammad@gmail.com>
2020-09-05 23:26:35 +0700

    update start_session_on_site function

--- src/lib/utility/start-session-on-site.php
+++ src/lib/utility/start-session-on-site.php
@@ -2,27 +2,22 @@
 /**
  * Function start session on site
  * 
+ * @category function 
  * @see https://www.php.net/manual/en/ref.session.php
  * @see https://github.com/GoogleChromeLabs/samesite-examples/blob/master/php.md 
  * @see https://stackoverflow.com/questions/36877/how-do-you-set-up-use-httponly-cookies-in-php
  * @see https://stackoverflow.com/a/46971326/2308553 
- * @category function 
- * @uses turn_on_session()
+ * @uses turn_on_session() functions
  * @return mixed
  * 
  */
-function start_session_on_site()
+
+function start_session_on_site($session_handler)
 {
   
   $life_time = Authentication::COOKIE_EXPIRE;
 
-  if(is_dir(__DIR__ . '/.sessions')) {
-
-    session_save_path(__DIR__ . '/.sessions');
-    
-  }
-
-  $session_name = session_name('scriptlog');
+  $session_name = session_name();
   
   if(ini_get('session.use_cookies')) {
 
@@ -30,29 +25,6 @@ function start_session_on_site()
 
   }
 
-  if (PHP_VERSION_ID >= 70300) { 
-      
-      session_set_cookie_params([
-        'lifetime' => $life_time,
-        'path' => '/',
-        'domain' => $current_cookie_params["domain"],
-        'secure' => $current_cookie_params["secure"],
-        'httponly' => 1,
-        'samesite' => 'Lax'
-      ]);
-
-  } else { 
-
-      session_set_cookie_params(
-        $life_time,
-        '/; samesite=Lax',
-        $current_cookie_params["domain"],
-        $current_cookie_params["secure"],
-        1
-      );
-
-  }
- 
  if (isset($_COOKIE[$session_name])) {
 
    $session_id = $_COOKIE[$session_name];
@@ -63,7 +35,7 @@ function start_session_on_site()
 
  } else {
 
-   return turn_on_session($life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], true);
+   return turn_on_session($session_handler, $life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], true);
    
  }
 
@@ -73,7 +45,7 @@ function start_session_on_site()
 
  }
 
-return turn_on_session($life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], true);
+return turn_on_session($session_handler, $life_time, $session_name, $current_cookie_params["path"], $current_cookie_params["domain"], $current_cookie_params["secure"], true);
 
 }
 
@@ -87,7 +59,7 @@ return turn_on_session($life_time, $session_name, $current_cookie_params["path"]
  */
 function session_valid_id($session_id)
 {
-   return preg_match('/^[-,a-zA-Z0-9]{1,128}$/', $session_id) > 0;
+  return preg_match('/^[-,a-zA-Z0-9]{1,128}$/', $session_id) > 0;
 }
 
 
