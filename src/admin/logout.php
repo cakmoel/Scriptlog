d72ec12cafe15bb5d94831164f4eb15e97b1e185
cakmoel <alanmoehammad@gmail.com>
2020-09-13 20:57:47 +0700

    update logout

--- src/admin/logout.php
+++ src/admin/logout.php
@@ -1,8 +1,45 @@
 <?php if (!defined('SCRIPTLOG')) die("Direct Access Not Allowed!");
 
-if(isset($_GET['load']) && $_GET['load'] == basename('logout')) {
+$action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action'])) : "";
+$logOutId =  isset($_GET['logOutId']) ? safe_html($_GET['logOutId']): null;
 
- $authenticator -> logout();
+try {
     
+    switch ($action) {
+
+        default:
+        case ActionConst::LOGOUT:
+            
+            if (false === $authenticator->userAccessControl()) {
+    
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
+           } else {
+
+              $valid_logout = !empty($logOutId) && verify_logout_id($logOutId);
+
+              if (!$valid_logout) {
+                  
+                  header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                  throw new AppException("URL Redirection to Untrusted Site");
+
+              } else {
+
+                $authenticator->logout();
+
+              }
+
+           }
+        
+           break;
+    
+    }
+
+} catch (AppException $e) {
+    
+    LogError::setStatusCode(http_response_code());
+    LogError::newMessage($e);
+    LogError::customErrorMessage('admin');
+
 }
 
