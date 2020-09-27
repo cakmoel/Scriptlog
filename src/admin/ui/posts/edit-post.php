6ccfe1ef46d0a813cc62a1ae863bc26f31896d4f
cakmoel <alanmoehammad@gmail.com>
2020-09-27 12:46:22 +0700

    fix web accessibility on admin/ui/posts/edit-page.php

--- src/admin/ui/posts/edit-post.php
+++ src/admin/ui/posts/edit-post.php
@@ -17,7 +17,7 @@
  <!-- Main content -->
 <section class="content">
 <div class="row">
-<div class="col-md-12">
+<div class="col-md-8">
 <div class="box box-primary">
 <div class="box-header with-border"></div>
 
@@ -26,7 +26,7 @@ if (isset($errors)) :
 ?>
 <div class="alert alert-danger alert-dismissible">
 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-<h4><i class="icon fa fa-warning"></i> Invalid Form Data!</h4>
+<h2><i class="icon fa fa-warning"></i> Invalid Form Data!</h2>
 <?php 
 foreach ($errors as $e) :
 echo '<p>' . $e . '</p>';
@@ -35,23 +35,10 @@ endforeach;
 </div>
 <?php 
 endif;
-?>
-
-<?php
-if (isset($saveError)) :
-?>
-<div class="alert alert-danger alert-dismissible">
-<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-<h4><i class="icon fa fa-ban"></i> Alert!</h4>
-<?php 
-echo "Error saving data. Please try again." . $saveError;
-?>
-</div>
-<?php 
-endif;
 
 $action = isset($formAction) ? $formAction : null;
 $post_id = isset($postData['ID']) ? (int)$postData['ID'] : 0;
+
 ?>
 
 <form method="post" action="<?=generate_request('index.php', 'post', ['posts', $action, $post_id])['link']; ?>" role="form" enctype="multipart/form-data" >
@@ -60,15 +47,15 @@ $post_id = isset($postData['ID']) ? (int)$postData['ID'] : 0;
 
 <div class="box-body">
 <div class="form-group">
-<label>Title (required)</label>
-<input type="text" class="form-control" name="post_title" placeholder="Enter title here" value="
+<label for="title">Title (required)</label>
+<input type="text" class="form-control" id="title" name="post_title" placeholder="Enter title here" value="
 <?=(isset($postData['post_title'])) ? safe_html($postData['post_title']) : ""; ?>
 <?=(isset($formData['post_title'])) ? safe_html($formData['post_title']) : ""; ?>" maxlength="200" required>
 </div>
 
 <div class="form-group">
-<label>Meta Description</label>
-<textarea class="form-control" name="post_summary" rows="3" maxlength="320" >
+<label for="meta_desc">Meta Description</label>
+<textarea class="form-control" id="meta_desc" rows="3" placeholder="Enter ..." name="post_summary" maxlength="320" >
 <?=(isset($postData['post_summary'])) ? safe_html($postData['post_summary']) : ""; ?>
 <?=(isset($formData['post_summary'])) ? safe_html($formData['post_summary']) : ""; ?>
 </textarea>
@@ -76,8 +63,8 @@ $post_id = isset($postData['ID']) ? (int)$postData['ID'] : 0;
 </div>
 
 <div class="form-group">
-<label>Meta Keywords</label>
-<textarea class="form-control" name="post_keyword" rows="3" maxlength="200" >
+<label for="meta_key">Meta Keywords</label>
+<textarea class="form-control" id="meta_key" rows="3" placeholder="Enter ..." name="post_keyword" maxlength="200" >
 <?=(isset($postData['post_keyword'])) ? safe_html($postData['post_keyword']) : ""; ?>
 <?=(isset($formData['post_keyword'])) ? safe_html($formData['post_keyword']) : ""; ?>
 </textarea>
@@ -85,16 +72,16 @@ $post_id = isset($postData['ID']) ? (int)$postData['ID'] : 0;
 </div>
 
 <div class="form-group">
-<label>Tags</label>
-<input type="text" class="form-control" name="post_tags" placeholder="Enter tags here" value="
+<label for="tag">Tags</label>
+<input type="text" class="form-control" id="tag" name="post_tags" placeholder="Enter tags here" value="
 <?=(isset($postData['post_tags'])) ? safe_html($postData['post_tags']) : ""; ?>
 <?=(isset($formData['post_tags'])) ? safe_html($formData['post_tags']) : ""; ?>" maxlength="400">
 <p class="help-block">Comma separated</p>
 </div>
 
 <div class="form-group">
-<label>Content (required)</label>
-<textarea class="textarea" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;" name="post_content" maxlength="50000"  required>
+<label for="summernote">Content (required)</label>
+<textarea class="form-control" id="summernote" name="post_content" rows="10" cols="80" maxlength="50000" required>
 <?=(isset($postData['post_content'])) ? safe_html($postData['post_content']) : ""; ?>
 <?=(isset($formData['post_content'])) ? safe_html($formData['post_content']) : ""; ?>
 </textarea>
@@ -103,12 +90,12 @@ $post_id = isset($postData['ID']) ? (int)$postData['ID'] : 0;
 <?=(isset($topics)) ? $topics : ""; ?>
 
 <div class="form-group">
-<label>Post status</label>
+<label for="post_status">Post status</label>
 <?=(isset($postStatus)) ? $postStatus : ""; ?>
 </div>
 
 <div class="form-group">
-<label>Comment status</label>
+<label for="comment_status">Comment status</label>
 <?=(isset($commentStatus)) ? $commentStatus : ""; ?>
 </div>
 
