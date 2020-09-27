7ec2fe50c1049e2df39c6e86e9e7319b7c00f360
cakmoel <alanmoehammad@gmail.com>
2020-09-27 07:25:25 +0700

    fix web accessibility continued

--- src/admin/admin-layout.php
+++ src/admin/admin-layout.php
@@ -6,14 +6,14 @@ function admin_header($stylePath, $breadCrumbs = null, $allowedQuery = null)
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 <title><?= cp_tag_title($breadCrumbs, $allowedQuery); ?></title>
   <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <meta content="width=device-width" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?= $stylePath; ?>/assets/components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= $stylePath; ?>/assets/components/bootstrap/dist/css/bootstrap-select.css">
