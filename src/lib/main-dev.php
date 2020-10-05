f35f5f08cad4ddbf60d2895e2603c704c00b594c
cakmoel <alanmoehammad@gmail.com>
2020-10-05 19:36:25 +0700

    revamp main-dev.php

--- src/lib/main-dev.php
+++ src/lib/main-dev.php
@@ -12,8 +12,6 @@
  * 
  */
 
-ini_set('memory_limit', "5M");
-error_reporting(-1);
 #ini_set("session.cookie_secure", 1);  
 #ini_set("session.cookie_lifetime", 86400);  
 ini_set("session.cookie_httponly", 1);
@@ -25,42 +23,42 @@ ini_set('session.save_handler', 'files');
 ini_set('session.gc_divisor', 100);
 ini_set('session.gc_maxlifetime', 1440);
 ini_set('session.gc_probability',1);
-#date_default_timezone_set("GMT");
 
+#date_default_timezone_set("GMT");
 
 require __DIR__ . '/common.php';
 
 if (!defined('PHP_EOL')) {
 
-    if (strtoupper(substr(PHP_OS,0,3) == 'WIN')) {
-          
-        define('PHP_EOL',"\r\n");
-  
-    } elseif (strtoupper(substr(PHP_OS,0,3) == 'MAC')) {
-          
-        define('PHP_EOL',"\r");
+  if (strtoupper(substr(PHP_OS,0,3) == 'WIN')) {
+        
+    define('PHP_EOL',"\r\n");
+
+  } elseif (strtoupper(substr(PHP_OS,0,3) == 'MAC')) {
+        
+    define('PHP_EOL',"\r");
+    
+  } elseif (strtoupper(substr(PHP_OS,0,3) == 'DAR')) {
+        
+    define('PHP_EOL',"\n");
       
-    } elseif (strtoupper(substr(PHP_OS,0,3) == 'DAR')) {
-          
-        define('PHP_EOL',"\n");
+  } else {
         
-    } else {
-          
-        define('PHP_EOL',"\n");
-        
-    }
-  
+    define('PHP_EOL',"\n");
+      
+  }
+
 } 
 
 $is_secure = false;
 
 if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
 
-    $is_secure = true;
+ $is_secure = true;
 
 } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
     
-    $is_secure = true;
+ $is_secure = true;
     
 }
 
@@ -70,14 +68,19 @@ if (!defined('APP_HOSTNAME')) define('APP_HOSTNAME', isset($_SERVER['HTTP_HOST']
 
 $config = null;
 
-if (file_exists(APP_ROOT . 'config.php')) {
-
-    $config = require __DIR__ . '/../config.php';
-
-} else {
-
-    $config = require __DIR__ . '/../config.sample.php';
+if (is_readable(APP_ROOT.'config.php')) {
     
+    $config = require __DIR__ . '/../config.php';
+    
+ } else {
+     
+    if (is_dir(APP_ROOT . 'install')) {
+
+        header("Location: ".APP_PROTOCOL."://".APP_HOSTNAME.dirname($_SERVER['PHP_SELF']).DS.'install');
+        exit();
+         
+    }
+         
 }
 
 #================================== call functions in directory lib/utility ===========================================
@@ -158,7 +161,7 @@ call_htmlpurifier();
     
      ### 'archive/12/2017
      'archive' => "/archive/[0-9]{2}/[0-9]{2}/[0-9]{4}",
-     
+
      ### '/blog?p=255'
     'blog' => "/blog([^/]*)",                       
     
@@ -190,14 +193,13 @@ $rules = array(
 #====== an instantiation of Database connection =========
 $dbc = DbFactory::connect(['mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'], $config['db']['user'], $config['db']['pass']]);
 
-// Register rules and an instance of database connection
+// Register rules of routes and an instance of database connection
 Registry::setAll(array('dbc' => $dbc, 'route' => $rules));
 
 /* an instances of class that necessary for the system
  * please do not change this below variable 
  * 
  * @var $searchPost invoked by search functionality
- * @var $postFeeds run by rss feed functionality
  * @var $sanitizer adapted by sanitize functionality
  * @var $userDao, $validator, $authenticator, $ubench --
  * these are collection of objects or instances of classes 
@@ -206,12 +208,12 @@ Registry::setAll(array('dbc' => $dbc, 'route' => $rules));
  */
 $key = scriptlog_cipher_key();
 $searchPost = new SearchFinder($dbc);
-$frontPaginator = new Paginator(10, 'p');
 $sanitizer = new Sanitize();
 $userDao = new UserDao();
 $userToken = new UserTokenDao();
 $postDao = new PostDao();
 $topicDao = new TopicDao();
+$postTopicDao = new PostTopicDao();
 $pageDao = new PageDao();
 $menuDao = new MenuDao();
 $frontDispatcher = new Dispatcher();
@@ -220,27 +222,17 @@ $authenticator = new Authentication($userDao, $userToken, $validator);
 $ubench = new Ubench();
 $sessionMaker = new SessionMaker(set_session_cookies_key());
 
-// register core of front registry objects
-Registry::registryObject('postDao', 'post');
-Registry::registryObject('topicDao', 'topic');
-Registry::registryObject('pageDao', 'page');
-Registry::registryObject('menuDao', 'menu');
-
 content_security_policy();
 
 session_set_save_handler($sessionMaker, true);
-session_save_path(__DIR__ . '/utility/.sessions');
-
-// These line (179 and 180) are experimental code. You do not need it.
-# $bones = new Bones();
-# $request = new RequestHandler($bones);
+session_save_path(__DIR__ . '/utility/.sessions'.DS);
 
 # set_exception_handler('LogError::exceptionHandler');
 # set_error_handler('LogError::errorHandler');
 # register_shutdown_function('scriptlog_shutdown_fatal');
 
 if (!start_session_on_site($sessionMaker)) {
-     
+
     ob_start();
     
 }
