6114dd8fb6f660ee32ee5a3750f014d60a57004a
cakmoel <alanmoehammad@gmail.com>
2020-10-06 19:40:48 +0700

    revamping sanitize untrustworthy HTML user input

--- src/lib/utility/prevent-injection.php
+++ src/lib/utility/prevent-injection.php
@@ -1,10 +1,11 @@
 <?php
+use HtmlSanitizer\Sanitizer;
 /**
  * Prevent Injection Function
  * 
  * @category Function
- * @uses htmLawed($text) htmLawed
  * @see  http://www.bioinformatics.org/phplabware/internal_utilities/htmLawed 
+ * @see https://github.com/tgalopin/html-sanitizer/blob/master/docs/1-getting-started.md
  * @param string $data
  * @return string
  * 
@@ -14,6 +15,18 @@ function prevent_injection($str)
     
   $filter = @trim(stripslashes(strip_tags(htmlspecialchars($str, ENT_COMPAT|ENT_HTML5, 'UTF-8'))));
   
-  return htmLawed($filter);
+  $sanitizer = \HtmlSanitizer\Sanitizer::create(['extensions' => ['basic']]);
+
+  if (PHP_VERSION_ID >= 70100) {
+
+     $safe_html = $sanitizer->sanitize($filter);
+
+  } else {
+
+     $safe_html = htmLawed($filter);
+
+  }
+  
+  return $safe_html;
 
 }
