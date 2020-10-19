859021f04b4534c3695818ef1aeecd70daf739b2
cakmoel <alanmoehammad@gmail.com>
2020-10-19 18:42:52 +0700

    revamp get-mime function

--- src/lib/utility/get-mime.php
+++ src/lib/utility/get-mime.php
@@ -5,67 +5,47 @@
  * 
  * @category Function
  * @param string $filename
+ * @param number|int $mode
+ * @return string
  * 
  */
 function get_mime($filename, $mode = 0)
 {
     $mime_types = array(
 
-        'txt' => 'text/plain',
-        'htm' => 'text/html',
-        'html' => 'text/html',
-        'php' => 'text/html',
-        'css' => 'text/css',
-        'js' => 'application/javascript',
-        'json' => 'application/json',
-        'xml' => 'application/xml',
-        'swf' => 'application/x-shockwave-flash',
-        'flv' => 'video/x-flv',
-
         // images
-        'png' => 'image/png',
-        'jpe' => 'image/jpeg',
+        'png'  => 'image/png',
+        'jpe'  => 'image/jpeg',
         'jpeg' => 'image/jpeg',
-        'jpg' => 'image/jpeg',
-        'gif' => 'image/gif',
-        'bmp' => 'image/bmp',
-        'ico' => 'image/vnd.microsoft.icon',
+        'jpg'  => 'image/jpeg',
+        'gif'  => 'image/gif',
+        'bmp'  => 'image/bmp',
+        'ico'  => 'image/vnd.microsoft.icon',
         'tiff' => 'image/tiff',
-        'tif' => 'image/tiff',
-        'svg' => 'image/svg+xml',
-        'svgz' => 'image/svg+xml',
-
+        'tif'  => 'image/tiff',
+        'webp' => 'image/webp',
+      
         // archives
         'zip' => 'application/zip',
-        'rar' => 'application/x-rar-compressed',
-        'exe' => 'application/x-msdownload',
-        'msi' => 'application/x-msdownload',
-        'cab' => 'application/vnd.ms-cab-compressed',
-
+        
         // audio/video
         'mp3' => 'audio/mpeg',
         'wav' => 'audio/wav',
         'ogg' => 'audio/ogg',
-        'qt'  => 'video/quicktime',
-        'mov' => 'video/quicktime',
         'mp4' => 'video/mp4',
         'ogg' => 'video/ogg',
         'webm' => 'video/webm',
 
         // adobe
         'pdf' => 'application/pdf',
-        'psd' => 'image/vnd.adobe.photoshop',
-        'ai' => 'application/postscript',
-        'eps' => 'application/postscript',
-        'ps' => 'application/postscript',
-
+     
         // ms office
         'doc' => 'application/msword',
+        'docx' => 'application/msword',
         'rtf' => 'application/rtf',
         'xls' => 'application/vnd.ms-excel',
-        'ppt' => 'application/vnd.ms-powerpoint',
-        'docx' => 'application/msword',
         'xlsx' => 'application/vnd.ms-excel',
+        'ppt' => 'application/vnd.ms-powerpoint',
         'pptx' => 'application/vnd.ms-powerpoint',
 
         // open office
@@ -82,7 +62,7 @@ function get_mime($filename, $mode = 0)
     }
 
     if (function_exists('finfo_open') && $mode==0) {
-        $finfo = finfo_open(FILEINFO_MIME);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $mimetype = finfo_file($finfo, $filename);
         finfo_close($finfo);
         return $mimetype;
