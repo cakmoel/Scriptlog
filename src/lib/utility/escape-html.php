c504f864353428542fb78d5ea2565d6471c78e75
cakmoel <alanmoehammad@gmail.com>
2020-10-19 08:51:52 +0700

    fix full path disclosure with null byte

--- src/lib/utility/escape-html.php
+++ src/lib/utility/escape-html.php
@@ -12,3 +12,19 @@ function escape_html($input, $encoding = "UTF-8")
 {
   return htmlentities($input, ENT_QUOTES | ENT_HTML5, $encoding);
 }
+
+/**
+ * escape_null_byte
+ *
+ * @param string $input
+ * @return void
+ * 
+ */
+function escape_null_byte($input)
+{
+
+ $input = str_replace(chr(0), '', $input);
+
+ return $input;
+
+}
