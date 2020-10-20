db1e2fff83d154aa6eec8c0e05f75843159e20fb
cakmoel <alanmoehammad@gmail.com>
2020-10-20 10:23:17 +0700

    revamp Scriptlog Cryptograph

---
+++ src/lib/core/ScriptlogCryptonizeException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ScriptlogCryptoException extends CoreException
+ * 
+ * @category Core Class
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * 
+ */
+class ScriptlogCryptonizeException extends CoreException
+{
+
+}
