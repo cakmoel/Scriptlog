cfb79a19a311d07f1e59e45875e810b8e13b1530
cakmoel <alanmoehammad@gmail.com>
2020-10-20 10:24:10 +0700

    revamp SessionMaker

---
+++ src/lib/core/SessionMakerException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class SessionMakerException extends CoreException
+ * 
+ * @category Core Class
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * 
+ */
+class SessionMakerException extends CoreException
+{
+
+}
