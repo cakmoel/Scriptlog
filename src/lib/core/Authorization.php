5d34f69af9aa66be14cab2d62c5490c5d208431b
cakmoel <alanmoehammad@gmail.com>
2020-10-28 14:38:44 +0700

    update class Authorization

--- src/lib/core/Authorization.php
+++ src/lib/core/Authorization.php
@@ -11,29 +11,62 @@
 class Authorization
 {
 
+/**
+ * authentication
+ *
+ * @var object
+ * @static
+ * 
+ */
 private static $authentication;
 
+/**
+ * setAuthInstance
+ *
+ * @param object $authentication
+ * @return void
+ * 
+ */
 public static function setAuthInstance($authentication)
 {
 
   if (is_a($authentication, 'Authentication')) {
 
-       self::$authentication = $authentication;
+      self::$authentication = $authentication;
 
   }
 
 }
 
+/**
+ * getAuthInstance
+ *
+ * @return void
+ * 
+ */
 public static function getAuthInstance()
 {
   return self::$authentication;
 }
 
+/**
+ * authorizeRole
+ *
+ * @param string $role
+ * @return void
+ * 
+ */
 public static function authorizeRole($role)
 {
 return self::getAuthInstance()->userAccessControl($role);
 }
 
+/**
+ * authorizeLevel
+ *
+ * @return void
+ * 
+ */
 public static function authorizeLevel()
 {
 return self::getAuthInstance()->accessLevel();
