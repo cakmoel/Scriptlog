425d04da4c041c39d64a55ed336a326f2367408c
cakmoel <alanmoehammad@gmail.com>
2020-11-12 18:17:01 +0700

    revamp function upload-doc

--- src/lib/utility/upload-doc.php
+++ src/lib/utility/upload-doc.php
@@ -6,20 +6,26 @@
  * @param string $folder
  * 
  */
-function upload_doc($file_location, $file_size, $file_type, $file_name)
+function upload_doc($file_location, $file_name)
 {
   // Doc directory
-  $doc_path = __DIR__ . '/../../public/files/docs/';
+  $doc_path = __DIR__ . '/../../'.APP_DOCUMENT;
   $doc_uploaded = $doc_path . $file_name;
 
-  if ($file_size > APP_FILE_SIZE) {
+  if (get_file_extension($file_name) == 'zip') {
 
-     throw new UploadException("Error - File size too big!");
+      if ((zip_file_scanner($file_name) === true) || (format_size_unit(get_zip_size($file_location)) == '0 bytes')) {
 
-  } else {
+          return false;
 
-     move_uploaded_file($file_location, $doc_uploaded);
-     
+      }
+
   }
-  
+
+  if (!move_uploaded_file($file_location, $doc_uploaded)) {
+
+    return false;
+
+  }
+
 }
