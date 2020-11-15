bfb546a4ce80ca6e47f23fa5f51649dab3a94002
cakmoel <alanmoehammad@gmail.com>
2020-11-15 12:58:43 +0700

    'update guzzle'

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 Scriptlog's base original code was forked from **Piluscart 1.4.1**. We took her out from her weblog engine that support blog functionality on Piluscart 1.4.1. Obviously Scriptlog is Piluscart's little sister. She was being refactored to help her siblings going further on next generation of Piluscart development which is undefined yet.
 
-Scriptlog is not a replacement for a full Content Management System. Do not plan on building the next Drupal or CMS Framework with it! However, It is built for several reasons.
+Scriptlog is not a replacement for a full Content Management System. Do not plan on building the next CMS Framework with it! However, It is built for several reasons.
 
 1. To power parts of weblog or personal website that did not need a full CMS.
 2. Runs small scripts and systems without a lot of overhead.
---
+++ src/lib/vendor/composer/installed.php
@@ -0,0 +1,193 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ccb7d027aa034c5d9afd26071069bd97c98d92da',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ccb7d027aa034c5d9afd26071069bd97c98d92da',
+    ),
+    'doctrine/lexer' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e864bbf5904cb8f5bb334f99209b48018522f042',
+    ),
+    'egulias/email-validator' => 
+    array (
+      'pretty_version' => '2.1.23',
+      'version' => '2.1.23.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa792ad1853ae2bc60528dd3e5cbf4542d3c1df',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.7.0',
+      'version' => '1.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53330f47520498c0ae1f61f7e2c90f55690c06a3',
+    ),
+    'intervention/image' => 
+    array (
+      'pretty_version' => '2.5.1',
+      'version' => '2.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'abbf18d5ab8367f96b3205ca3c89fb2fa598c69e',
+    ),
+    'ircmaxell/random-lib' => 
+    array (
+      'pretty_version' => 'v1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e9e0204f40e49fa4419946c677eccd3fa25b8cf4',
+    ),
+    'ircmaxell/security-lib' => 
+    array (
+      'pretty_version' => 'v1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f3db6de12c20c9bcd1aa3db4353a1bbe0e44e1b5',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'sinergi/browser-detector' => 
+    array (
+      'pretty_version' => '6.1.3',
+      'version' => '6.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16cad9cafdd5bde8cc03d9ad4cc826988a67401e',
+    ),
+    'symfony/polyfill-iconv' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c536646fdb4f29104dd26effc2fdcb9a5b085024',
+    ),
+    'symfony/polyfill-intl-grapheme' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c7cf3f858ec7d70b89559d6e6eb1f7c2517d479c',
+    ),
+    'symfony/polyfill-intl-idn' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3b75acd829741c768bc8b1f84eb33265e7cc5117',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '727d1096295d807c309fb01a851577302394c897',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39d483bdf39be819deabf04ec872eb0b2410b531',
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cede45fcdfabdd6043b3592e83678e42ec69e930',
+    ),
+    'voku/anti-xss' => 
+    array (
+      'pretty_version' => '4.1.28',
+      'version' => '4.1.28.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8919a19846d2b100e664d9e3cecfcaba4f57d9b4',
+    ),
+    'voku/portable-ascii' => 
+    array (
+      'pretty_version' => '1.5.3',
+      'version' => '1.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '25bcbf01678930251fd572891447d9e318a6e2b8',
+    ),
+    'voku/portable-utf8' => 
+    array (
+      'pretty_version' => '5.4.47',
+      'version' => '5.4.47.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c92522515a2d5ec9b03fd3c9e231b8b277c65121',
+    ),
+  ),
+);
