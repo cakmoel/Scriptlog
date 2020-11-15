4e8943799f83bc75daf33469430597d9b6fb4a81
cakmoel <alanmoehammad@gmail.com>
2020-11-15 17:00:51 +0700

    update email_validaiton

--- src/lib/utility/email-validation.php
+++ src/lib/utility/email-validation.php
@@ -1,13 +1,17 @@
 <?php
 /**
- * email_validation
+ * validator_email_instance
  * 
  * @category function
- * @param string $email
+ * @author M.Noermoehammad
+ * @author Eduardo Gulias Davis author of EmailValidator Package
  * @license MIT
+ * @version 1.0
+ * @param string $email
  * @see https://github.com/egulias/EmailValidator
  * @see https://www.cloudways.com/blog/php-libraries/#email-validator
- * @return void
+ * @uses \Egulias\EmailValidator\EmailValidator
+ * @return object
  * 
  */
 
@@ -16,6 +20,12 @@ use Egulias\EmailValidator\Validation\DNSCheckValidation;
 use Egulias\EmailValidator\Validation\MultipleValidationWithAnd;
 use Egulias\EmailValidator\Validation\RFCValidation;
 
+/**
+ * validator_email_instance()
+ *
+ * @return object
+ * 
+ */
 function validator_email_instance()
 {
   
@@ -25,14 +35,41 @@ function validator_email_instance()
 
 }
 
+/**
+ * email_validation
+ *
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * @param string $email
+ * @param string $validation_type
+ * @return object
+ * 
+ */
 function email_validation($email, $validation_type)
 {
   return validator_email_instance()->isValid($email, $validation_type);
 }
 
-function email_multiple_validation($email, array $validation_type)
+/**
+ * email_multiple_validation
+ *
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * @param string $email
+ * @param array $validation_type
+ * @uses \Egulias\EmailValidator\Validation\MultipleValidationWithAnd
+ * @uses \Egulias\EmailValidator\Validation\DNSCheckValidation
+ * @uses \Egulias\EmailValidator\Validation\RFCValidation
+ * @return bool
+ * 
+ */
+function email_multiple_validation($email)
 {
-  $multiple_validations = new MultipleValidationWithAnd($validation_type);
+  $multiple_validations = new MultipleValidationWithAnd([new RFCValidation(), new DNSCheckValidation()]);
 
   return validator_email_instance()->isValid($email, $multiple_validations);
 }
