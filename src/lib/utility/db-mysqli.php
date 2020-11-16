4c52f09cf83252dbefb35aa522b82cf50e0ae1f0
cakmoel <alanmoehammad@gmail.com>
2020-11-16 08:55:49 +0700

    update db-mysqli function

--- src/lib/utility/db-mysqli.php
+++ src/lib/utility/db-mysqli.php
@@ -1,24 +1,36 @@
 <?php
 /**
- * collection of function uses MySQL Improved (MySQLi) extension
+ * db_instance
+ * An instance of DbMySQLi Class
  * 
- * @category function
+ * @category function db_instace()
+ * @author M.Noermoehammad
+ * @license MIT
  * @uses DbMySQLi::getInstance
- * @author   M.Noermoehammad
- * @license  MIT
- * @version  1.0
+ * @version 1.0
+ * @return object
  * 
  */
-
-// database connection 
 function db_instance()
 {
+
   $database = DbMySQLi::getInstance();
 
   return $database;
+
 }
 
-// database query
+/**
+ * db_simple_query
+ * 
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * @param string $sql
+ * @return object
+ * 
+ */
 function db_simple_query($sql)
 {
   
@@ -29,12 +41,17 @@ function db_simple_query($sql)
 }
 
 /**
- * db_prepared_query function
+ * db_prepared_query
  * $sql = DML
  * 
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
  * @param string $sql
  * @param array $params
- * @return void
+ * @return object
+ * 
  */
 function db_prepared_query($sql, $params, $types)
 {
@@ -46,11 +63,16 @@ function db_prepared_query($sql, $params, $types)
 }
 
 /**
- * is_table_exists function
+ * is_table_exists
  * Checking whether table name exists on database
  * 
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
  * @param string $table
  * @return boolean
+ * 
  */
 function is_table_exists($table)
 {
@@ -62,21 +84,36 @@ function is_table_exists($table)
 }
 
 /**
- * check_table function
+ * check_table 
  * This function will check all of tables needed for your weblog 
+ * 
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
  * @return void
+ * 
  */
 function check_table()
 {
 
   $dbscheme = false;
 
-  if (!(is_table_exists('tbl_comments') || (is_table_exists('tbl_comment_reply') || (is_table_exists('tbl_login_attempt')) 
-      || (is_table_exists('tbl_media') || (is_table_exists('tbl_mediameta') || (is_table_exists('tbl_media_download') 
-      || (is_table_exists('tbl_menu') || (is_table_exists('tbl_menu_child') || (is_table_exists('tbl_plugin') 
-      || (is_table_exists('tbl_posts') || (is_table_exists('tbl_post_topic') || (is_table_exists('tbl_settings') 
-      || (is_table_exists('tbl_themes') || (is_table_exists('tbl_topics') || (is_table_exists('tbl_users') 
-      || (is_table_exists('tbl_user_token'))))))))))))))))) {
+  if (!(is_table_exists('tbl_comments') 
+      || is_table_exists('tbl_comment_reply') 
+      || is_table_exists('tbl_login_attempt')
+      || is_table_exists('tbl_media') 
+      || is_table_exists('tbl_mediameta') 
+      || is_table_exists('tbl_media_download') 
+      || is_table_exists('tbl_menu') 
+      || is_table_exists('tbl_plugin') 
+      || is_table_exists('tbl_posts') 
+      || is_table_exists('tbl_post_topic') 
+      || is_table_exists('tbl_settings') 
+      || is_table_exists('tbl_themes') 
+      || is_table_exists('tbl_topics') 
+      || is_table_exists('tbl_users') 
+      || is_table_exists('tbl_user_token'))) {
 
         $dbscheme = false;
 
@@ -90,3 +127,42 @@ function check_table()
 
 }
 
+/**
+ * get_result
+ * 
+ * Example: withoud mysqlnd on the server
+ * $Statement = $Database->prepare( 'SELECT x FROM y WHERE z = ?' );
+ * $Statement->bind_param( 's', $z );
+ * $Statement->execute();
+ * $RESULT = get_result( $Statement );
+ * while ( $DATA = array_shift( $RESULT ) ) {
+ *    // Do stuff with the data
+ * }
+ * 
+ * @category function
+ * @author Contributors
+ * @license MIT
+ * @version 1.0
+ * @param string $statement
+ * @see https://stackoverflow.com/questions/10752815/mysqli-get-result-alternative
+ * @return array
+ * 
+ */
+function get_result($Statement)
+{
+
+$result = array();
+$Statement->store_result();
+    for ( $i = 0; $i < $Statement->num_rows; $i++ ) {
+        $Metadata = $Statement->result_metadata();
+        $params = array();
+        while ( $Field = $Metadata->fetch_field() ) {
+            $params[] = &$result[ $i ][ $Field->name ];
+        }
+        call_user_func_array( array( $Statement, 'bind_result' ), $params );
+        $Statement->fetch();
+    }
+    
+  return $result;
+
+}
