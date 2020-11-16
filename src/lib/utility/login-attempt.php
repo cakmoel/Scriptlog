a64309e2dd462a76bf69e148bfa20195f8175761
cakmoel <alanmoehammad@gmail.com>
2020-11-16 08:56:31 +0700

    revamp login-attempt.php

--- src/lib/utility/login-attempt.php
+++ src/lib/utility/login-attempt.php
@@ -4,44 +4,17 @@
  *
  * @category function
  * @param string $ip
- * @return mixed
  * 
  */
 function get_login_attempt($ip)
 {
  
-  if(version_compare(PHP_VERSION, "7.3", ">=")) {
+  $sql = "SELECT count(ip_address) AS failed_login_attempt 
+          FROM tbl_login_attempt 
+          WHERE ip_address = ? 
+          AND login_date BETWEEN DATE_SUB( NOW() , INTERVAL 1 DAY ) AND NOW()";
 
-    $sql = "SELECT count(ip_address) AS failed_login_attempt 
-            FROM tbl_login_attempt 
-            WHERE ip_address = '$ip' 
-            AND login_date BETWEEN DATE_SUB( NOW() , INTERVAL 1 DAY ) AND NOW()";
-
-    if($result = db_simple_query($sql)) {
-
-      if(is_array($result)) {
-
-        while ($row = $result->fetch_assoc()) {
-         
-             $row['failed_login_attempt'];
-
-        }
-
-      }
-       
-    }
-    
-  } else {
-
-    $sql = "SELECT count(ip_address) AS failed_login_attempt 
-    FROM tbl_login_attempt 
-    WHERE ip_address = ? 
-    AND login_date BETWEEN DATE_SUB( NOW() , INTERVAL 1 DAY ) AND NOW()";
-
-    $row = db_prepared_query($sql, [$ip], "s")->get_result()->fetch_assoc();
-
-
-  }
+  $row = db_prepared_query($sql, [$ip], "s")->get_result()->fetch_assoc();
 
   return $row;
 
