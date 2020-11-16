9c9c0fcdbf2a0067830bf0f85b8c76ca7cbd01dc
cakmoel <alanmoehammad@gmail.com>
2020-11-17 00:50:35 +0700

    update upload_doc function

--- src/lib/utility/upload-doc.php
+++ src/lib/utility/upload-doc.php
@@ -12,20 +12,46 @@ function upload_doc($file_location, $file_name)
   $doc_path = __DIR__ . '/../../'.APP_DOCUMENT;
   $doc_uploaded = $doc_path . $file_name;
 
-  if (get_file_extension($file_name) == 'zip') {
+  if (move_uploaded_file($file_location, $doc_uploaded)) {
 
-      if ((zip_file_scanner($file_name) === true) || (format_size_unit(get_zip_size($file_location)) == '0 bytes')) {
+    if ( get_file_extension($file_name) == 'zip') {
 
-          scriptlog_error("Document corrupted!");
+      if ((zip_file_scanner($doc_uploaded) === true) || (format_size_unit(get_zip_size($file_location)) == '0 bytes')) {
 
+          unlink($doc_uploaded);
+
+      } else {
+
+         $zip = new ZipArchive();
+
+         $opened = $zip->open($doc_uploaded);
+
+         if ( $opened === true ) {
+
+            $file_count = (version_compare(phpversion(), "7.2.0", ">=")) ? $zip->count() : $zip->numFiles;
+
+            for ($i = 0; $i < $file_count; $i++) {
+
+               $file_index = $zip->getNameIndex($i);
+
+               preg_match('/(.*)(phpinfo|system|php_uname|chmod|fopen|eval|flclose|readfile|base64_decode|passthru)(.*)/Us', $file_index, $matches);
+
+               if (count($matches) > 0) {
+
+                    $zip->deleteName($file_index);
+
+               }
+            
+            }
+
+            $zip->close();
+           
+          }
       }
 
-  }
-
-  if (!move_uploaded_file($file_location, $doc_uploaded)) {
-
-    scriptlog_error("upload doc failed!");
+    }
 
   }
+  
 
 }
