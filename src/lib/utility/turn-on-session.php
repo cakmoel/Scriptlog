51a0adedd8a18ed9068fb8fdec0dcde3be617dff
cakmoel <alanmoehammad@gmail.com>
2020-11-17 15:00:55 +0700

    revamping turn_on_session

--- src/lib/utility/turn-on-session.php
+++ src/lib/utility/turn-on-session.php
@@ -1,21 +1,23 @@
 <?php
 /**
- * Turn on Function
+ * turn_on_session()
+ * 
  * Checking too old session ID and start session
  * 
  * @category function
- * @see https://github.com/GoogleChromeLabs/samesite-examples/blob/master/php.md 
- * @see https://stackoverflow.com/questions/36877/how-do-you-set-up-use-httponly-cookies-in-php
- * @see https://stackoverflow.com/a/46971326/2308553 
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
  * @param number $life_time
  * @param string $session_name
+ * @see https://www.php.net/manual/en/function.session-create-id.php
  * @return void
  * 
  */
 function turn_on_session($session_handler, $life_time, $cookies_name, $path, $domain, $secure, $httponly)
 {
 
- if (is_object($session_handler)) {
+ if (is_a($session_handler, 'SessionMaker')) {
 
     $session_handler->start();
 
@@ -30,22 +32,16 @@ function turn_on_session($session_handler, $life_time, $cookies_name, $path, $do
   // Do not allow to use too old session ID
  if (!empty($_SESSION['deleted_time']) && $_SESSION['deleted_time'] < time() - $life_time) {
         
-      session_unset();
-
       $session_handler->forget();
 
-      session_write_close();
-
-      session_id();
- 
       $session_handler->start();
         
       set_cookies_scl($cookies_name, session_id(), $life_time, $path, $domain, $secure, $httponly);
 
       $session_handler->refresh();
-     
-  }
-   
+      
+ }
+  
 }
 
 
