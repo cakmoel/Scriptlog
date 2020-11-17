9af07cbed7d10b8d31200ae727793b66712bc124
cakmoel <alanmoehammad@gmail.com>
2020-11-17 18:56:22 +0700

    revamp regenerate_session function

--- src/lib/utility/regenerate-session.php
+++ src/lib/utility/regenerate-session.php
@@ -17,6 +17,8 @@
 function regenerate_session()
 {
 
+ $newid = session_id();
+
  if (session_status() != PHP_SESSION_ACTIVE) {
       
    session_start();
@@ -34,8 +36,6 @@ function regenerate_session()
    session_regenerate_id(true);
 
 }
-
-$newid = (version_compare(phpversion(), "7.1.0", ">=")) ? session_create_id() : session_id();
 
 $_SESSION['deleted_time'] = time();
 
@@ -86,51 +86,4 @@ function clear_duplicate_cookies()
 
  }
  
-}
-
-/**
- * get_session_data()
- *
- * @param string $session_name
- * @param string $session_save_handle
- * @see https://www.php.net/manual/en/ref.session.php#96309
- * @return void
- * 
- */
-function get_session_data($session_name = '_scriptlog', $session_save_handler = 'files')
-{
-
-   $session_data = array();
-   # did we get told what the old session id was? we can't continue it without that info
-   if (array_key_exists($session_name, $_COOKIE)) {
-       # save current session id
-       $session_id = $_COOKIE[$session_name];
-       $old_session_id = session_id();
-      
-       # write and close current session
-       session_write_close();
-      
-       # grab old save handler, and switch to files
-       $old_session_save_handler = ini_get('session.save_handler');
-       ini_set('session.save_handler', $session_save_handler);
-      
-       # now we can switch the session over, capturing the old session name
-       $old_session_name = session_name($session_name);
-       session_id($session_id);
-       session_start();
-      
-       # get the desired session data
-       $session_data = $_SESSION;
-      
-       # close this session, switch back to the original handler, then restart the old session
-       session_write_close();
-       ini_set('session.save_handler', $old_session_save_handler);
-       session_name($old_session_name);
-       session_id($old_session_id);
-       session_start();
-   }
-  
-   # now return the data we just retrieved
-   return $session_data;
-
 }
