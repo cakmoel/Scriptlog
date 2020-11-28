85304eb3cf4b0a3cca4c6cd0b7fdc25edcdf989a
cakmoel <alanmoehammad@gmail.com>
2020-11-28 23:57:59 +0700

    update admin/posts.php

--- src/admin/posts.php
+++ src/admin/posts.php
@@ -6,107 +6,117 @@ $postDao = new PostDao();
 $postEvent = new PostEvent($postDao, $validator, $sanitizer);
 $postApp = new PostApp($postEvent);
 
-switch ($action) {
+try {
+
+    switch ($action) {
     
-    case ActionConst::NEWPOST:
-        
-        if (false === $authenticator->userAccessControl()) {
-
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            if ((!check_integer($postId)) && (gettype($postId) !== "integer")) {
-
-                header($_SERVER["SERVER_PROTOCOL"]." 403");
-                throw new AppException("Invalid ID data type");
-
-            }
-
-            if ($postId == 0) {
+        case ActionConst::NEWPOST:
             
-                $postApp -> insert();
-                
-            } else {
-
-                direct_page('index.php?load=dashboard', 302);
-                
-            }
-
-        }
-        
-        break;
-        
-    case ActionConst::EDITPOST:
-        
-        if ((!check_integer($postId)) && (gettype($postId) !== "integer")) {
-
-            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-            throw new AppException("Invalid ID data type!");
-
-        }
-
-        if (false === $authenticator->userAccessControl()) {
-
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-        
-        } else {
-
-            if ($postDao -> checkPostId($postId, $sanitizer)) {
-        
-                $postApp -> update((int)$postId);
-                
-            } else {
-                
-               direct_page('index.php?load=posts&error=postNotFound', 404);
-                
-            }
-
-        }
-        
-        break;
-        
-    case ActionConst::DELETEPOST:
-        
-        if ((!check_integer($postId)) && (gettype($postId) !== "integer")) {
-
-            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-            throw new AppException("Invalid ID data type!");
-
-        }
-
-        if (false === $authenticator->userAccessControl()) {
-
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            if ($postDao->checkPostId($postId, $sanitizer)) {
-
-                $postApp -> remove((int)$postId);
-
-            } else {
-
-                direct_page('index.php?load=posts&error=postNotFound', 404);
-
-            }
-                   
-        }
+            if (false === $authenticator->userAccessControl()) {
     
-        break;
-        
-    default:
-        
-        if (false === $authenticator->userAccessControl()) {
-
-             direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            $postApp -> listItems();
-
-        }
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
     
-        break;
+            } else {
+    
+                if ((!check_integer($postId)) && (gettype($postId) !== "integer")) {
+    
+                    header($_SERVER["SERVER_PROTOCOL"]." 403");
+                    throw new AppException("Invalid ID data type");
+    
+                }
+    
+                if ($postId == 0) {
+                
+                    $postApp->insert();
+                    
+                } else {
+    
+                    direct_page('index.php?load=dashboard', 302);
+                    
+                }
+    
+            }
+            
+            break;
+            
+        case ActionConst::EDITPOST:
+            
+            if ((!check_integer($postId)) && (gettype($postId) !== "integer")) {
+    
+                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                throw new AppException("Invalid ID data type!");
+    
+            }
+    
+            if (false === $authenticator->userAccessControl()) {
+    
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+            
+            } else {
+    
+                if ($postDao->checkPostId($postId, $sanitizer)) {
+            
+                    $postApp->update((int)$postId);
+                    
+                } else {
+                    
+                   direct_page('index.php?load=posts&error=postNotFound', 404);
+                    
+                }
+    
+            }
+            
+            break;
+            
+        case ActionConst::DELETEPOST:
+            
+            if ((!check_integer($postId)) && (gettype($postId) !== "integer")) {
+    
+                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                throw new AppException("Invalid ID data type!");
+    
+            }
+    
+            if (false === $authenticator->userAccessControl()) {
+    
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
+            } else {
+    
+                if ($postDao->checkPostId($postId, $sanitizer)) {
+    
+                    $postApp->remove((int)$postId);
+    
+                } else {
+    
+                    direct_page('index.php?load=posts&error=postNotFound', 404);
+    
+                }
+                       
+            }
         
+            break;
+            
+        default:
+            
+            if (false === $authenticator->userAccessControl()) {
+    
+                 direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
+            } else {
+    
+                $postApp->listItems();
+    
+            }
+        
+            break;
+            
+    }
+
+} catch (AppException $e) {
+    
+    LogError::setStatusCode(http_response_code());
+    LogError::newMessage($e);
+    LogError::customErrorMessage('admin');
+    
 }
