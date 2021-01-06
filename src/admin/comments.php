8c182cd3f470c91541b73cca2e65934d113ef594
cakmoel <alanmoehammad@gmail.com>
2021-01-06 12:52:12 +0800

    update admin/comments.php

--- src/admin/comments.php
+++ src/admin/comments.php
@@ -77,6 +77,12 @@ try {
         
 }
 
+} catch (Throwable $th) {
+
+    LogError::setStatusCode(http_response_code());
+    LogError::newMessage($th);
+    LogError::customErrorMessage('admin');
+    
 } catch (AppException $e) {
 
     LogError::setStatusCode(http_response_code());
