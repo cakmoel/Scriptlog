1274083f47d6a0d274583e89a7e6189f83a90914
cakmoel <alanmoehammad@gmail.com>
2021-01-06 11:23:32 +0800

    update pages.php

--- src/admin/pages.php
+++ src/admin/pages.php
@@ -6,107 +6,123 @@ $pageDao = new PageDao();
 $pageEvent = new PageEvent($pageDao, $validator, $sanitizer);
 $pageApp = new PageApp($pageEvent);
 
-switch ($action) {
+try {
+
+    switch ($action) {
     
-    case ActionConst::NEWPAGE:
+        case ActionConst::NEWPAGE:
+        
+            if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
     
-        if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
-
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            if ((!check_integer($pageId)) && (gettype($pageId) !== "integer")) {
-
-                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-                throw new AppException("Invalid ID data type!");
-   
-            }
-
-            if ($pageId == 0) {
-            
-                $pageApp->insert();
-                
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
             } else {
-
-                direct_page('index.php?load=dashboard', 302);
-
-            }
-            
-        }
-        
-        break;
     
-    case ActionConst::EDITPAGE:
-        
-        if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
-
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            if ((!check_integer($pageId)) && (gettype($pageId) !== "integer")) {
-
-                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-                throw new AppException("Invalid ID data type!");
-   
-            }
-
-            if ($pageDao->checkPageId($pageId, $sanitizer)) {
-            
-                $pageApp->update((int)$pageId);
+                if ((!check_integer($pageId)) && (gettype($pageId) !== "integer")) {
+    
+                    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                    throw new AppException("Invalid ID data type!");
+       
+                }
+    
+                if ($pageId == 0) {
                 
-            } else {
+                    $pageApp->insert();
+                    
+                } else {
+    
+                    direct_page('index.php?load=dashboard', 302);
+    
+                }
                 
-                direct_page('index.php?load=pages&error=pageNotFound', 404);
-    
             }
             
-        }
+            break;
         
-        break;
-        
-    case ActionConst::DELETEPAGE:
-        
-        if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
-
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            if ((!check_integer($pageId)) && (gettype($pageId) !== "integer")) {
-
-                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-                throw new AppException("Invalid ID data type");
-
-            }
-
-            if ($pageDao->checkPageId($pageId, $sanitizer)) {
-
-                $pageApp->remove((int)$pageId);
-
+        case ActionConst::EDITPAGE:
+            
+            if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
+    
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
             } else {
-
-                direct_page('index.php?load=pages&error=pageNotFound', 404);
-
+    
+                if ((!check_integer($pageId)) && (gettype($pageId) !== "integer")) {
+    
+                    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                    throw new AppException("Invalid ID data type!");
+       
+                }
+    
+                if ($pageDao->checkPageId($pageId, $sanitizer)) {
+                
+                    $pageApp->update((int)$pageId);
+                    
+                } else {
+                    
+                    direct_page('index.php?load=pages&error=pageNotFound', 404);
+        
+                }
+                
             }
             
-        }
-        
-        break;
-    
-    default:
-         
-        if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
+            break;
             
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-
-        } else {
-
-            $pageApp->listItems();
-
-        }
-        
-    break;
+        case ActionConst::DELETEPAGE:
+            
+            if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
     
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
+            } else {
+    
+                if ((!check_integer($pageId)) && (gettype($pageId) !== "integer")) {
+    
+                    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+                    throw new AppException("Invalid ID data type");
+    
+                }
+    
+                if ($pageDao->checkPageId($pageId, $sanitizer)) {
+    
+                    $pageApp->remove((int)$pageId);
+    
+                } else {
+    
+                    direct_page('index.php?load=pages&error=pageNotFound', 404);
+    
+                }
+                
+            }
+            
+            break;
+        
+        default:
+             
+            if (false === $authenticator->userAccessControl(ActionConst::PAGES)) {
+                
+                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
+    
+            } else {
+    
+                $pageApp->listItems();
+    
+            }
+            
+        break;
+        
+    }
+
+} catch (Throwable $th) {
+    
+    LogError::setStatusCode(http_response_code());
+    LogError::newMessage($th);
+    LogError::customErrorMessage('admin');
+
+} catch (AppException $e) {
+
+    LogError::setStatusCode(http_response_code());
+    LogError::newMessage($e);
+    LogError::customErrorMessage('admin');
+
 }
