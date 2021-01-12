c06f45f1e4f801e7a8e16753c597e393e3235562
cakmoel <alanmoehammad@gmail.com>
2021-01-12 18:53:07 +0800

    revamping admin/request.php

--- src/admin/request.php
+++ src/admin/request.php
@@ -3,21 +3,29 @@
 $load = null;
 $current_request = current_request_method();
 $method_allowed = ['GET', 'POST'];
+$protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : "";
 
 try {
 
+   if (! in_array($protocol, ['HTTP/1.1', 'HTTP/2', 'HTTP/2.0'], true)) {
+
+    $protocol = 'HTTP/1.0';
+
+   }
+
    if ((isset($_GET['load'])) || (array_key_exists('load', $_GET))) {
      
-        $load = is_array($_GET['load']) ? implode($_GET['load']) : $_GET['load'];
+        $load = is_array($_GET['load']) ? implode('', $_GET['load']) : $_GET['load'];
         $load = filter_var($load, FILTER_VALIDATE_URL, ['flags' => FILTER_FLAG_QUERY_REQUIRED]);
         $load = filter_input(INPUT_GET, 'load', FILTER_SANITIZE_STRING);
         $load = str_replace(chr(0), '', $load);
+        $load = preg_replace( '/[^a-z0-9,_-]+/i', '', $load );
         $load = htmlspecialchars(strtolower($load), ENT_QUOTES|ENT_SUBSTITUTE, 'UTF-8');
        
         // checking if the string contains parent directory
         if (strpos($load, '..')) {
 
-            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            header($protocol." 400 Bad Request", true, 400);
             throw new AppException("Directory traversal attempt!");
             
         }
@@ -25,14 +33,14 @@ try {
         // checking remote file inclusions
         if ((strstr($load, '../') !== false) || (strstr($load, 'file://') !== false) || (strstr($load, 'http://') !== false)) {
             
-            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            header($protocol." 400 Bad Request", true, 400);
             throw new AppException("Remote file inclusion attempt!");
             
         }
     
         if ((strstr($load, 'php://input')) || (strstr($load, 'php://filter')) || (strstr($load, 'data:')) || (strstr($load, 'zip://'))) {
 
-            header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+            header($protocol." 400 Bad Request", true, 400);
             throw new AppException("Remote file inclusion attempt!");
 
         }
@@ -40,7 +48,7 @@ try {
         if ((!is_readable(dirname(dirname(__FILE__)) .DS. APP_ADMIN .DS."{$load}.php")) 
             || (empty($load)) || (!in_array($load, array_keys(admin_query())))) {
 
-            header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+            header($protocol." 404 Not Found", true, 400);
             throw new AppException(" 404 Page requested not found");
 
         } else {
