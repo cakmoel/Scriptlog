4d162c60353937fc2c193890444652d2ca55eafd
M.Noermoehammad <alanmoehammad@gmail.com>
2021-01-30 03:43:20 +0800

    update admin/reset-password

--- src/admin/reset-password.php
+++ src/admin/reset-password.php
@@ -218,5 +218,8 @@ document.onkeydown=function(e){if(e.ctrlKey&&(e.keyCode===67||e.keyCode===86||e.
 else
 {return true;}});
 </script>
+<script>
+$(document).keydown(function(event){if(event.keyCode==123){return false}else if(event.ctrlKey&&event.shiftKey&&event.keyCode==73){return false}});
+</script>
 </body>
 </html>
