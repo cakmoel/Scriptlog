f7dab1b3a53e07830cf874c705dfce691f3042e3
M.Noermoehammad <alanmoehammad@gmail.com>
2021-02-08 22:05:51 +0800

    revamping admin/login-layout.php

--- src/admin/login-layout.php
+++ src/admin/login-layout.php
@@ -24,19 +24,19 @@ function login_header($stylePath)
   <!-- iCheck -->
   <link rel="stylesheet" href="<?=$stylePath; ?>/assets/components/iCheck/square/blue.css">
   
-<link rel="apple-touch-icon" sizes="57x57" href="<?= $stylePath; ?>/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?= $stylePath; ?>/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?= $stylePath; ?>/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?= $stylePath; ?>/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?= $stylePath; ?>/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?= $stylePath; ?>/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?= $stylePath; ?>/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?= $stylePath; ?>/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?= $stylePath; ?>/apple-icon-180x180.png">
+<link rel="apple-touch-icon" sizes="57x57" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-180x180.png">
 <link rel="icon" type="image/png" sizes="192x192"  href="<?= $stylePath; ?>/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?= $stylePath; ?>/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="<?= $stylePath; ?>/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?= $stylePath; ?>/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= $stylePath; ?>/assets/dist/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?= $stylePath; ?>/assets/dist/img/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= $stylePath; ?>/assets/dist/img/favicon-16x16.png">
 <link rel="manifest" href="<?= $stylePath; ?>/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="<?= $stylePath; ?>/ms-icon-144x144.png">
