100fabe63df4f5e3a78e45038cce16cc4240c004
M.Noermoehammad <alanmoehammad@gmail.com>
2021-04-11 18:28:09 +0800

    revamping admin/option-general.php

--- src/admin/option-general.php
+++ src/admin/option-general.php
@@ -58,13 +58,11 @@ try {
 } catch (Throwable $th) {
     
     LogError::setStatusCode(http_response_code());
-    LogError::newMessage($th);
-    LogError::customErrorMessage($th);
+    LogError::exceptionHandler($th);
 
 } catch (AppException $e) {
 
     LogError::setStatusCode(http_response_code());
-    LogError::newMessage($e);
-    LogError::customErrorMessage('admin');
+    LogError::exceptionHandler($e);
     
 }
