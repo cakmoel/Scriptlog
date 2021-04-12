4fbb1f55fd0f1de7801b4066101383f7f10509a0
M.Noermoehammad <alanmoehammad@gmail.com>
2021-04-12 14:55:39 +0800

    revamping Session Exception

---
+++ src/lib/core/ISessionThrowable.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * interface ISessionThrowable extends IThrowable
+ * 
+ * @category Core Class
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * 
+ */
+interface ISessionThrowable extends IThrowable
+{
+
+}
