dffe7d1656e1ee5847a6f630ca857faf41c98a0b
M.Noermoehammad <alanmoehammad@gmail.com>
2021-04-22 21:10:49 +0800

    updating lib/utility/theme-meta.php

--- src/lib/utility/theme-meta.php
+++ src/lib/utility/theme-meta.php
@@ -1,81 +1,141 @@
 <?php
 /**
- * Theme Meta Function 
+ * theme_meta
+ * 
  * Display meta tag, link tag, title tag based on client request
- *  
+ *
+ * @category Function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
+ * 
  */
 function theme_meta()
 {
+
+  $uri = new RequestPath();
   
-  $match = find_request(0);
-  $param1 = find_request(1);
-  $param2 = find_request(2);
+  $theme_meta = array();
 
-  $meta_title = (!empty(app_info()['site_name'])) ? app_info()['site_name'] : $_SERVER['REQUEST_URI'];
-  $theme_dir = theme_dir();
-
-  switch ($match) {
+  switch ($uri->param1) {
 
       case 'post':
- 
-        meta_tag();
-        title_tag($param2);
-          
+
+        if (!empty($uri->param2)) {
+
+          $read_post = FrontHelper::frontPostById($uri->param2);
+
+          $post_title = (!empty($read_post['post_title'])) ? escape_html($read_post['post_title']) : null;
+           
+        }
+        
+        $theme_meta['site_title'] = (is_null($post_title)) ? title_tag(app_info()['site_name']) : title_tag(ucfirst(trim($post_title)) . ' &#8211; ' . app_info()['site_name']);
+        
+        $theme_meta['meta_tag'] = meta_tag();
+
         break;
 
       case 'page':
 
-        meta_tag();
-        title_tag($param1);
+        if (!empty($uri->param2)) {
+
+           $read_page = FrontHelper::frontPageBySlug($uri->param2);
+
+           $page_title = (!empty($read_page['post_title'])) ? escape_html($read_page['post_title']) : null;
+
+        }
+
+        $theme_meta['site_title'] = (is_null($page_title)) ? title_tag(escape_html(app_info()['site_name'])) : title_tag(ucfirst(trim($page_title)) . ' &#8211; ' . escape_html(app_info()['site_name']));
+        
+        $theme_meta['meta_tag'] = meta_tag();
 
         break;
 
       case 'blog':
 
-        meta_tag();
-        title_tag("Blog");
-          
-        break;
-      
-      case 'category':
-          
-        meta_tag();
-        title_tag($match.' | '.$param1);
-          
+        if (empty($uri->param2) && empty($uri->param3)) {
+
+          $theme_meta['site_title'] = title_tag($uri->param1 . ' &#8211; ' . app_info()['site_name']);
+           
+        }
+        
+        $theme_meta['meta_tag'] = meta_tag();
+
         break;
 
-       default:
-          
-          meta_tag();
-          title_tag($meta_title);
-          
-          break;
+      case 'category':
+
+        if (!empty($uri->param2)) {
+
+          $read_topic = FrontHelper::frontTopicBySlug($uri->param2);
+
+          $topic_title = (!empty($read_topic['topic_title'])) ? escape_html($read_topic['topic']) : null;
+
+        }
+
+        $theme_meta['site_title'] = (is_null($topic_title)) ? title_tag(escape_html(app_info()['site_name'])) : title_tag(ucfirst(trim($topic_title)) . ' &#8211; ' . escape_html(app_info()['site_name']));
+
+        $theme_meta['meta_tag'] = meta_tag();
+
+        break;
+
+      case 'archive':
+
+        break;
+
+      default:
+
+        $meta_title = (!empty(app_info()['site_name'])) ? app_info()['site_name']: "";
+
+        $theme_meta['site_title'] = title_tag($meta_title);
+        $theme_meta['meta_tag'] = meta_tag();
+
+        break;
 
   }
-  
+
+  return array('site_title' => $theme_meta['site_title'], 'meta_tag' => $theme_meta['meta_tag']);
+
 }
 
-// function title tag
+/**
+ * title_tag
+ *
+ * @param string $title
+ * @return void
+ * 
+ */
 function title_tag($title)
 {
-    print<<<_HTML_
 
- <title>$title</title>
+$tag_title = <<<_TITLE
 
-_HTML_;
+<title>$title</title>
+
+_TITLE;
+
+return $tag_title;
 
 }
 
-// function tag meta
+// function meta_tag
 function meta_tag()
 {
-  print<<<_HTML_
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="robots" content="all,follow">
+$meta_title = app_info()['site_name'];
+$meta_desc = app_info()['site_description'];
+$meta_key = app_info()['site_keywords'];
+$meta_tagline = app_info()['site_tagline'];
 
-_HTML_;
+$meta_tag = <<<_META
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="$meta_desc">
+<meta name="keywords" content="$meta_key">
+
+_META;
+
+return $meta_tag;
 
 }
