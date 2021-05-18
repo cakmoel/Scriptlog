d260b077c7acd46ad800c502b022822c7db3bc95
M.Noermoehammad <alanmoehammad@gmail.com>
2021-05-18 22:26:46 +0800

    updating lib/core/IAppThrowable

--- src/lib/core/IAppThrowable.php
+++ src/lib/core/IAppThrowable.php
@@ -1,8 +1,9 @@
-<?php
+<?php defined('SCRIPTLOG') || die("Direct access not permitted");
 /**
  * interface IAppThrowable extends IThrowable
  * 
  * @category Core Class
+ * @author M.Noermoehammad
  * 
  */
 interface IAppThrowable extends IThrowable 
