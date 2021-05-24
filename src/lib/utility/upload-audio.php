02229cc2ee36f7458f2fa96a1311718424c13ab1
M.Noermoehammad <alanmoehammad@gmail.com>
2021-05-24 21:32:34 +0800

    updating lib/utility/upload-audio

--- src/lib/utility/upload-audio.php
+++ src/lib/utility/upload-audio.php
@@ -7,21 +7,18 @@
  * @param string $file_type
  * @param string $file_name
  * @category function
- * @return void
+ * @return bool
+ * 
  */
-function upload_audio($file_location, $file_size, $file_type, $file_name)
+function upload_audio($file_location, $file_name)
 {
   
-  $audio_path = __DIR__ . '/../../public/files/audio/';
+  $audio_path = __DIR__ . '/../../'. APP_AUDIO;
   $audio_uploaded = $audio_path . $file_name;
+  
+  if (!move_uploaded_file($file_location, $audio_uploaded)) {
 
-  if($file_size > APP_FILE_SIZE) {
-
-    throw new UploadException("Error - File size too big!");
-
-  } else {
-
-    move_uploaded_file($file_location, $audio_uploaded);
+   scriptlog_error("Audio file uploaded failure");
 
   }
   
