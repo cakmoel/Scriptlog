d54a0543e822cb3d0799eb95d1b303478f9e63bd
M.Noermoehammad <alanmoehammad@gmail.com>
2021-07-05 13:41:57 +0700

    updating admin/ui/setting/permalink-setting

--- src/admin/ui/setting/permalink-setting.php
+++ src/admin/ui/setting/permalink-setting.php
@@ -5,7 +5,7 @@
     <section class="content-header">
       <h1>
         <?=(isset($pageTitle)) ? $pageTitle : ""; ?>
-        <small></small>
+        <small>Control Panel</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="index.php?load=dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
