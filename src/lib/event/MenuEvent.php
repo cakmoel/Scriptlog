a03d39599f85919663b35891c8ba0d6d8c7ff845
M.Noermoehammad <alanmoehammad@gmail.com>
2021-07-08 22:14:15 +0700

    updating lib/event/MenuEvent

--- src/lib/event/MenuEvent.php
+++ src/lib/event/MenuEvent.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('SCRIPTLOG') || die("Direct access not permitted");
 /**
  * Menu Event Class
  * 
@@ -11,22 +11,84 @@
  */
 class MenuEvent
 {
+  /**
+   * menu_id
+   *
+   * @var int
+   * 
+   */
   private $menu_id;
   
+  /**
+   * parent_id
+   *
+   * @var int
+   * 
+   */
   private $parent_id;
   
+  /**
+   * label
+   *
+   * @var string
+   * 
+   */
   private $label;
   
+  /**
+   * link
+   *
+   * @var string
+   * 
+   */
   private $link;
   
+  /**
+   * order
+   *
+   * @var int|string
+   * 
+   */
   private $order;
   
+  /**
+   * status
+   *
+   * @var string
+   * 
+   */
   private $status;
 
+  /**
+   * position
+   *
+   * @var string
+   * 
+   */
+  private $position;
+
+  /**
+   * menuDao
+   *
+   * @var object
+   * 
+   */
   private $menuDao;
 
+  /**
+   * validator
+   *
+   * @var object
+   * 
+   */
   private $validator;
 
+  /**
+   * sanitize
+   *
+   * @var object
+   * 
+   */
   private $sanitize;
  
   public function __construct(MenuDao $menuDao, FormValidator $validator, Sanitize $sanitize)
@@ -36,51 +98,132 @@ class MenuEvent
     $this->sanitize = $sanitize;
   }
 
+  /**
+   * setMenuId
+   *
+   * @param int $menu_id
+   * @return int
+   * 
+   */
   public function setMenuId($menu_id)
   {
     $this->menu_id = $menu_id;
   }
 
+  /**
+   * setParentId
+   *
+   * @param int $parent_id
+   * @return int
+   * 
+   */
   public function setParentId($parent_id)
   {
     $this->parent_id = $parent_id;
   }
 
+  /**
+   * setMenuLabel
+   *
+   * @param string $menu_label
+   * @return int
+   * 
+   */
   public function setMenuLabel($menu_label)
   {
     $this->label = $menu_label;
   }
 
+  /**
+   * setMenuLink
+   *
+   * @param string $menu_link
+   * @return string
+   * 
+   */
   public function setMenuLink($menu_link)
   {
     $this->link = $menu_link;
   }
 
+  /**
+   * setMenuPosition
+   *
+   * @param string $menu_position
+   * @return string
+   * 
+   */
+  public function setMenuPosition($menu_position)
+  {
+    $this->position = $menu_position;
+  }
+
+  /**
+   * setMenuOrder
+   *
+   * @param string|int $menu_order
+   * @return int|string
+   * 
+   */
   public function setMenuOrder($menu_order)
   {
     $this->order = $menu_order;
   }
 
+  /**
+   * setMenuStatus
+   * 
+   * @param string $menu_status
+   * @return string
+   * 
+   */
   public function setMenuStatus($menu_status)
   {
     $this->status = $menu_status;
   }
 
+  /**
+   * grabMenus
+   *
+   * @param string $orderBy
+   * @return mixed
+   * 
+   */
   public function grabMenus($orderBy = "ID")
   {
     return $this->menuDao->findMenus($orderBy);
   }
 
+  /**
+   * grabMenu
+   *
+   * @param int|string $id
+   * @return mixed
+   * 
+   */
   public function grabMenu($id)
   {
     return $this->menuDao->findMenu($id, $this->sanitize);
   }
 
+  /**
+   * grabMenuParent
+   *
+   * @param int|string $parent_id
+   * @return mixed
+   * 
+   */
   public function grabMenuParent($parent_id)
   {
     return $this->menuDao->findMenuParent($parent_id, $this->sanitize);
   }
 
+  /**
+   * addMenu
+   *
+   * @return mixed
+   * 
+   */
   public function addMenu()
   {
     
@@ -100,6 +243,10 @@ class MenuEvent
 
   }
 
+  /**
+   * modifyMenu
+   *
+   */
   public function modifyMenu()
   {
     $this->validator->sanitize($this->parent_id, 'int');
@@ -113,17 +260,24 @@ class MenuEvent
           'menu_label' => $this->label,
           'menu_link' => $this->link,
           'menu_sort' => $this->order,
-          'menu_status' => $this->status
+          'menu_status' => $this->status,
+          'menu_position' => $this->position
     
         ], $this->menu_id);
 
   }
 
+  /**
+   * enableMenu()
+   * 
+   */
   public function enableMenu()
   {
     $this->validator->sanitize($this->menu_id, 'int');
 
-    if (!$data_menu = $this->menuDao->findMenu($this->menu_id, $this->sanitize)) {
+    $menu_enabled = $this->menuDao->findMenu($this->menu_id, $this->sanitize);
+    
+    if (! $menu_enabled ) {
        direct_page('index.php?load=menu&error=menuNotFound', 404);
     }
 
@@ -131,11 +285,16 @@ class MenuEvent
 
   }
 
+  /**
+   * disableMenu()
+   *
+   */
   public function disableMenu()
   {
     $this->validator->sanitize($this->menu_id, $this->sanitize);
 
-    if (!$data_menu = $this->menuDao->findMenu($this->menu_id, $this->sanitize)) {
+    $menu_disabled = $this->menuDao->findMenu($this->menu_id, $this->sanitize);
+    if (! $menu_disabled) {
       direct_page('index.php?load=menu&error=menuNotFound', 404);
     }
 
@@ -143,6 +302,10 @@ class MenuEvent
     
   }
 
+  /**
+   * removeMenu()
+   * 
+   */
   public function removeMenu()
   {
     $this->validator->sanitize($this->menu_id, 'int');
@@ -154,17 +317,46 @@ class MenuEvent
     return $this->menuDao->deleteMenu($this->menu_id, $this->sanitize);
 
   }
-  
+
+  /**
+   * parentDropDown
+   *
+   * @param string $selected
+   * 
+   */
   public function parentDropDown($selected = "")
   {
     return $this->menuDao->dropDownMenuParent($selected);
   }
 
+  /**
+   * positionDropDown()
+   * 
+   * @param string $selected
+   * 
+   */
+  public function positionDropDown($selected = "")
+  {
+    return $this->menuDao->dropDownMenuPosition($selected);
+  }
+
+  /**
+   * isMenuExists
+   *
+   * @param string $menu_label
+   * 
+   */
   public function isMenuExists($menu_label)
   {
     return $this->menuDao->menuExists($menu_label);
   }
 
+  /**
+   * totalMenus
+   *
+   * @param mixed|array $data
+   *  
+   */
   public function totalMenus($data = null)
   {
     return $this->menuDao->totalMenuRecords($data);
