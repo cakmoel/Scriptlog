175c4577a9bfba24ca067dd281a22366503dfb22
M.Noermoehammad <alanmoehammad@gmail.com>
2021-08-05 12:57:16 +0700

    append function validate-date
    function for date string validation

---
+++ src/lib/utility/validate-date.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * validate_date()
+ *
+ * date & time validator for all format
+ * 
+ * @see https://www.php.net/manual/en/function.checkdate.php#113205
+ * @see https://www.codexworld.com/how-to/validate-date-input-string-in-php/
+ * @see https://stackoverflow.com/questions/19271381/correctly-determine-if-date-string-is-a-valid-date-in-that-format
+ * @param string $date
+ * @param string $format
+ * @return bool
+ * 
+ */
+function validate_date($date, $format = 'm/d/Y')
+{
+
+$d = DateTime::createFromFormat($format, $date);
+
+return $d && $d->format($format) === $date;
+
+}
