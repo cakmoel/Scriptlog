f58667612b156df353380f9842b6ddc74245afd5
M.Noermoehammad <alanmoehammad@gmail.com>
2021-10-10 23:43:43 +0700

    revamping admin/login-layout

--- src/admin/login-layout.php
+++ src/admin/login-layout.php
@@ -33,7 +33,7 @@ function login_header($stylePath)
 <link rel="apple-touch-icon" sizes="144x144" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="<?= $stylePath; ?>/assets/dist/img/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?= $stylePath; ?>/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?= $stylePath; ?>/assets/dist/img/android-icon-192x192.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?= $stylePath; ?>/assets/dist/img/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="96x96" href="<?= $stylePath; ?>/assets/dist/img/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?= $stylePath; ?>/assets/dist/img/favicon-16x16.png">
