84c0b7a62814b7d0f6211c730a10bec39d8d27c0
M.Noermoehammad <alanmoehammad@gmail.com>
2021-12-12 16:59:29 +0700

    updating admin/ui/users/all-users

--- src/admin/ui/users/all-users.php
+++ src/admin/ui/users/all-users.php
@@ -29,11 +29,13 @@
          <div class="alert alert-danger alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 <h4><i class="icon fa fa-ban"></i> Error!</h4>
+           
            <?php 
               foreach ($errors as $e) :
                 echo $e;
               endforeach;
            ?>
+
           </div>
          <?php 
          endif;
