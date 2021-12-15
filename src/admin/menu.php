d5ccf714541a78d7b5e620ca32c95b521af1ba6f
M.Noermoehammad <alanmoehammad@gmail.com>
2021-12-15 09:45:23 +0700

    updating admin/menu

--- src/admin/menu.php
+++ src/admin/menu.php
@@ -62,7 +62,7 @@ try {
         
                 } else {
         
-                    direct_page('index.php?load=menu&error=menuNotFound', 404);
+                    direct_page('index.php?load=404&notfound='.notfound_id(), 404);
         
                 }
 
@@ -92,7 +92,7 @@ try {
     
                 } else {
     
-                    direct_page('index.php?load=menu&error=menuNotFound', 404);
+                    direct_page('index.php?load=404&notfound='.notfound_id(), 404);
     
                 }
 
