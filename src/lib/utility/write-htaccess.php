43217754a148a2c1d57aa81096a2b87787e423f8
M.Noermoehammad <alanmoehammad@gmail.com>
2021-12-19 22:24:05 +0700

    updating lib/utility/write-htaccess

--- src/lib/utility/write-htaccess.php
+++ src/lib/utility/write-htaccess.php
@@ -19,7 +19,7 @@ function write_htaccess($permalink_status, $user_level, $data)
 
 $privilege = (isset($user_level) ) ? Session::getInstance()->scriptlog_session_level : "";
 
-if ( $privilege == 'administrator' ) {
+if ( $privilege == 'administrator' || $privilege == 'manager' ) {
 
    $fp = fopen(__DIR__ . '/../../.htaccess', 'w');
 
@@ -33,13 +33,13 @@ if ( $privilege == 'administrator' ) {
       sleep(5);
       fwrite($fp, $data);
       fclose($fp);
-   
+
    }
 
 } else {
 
-   scriptlog_error("Privilege is not compatible to perform this action");
-
+ scriptlog_error("Privilege is not compatible to perform this action");
+ 
 }
 
 }
@@ -56,41 +56,39 @@ if ( $privilege == 'administrator' ) {
 function read_htaccess_config($permalink_status)
 {
 
-$content  = '# BEGIN ScriptLog'. PHP_EOL;
-$content .= '<IfModule mod_rewrite.c>' . PHP_EOL;
-$content .= 'RewriteEngine On'. PHP_EOL; 
-$content .= '#RewriteCond %{HTTP_USER_AGENT} libwww-perl.* ' . PHP_EOL;
-$content .= '#RewriteRule .* – [F,L]'. PHP_EOL;
-$content .= '# The RewriteBase of the system (change if you are using this system in a sub-folder)' . PHP_EOL;
-$content .= '#RewriteBase /'. PHP_EOL;
-$content .= '# This will make the site only accessible without the -www- ' . PHP_EOL; 
-$content .= '#RewriteCond %{HTTP_HOST} ^www\.yourdomain\.TLD$'. PHP_EOL;
-$content .= '#RewriteRule ^/?$ "https\:\/\/yourdomain\.TLD\/" [R=301, L]' . PHP_EOL;
+$content  = '# START ScriptLog'. PHP_EOL;
+$content .= '# The directives (lines) between "START ScriptLog" and "FINISH ScriptLog" are' . PHP_EOL;
+$content .= '# dynamically generated, and should only be modified if you know what you are doing.' . PHP_EOL;
+$content .= '# Any changes to these lines will be overwritten by scriptlog herself on setting permalink.' . PHP_EOL;
    
 if ( $permalink_status == 'yes') {
 
+   $content .= '<IfModule mod_rewrite.c>' . PHP_EOL;
+   $content .= 'RewriteEngine On'. PHP_EOL; 
+   $content .= '#RewriteCond %{HTTP_USER_AGENT} libwww-perl.* ' . PHP_EOL;
+   $content .= '#RewriteRule .* – [F,L]'. PHP_EOL;
+   $content .= '# The RewriteBase of the system (change if you are using this system in a sub-folder)' . PHP_EOL;
+   $content .= '#RewriteBase /'. PHP_EOL;
+   $content .= '# This will make the site only accessible without the -www- ' . PHP_EOL; 
+   $content .= '#RewriteCond %{HTTP_HOST} ^www\.yourdomain\.TLD$'. PHP_EOL;
+   $content .= '#RewriteRule ^/?$ "https\:\/\/yourdomain\.TLD\/" [R=301, L]' . PHP_EOL;
    $content .= '# Ensure all front-end UI-UX files readable'. PHP_EOL;
    $content .= 'RewriteCond %{REQUEST_FILENAME} !\.(ico|css|png|jpg|jpeg|webp|gif|js|txt|htm|html|eot|svg|ttf|woff|woff2|webm|ogg|mp4|wav|mp3|pdf)$ [NC]' . PHP_EOL;
    $content .= 'RewriteRule ^public/.*$ index.php'. PHP_EOL;
    $content .= 'RewriteCond %{REQUEST_FILENAME} !-d ' . PHP_EOL;
    $content .= 'RewriteCond %{REQUEST_FILENAME} !-f ' . PHP_EOL;
    $content .= 'RewriteCond %{REQUEST_FILENAME} !-l ' . PHP_EOL; 
-   $content .= 'RewriteRule ^(.*)$ index.php [L,QSA]' . PHP_EOL;
-      
+   $content .= 'RewriteRule ^(.*)$ index.php [QSA,L]' . PHP_EOL;
+   $content .= '</IfModule>'. PHP_EOL;
+
 } else {
       
-   $content .= '# Ensure all front-end UI-UX files readable'. PHP_EOL;
-   $content .= 'RewriteCond %{REQUEST_FILENAME} !\.(ico|css|png|jpg|jpeg|webp|gif|js|txt|htm|html|eot|svg|ttf|woff|woff2|webm|ogg|mp4|wav|mp3|pdf)$ [NC]' . PHP_EOL;
-   $content .= 'RewriteRule ^public/.*$ index.php'. PHP_EOL;
-   $content .= 'RewriteCond %{REQUEST_FILENAME} !-d ' . PHP_EOL;
-   $content .= 'RewriteCond %{REQUEST_FILENAME} !-f ' . PHP_EOL;
-   $content .= 'RewriteCond %{REQUEST_FILENAME} !-l ' . PHP_EOL; 
-   $content .= 'RewriteRule ^(.*)$ index.php/$1 [L]'  . PHP_EOL;
+   $content .= ''.PHP_EOL;
+   $content .= ''.PHP_EOL;
       
 }
 
-$content .= '</IfModule>'. PHP_EOL;
-$content .= '# END ScriptLog' . PHP_EOL;
+$content .= '# FINISH ScriptLog' . PHP_EOL;
 
 return $content;
 
