f97b9f0658410c63be3f9dba0f4be6b4ec31cc61
M.Noermoehammad <alanmoehammad@gmail.com>
2022-05-03 23:41:48 +0700

    updating lib/dao/PageDao

--- src/lib/dao/PageDao.php
+++ src/lib/dao/PageDao.php
@@ -125,36 +125,6 @@ public function findPageById($pageId, $sanitize)
 }
 
 /**
- * Find page by slug title
- * 
- * @param string $slug
- * @return boolean|array|object
- * 
- */
-public function findPageBySlug($slug, $sanitize)
-{
-	$sql = "SELECT p.ID, p.media_id, p.post_author, p.post_date, p.post_modified, p.post_title, p.post_slug,
-    p.post_content, p.post_summary, p.post_keyword, p.post_tags, p.post_status, p.post_sticky, 
-	p.post_type, p.comment_status,
-    m.ID, m.media_filename, m.media_caption, m.media_access, u.ID, u.user_fullname
-    FROM tbl_posts AS p
-    INNER JOIN tbl_media AS m ON p.media_id = m.ID
-    INNER JOIN tbl_users AS u ON p.post_author = u.ID
-    WHERE p.post_slug = :slug 
-	AND p.post_status = 'publish' AND p.post_type = 'page' 
-	AND m.media_access = 'public' AND m.media_status = '1'";
-	
-	$this->setSQL($sql);
-	
-	$slug_sanitized = $this->filteringId($sanitize, $slug, 'xss');
-
-    $pageBySlug = $this->findRow([':slug' => $slug_sanitized]);
-    
-    return (empty($pageBySlug)) ?: $pageBySlug;
-    
-}
-
-/**
  * createPage()
  * 
  * Insert new page record
