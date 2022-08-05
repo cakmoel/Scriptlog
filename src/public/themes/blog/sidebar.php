7be738455cda51160a2bcc070d7a866ca0170a45
M.Noermoehammad <alanmoehammad@gmail.com>
2022-08-06 05:46:43 +0700

    update public/themes/blog/sidebar.php

--- src/public/themes/blog/sidebar.php
+++ src/public/themes/blog/sidebar.php
@@ -1,11 +1,13 @@
 <aside class="col-lg-4">
           
-          <!-- Widget [Latest Posts Widget]        -->
+          <!-- Widget [Latest Posts Widget] -->
           <div class="widget latest-posts">
             <header>
               <h3 class="h6">Latest Posts</h3>
             </header>
-            <div class="blog-posts"><a href="#">
+            <div class="blog-posts">
+               
+             <a href="#">
                 <div class="item d-flex align-items-center">
                   <div class="image"><img src="img/small-thumbnail-1.jpg" alt="..." class="img-fluid"></div>
                   <div class="title"><strong>Alberto Savoia Can Teach You About</strong>
@@ -14,7 +16,10 @@
                       <div class="comments"><i class="icon-comment"></i>12</div>
                     </div>
                   </div>
-                </div></a><a href="#">
+                </div>
+              </a>
+                
+              <a href="#">
                 <div class="item d-flex align-items-center">
                   <div class="image"><img src="img/small-thumbnail-2.jpg" alt="..." class="img-fluid"></div>
                   <div class="title"><strong>Alberto Savoia Can Teach You About</strong>
@@ -23,7 +28,10 @@
                       <div class="comments"><i class="icon-comment"></i>12</div>
                     </div>
                   </div>
-                </div></a><a href="#">
+                </div>
+              </a>
+                
+              <a href="#">
                 <div class="item d-flex align-items-center">
                   <div class="image"><img src="img/small-thumbnail-3.jpg" alt="..." class="img-fluid"></div>
                   <div class="title"><strong>Alberto Savoia Can Teach You About</strong>
@@ -32,7 +40,11 @@
                       <div class="comments"><i class="icon-comment"></i>12</div>
                     </div>
                   </div>
-                </div></a></div>
+                </div>
+              </a>
+              
+            </div>
+          
           </div>
           <!-- Widget [Categories Widget]-->
           <div class="widget categories">
@@ -51,11 +63,7 @@
               <h3 class="h6">Tags</h3>
             </header>
             <ul class="list-inline">
-              <li class="list-inline-item"><a href="#" class="tag">#Business</a></li>
-              <li class="list-inline-item"><a href="#" class="tag">#Technology</a></li>
-              <li class="list-inline-item"><a href="#" class="tag">#Fashion</a></li>
-              <li class="list-inline-item"><a href="#" class="tag">#Sports</a></li>
-              <li class="list-inline-item"><a href="#" class="tag">#Economy</a></li>
+              <?= outputting_tags(); ?>
             </ul>
           </div>
         </aside>
