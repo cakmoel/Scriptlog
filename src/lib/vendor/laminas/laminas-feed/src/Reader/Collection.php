4f90adfc1d1e52acd243aff1cbbae3b8693a6c78
M.Noermoehammad <alanmoehammad@gmail.com>
2022-09-12 18:27:07 +0700

    update third-party vendor

---
+++ src/lib/vendor/laminas/laminas-feed/src/Reader/Collection.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Feed\Reader;
+
+use ArrayObject;
+
+/**
+ * @deprecated This class is deprecated. Use the concrete collection classes
+ *     \Laminas\Feed\Reader\Collection\Author and \Laminas\Feed\Reader\Collection\Category
+ *     or the generic class \Laminas\Feed\Reader\Collection\Collection instead.
+ */
+class Collection extends ArrayObject
+{
+}
---
+++ src/lib/vendor/laminas/laminas-feed/src/Reader/Collection/AbstractCollection.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Feed\Reader\Collection;
+
+use ArrayObject;
+
+abstract class AbstractCollection extends ArrayObject
+{
+    /**
+     * Return a simple array of the most relevant slice of
+     * the collection values. For example, feed categories contain
+     * the category name, domain/URI, and other data. This method would
+     * merely return the most useful data - i.e. the category names.
+     *
+     * @return array
+     */
+    abstract public function getValues();
+}
