d2fb68bec7d9a38e325d2ad998b5889fc2c9b404
M.Noermoehammad <alanmoehammad@gmail.com>
2022-09-20 15:56:49 +0700

    revamped comments-post.php

--- src/comments-post.php
+++ src/comments-post.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/lib/main.php';
 
-if ( isset($_POST['SubmitComment']) ) {
+if ( isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] === 'POST') {
 
 processing_comment($_POST);
 
