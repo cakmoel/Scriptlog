0ccf3ff18c054ba52a5547d9b55874b6f3a4bfad
M.Noermoehammad <alanmoehammad@gmail.com>
2022-09-20 16:01:08 +0700

    Revert "updating third-party and revamp lib"
    This reverts commit d0922e0f9b64bccc25559c12280d160b6af75d11.

--- src/public/themes/blog/footer.php
+++ src/public/themes/blog/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
-  <div class="container">
-    <!--
+      <div class="container">
+        <!--
         <div class="row">
           <div class="col-md-4">
             <div class="logo">
@@ -53,35 +53,33 @@
         </div>
 -->
 
-  </div>
-  <div class="copyrights">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <p><?= copyright() . "\r"; ?><?= year_on_footer(date("Y")); ?>. All rights reserved. <?= app_info()['site_name']; ?> </p>
-        </div>
-        <div class="col-md-6 text-right">
-          <p>Template By <a href="https://bootstrapious.com" class="text-white">Ondrej Svetska</a>
-            <!-- Please do not remove the backlink to Bootstrap Temple unless you purchase an attribution-free license @ Bootstrap Temple or support us at http://bootstrapious.com/donate. It is part of the license conditions. Thanks for understanding :)                         -->
-          </p>
+      </div>
+      <div class="copyrights">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <p><?= copyright()."\r"; ?><?= year_on_footer(date("Y"));?>. All rights reserved. <?= app_info()['site_name'];?> </p>
+            </div>
+            <div class="col-md-6 text-right">
+              <p>Template By <a href="https://bootstrapious.com" class="text-white">Ondrej Svetska</a>
+                <!-- Please do not remove the backlink to Bootstrap Temple unless you purchase an attribution-free license @ Bootstrap Temple or support us at http://bootstrapious.com/donate. It is part of the license conditions. Thanks for understanding :)                         -->
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</footer>
-<!-- JavaScript files-->
-<script src="<?= theme_dir(); ?>assets/vendor/jquery/jquery.min.js"></script>
-<script src="<?= theme_dir(); ?>assets/vendor/popper.js/umd/popper.min.js"></script>
-<script src="<?= theme_dir(); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="<?= theme_dir(); ?>assets/vendor/jquery.cookie/jquery.cookie.js"></script>
-<script src="<?= theme_dir(); ?>assets/vendor/@fancyapps/fancybox/jquery.fancybox.min.js"></script>
-<script src="<?= theme_dir(); ?>assets/js/front.js"></script>
-<script src="<?= theme_dir(); ?>assets/js/multidropdown.js"></script>
-<script src="<?= theme_dir(); ?>assets/js/jquery.marquee.js"></script>
-<script src="<?= theme_dir(); ?>assets/js/jquery.pause.min.js"></script>
-<script src="<?= theme_dir(); ?>assets/js/jquery.easing.min.js"></script>
-<script src="<?= theme_dir(); ?>assets/js/comment.js"></script>
-<script type="text/javascript" src="<?= theme_dir(); ?>assets/js/validator.min.js"></script>
-
-</body>
+    </footer>
+    <!-- JavaScript files-->
+    <script src="<?= theme_dir(); ?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= theme_dir(); ?>assets/vendor/popper.js/umd/popper.min.js"></script>
+    <script src="<?= theme_dir(); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?= theme_dir(); ?>assets/vendor/jquery.cookie/jquery.cookie.js"></script>
+    <script src="<?= theme_dir(); ?>assets/vendor/@fancyapps/fancybox/jquery.fancybox.min.js"></script>
+    <script src="<?= theme_dir(); ?>assets/js/front.js"></script>
+    <script src="<?= theme_dir(); ?>assets/js/multidropdown.js"></script>
+    <script src="<?= theme_dir(); ?>assets/js/jquery.marquee.js"></script>
+    <script src="<?= theme_dir(); ?>assets/js/jquery.pause.min.js"></script>
+    <script src="<?= theme_dir(); ?>assets/js/jquery.easing.min.js"></script>
+    
+  </body>
 </html>
