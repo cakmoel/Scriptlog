bc5f1adf1ee1fce1f01f075a171b471d66abf8e0
cakmoel <alanmoehammad@gmail.com>
2022-10-15 13:00:47 +0700

    revamping src/lib/utility/escape-html

--- src/lib/utility/escape-html.php
+++ src/lib/utility/escape-html.php
@@ -5,8 +5,14 @@
  * Escape HTML, HTML Atrributes, JavaScript, CSS and Url.
  * This function for escaping data for output not be misused for filtering input data
  * 
+ * @category function
+ * @author M.Noermoehammad
+ * @license MIT
+ * @version 1.0
  * @param string $input The input string
+ * @param string $type
  * @param string $encoding Character encoding
+ * @param string $mode | mode = base or mode = various
  * @uses laminas-escaper
  * @see https://paragonie.com/blog/2015/06/preventing-xss-vulnerabilities-in-php-everything-you-need-know
  * @see https://docs.laminas.dev/laminas-escaper/
@@ -18,51 +24,75 @@
  * @return string
  * 
  */
-function escape_html($input, $type = 'html', $encoding = 'utf-8')
+function escape_html($input, $type = 'html', $encoding = 'utf-8', $mode = 'base')
 {
 
   $escaper = new Laminas\Escaper\Escaper($encoding);
 
-  switch ($type) {
+  switch ($mode) {
 
-    case 'html_attributes':
-      
-      $output = $escaper->escapeHtmlAttr($input);
-
-      return $output;
-
-      break;
-
-    case 'js':
-
-      $output = $escaper->escapeJs($input);
-
-      return $output;
-
-      break;
-
-    case 'css':
-
-      $output = $escaper->escapeCss($input);
-
-      return $output;
-
-      break;
-
-    case 'url':
-
-      $output = $escaper->escapeUrl($input);
-
-      return $output;
-
-      break;
-    
     default:
+    case 'base':
 
-      return $escaper->escapeHtml($input); 
+      return base_escape($input, $type, $escaper); 
 
-      break;
+    break;
+
+    case 'various':
+
+      return various_escape($input, $type, $escaper);
+
+    break;
 
   }
+
+}
+
+/**
+ * base_escape
+ *
+ * @param string $input
+ * @param [type] $type
+ * @param object new Laminas\Escaper|Escaper $escaper
+ * @return void
+ */
+function base_escape($input, $type, $escaper)
+{
+
+ if ($type === 'html_attributes') {
+
+  return (is_object($escaper) ? $escaper->escapeHtmlAttr($input) : "");
+
+ } elseif ($type === 'html') {
+
+  return (is_object($escaper) ? $escaper->escapeHtml($input) : "") ;
+
+ }
+
+}
+
+/**
+ * various_escape
+ *
+ * @param string $input
+ * @param string $type
+ * @param object $escaper
+ */
+function various_escape($input, $type, $escaper)
+{
+
+  if ($type === 'js') {
+    
+    return (is_object($escaper) ? $escaper->escapeJs($input) : "");
+
+  } elseif ($type === 'css') {
+
+    return (is_object($escaper) ? $escaper->escapeCss($input) : "");
   
+  } elseif ($type === 'url') {
+
+    return (is_object($escaper) ? $escaper->escapeUrl($input) : "");
+
+  } 
+
 }
