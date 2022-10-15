6d008b815696f3c85ec4a05bae446287fea1584e
cakmoel <alanmoehammad@gmail.com>
2022-10-15 14:21:40 +0700

    Updating src/lib/utitlity/form-action

--- src/lib/utility/form-action.php
+++ src/lib/utility/form-action.php
@@ -13,9 +13,9 @@ function form_action($base, array $data, $type = "ordinary")
 {
   $form = [];
 
-  $action = ( is_array($data) && array_key_exists(0, $data) ? rawurlencode($data[0]) : '' );
-  $id = ( is_array($data) && array_key_exists(1, $data) ? urlencode($data[1]) : null );
-  $uniqueKey =  ( is_array($data) && array_key_exists(2, $data) ? urlencode($data[2]) : null );
+  $action = (is_array($data) && array_key_exists(0, $data) ? rawurlencode($data[0]) : '');
+  $id = (is_array($data) && array_key_exists(1, $data) ? urlencode($data[1]) : null);
+  $uniqueKey =  (is_array($data) && array_key_exists(2, $data) ? urlencode($data[2]) : null);
 
   $query_data = array(
     
@@ -25,27 +25,16 @@ function form_action($base, array $data, $type = "ordinary")
     
     );
     
-   switch ($type) {
+if ($type === 'login') {
 
-    case 'login':
-        
-        $form['doLogin'] = build_query($base, $query_data);
-        
-        break;
-    
-    case 'comment':
+    $form['doLogin'] = build_query($base, $query_data);
 
-        $form['doComment'] = build_query($base, $query_data);
+} else {
 
-        break;
+    $form['ordinary'] = isset($_SERVER["PHP_SELF"]) ? purify_dirty_html($_SERVER["PHP_SELF"]) : "";
 
-    default:
-        
-        $form['ordinary'] = isset($_SERVER["PHP_SELF"]) ? $_SERVER["PHP_SELF"] : "";
+}
 
-        break;
-   }
-
-   return Sanitize::severeSanitizer($form);
+return Sanitize::severeSanitizer($form);
 
 }
