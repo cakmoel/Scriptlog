42552de8b8b4b33d78bea2d528c521ba58b32eec
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-21 21:26:54 +0700

    revamping admin/dashboard.php

--- src/admin/dashboard.php
+++ src/admin/dashboard.php
@@ -1,40 +1,36 @@
-<?php defined('SCRIPTLOG') || die("Direct access not permitted"); 
+<?php defined('SCRIPTLOG') || die("Direct access not permitted");
 
 $action = isset($_GET['action']) ? safe_html($_GET['action']) : "";
-$displayWall = new Wall();
+$displayWall = class_exists('Wall') ? new Wall() : "";
 
 try {
-   
-   switch ($action) {
-    
-      case 'detailItem':
-  
-         break;
-         
-      default:
-          
-         if (false === $authenticator->userAccessControl(ActionConst::DASHBOARD)) {
-  
-            direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-  
-         } else {
-  
-           $displayWall->listItems($authenticator, $user_login);
-  
-         }
-          
-        break;
-         
-  }
-     
-} catch (Throwable $th) {
-   
-   LogError::setStatusCode(http_response_code());
-   LogError::exceptionHandler($th);
 
+   switch ($action) {
+
+      case 'detailItem':
+
+         break;
+
+      default:
+
+         if (false === $authenticator->userAccessControl(ActionConst::DASHBOARD)) {
+
+            direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
+         } else {
+
+            $displayWall->listItems($authenticator, $user_login);
+         }
+
+         break;
+   }
+} catch (Throwable $th) {
+
+   if (class_exists('LogError')) {
+      LogError::setStatusCode(http_response_code());
+      LogError::exceptionHandler($th);
+   }
 } catch (AppException $e) {
 
    LogError::setStatusCode(http_response_code());
    LogError::exceptionHandler($e);
-
 }
