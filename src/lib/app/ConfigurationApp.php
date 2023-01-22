b6855c0bed3ca6f2519975e5a7a8e70a126ebb8d
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-22 13:43:12 +0700

    revamping lib/app/ConfigurationApp

--- src/lib/app/ConfigurationApp.php
+++ src/lib/app/ConfigurationApp.php
@@ -98,7 +98,7 @@ class ConfigurationApp
 
        ];
 
-      $size = ( !empty($_POST['setting_value']) ? count($_POST['setting_value']) : null );
+      $size = (!empty($_POST['setting_value']) ? count($_POST['setting_value']) : null);
        
       try {
         
@@ -109,7 +109,7 @@ class ConfigurationApp
 
         }
 
-        if(isset($_POST['setting_value']['1']) && strcmp($_POST['setting_value']['1'], app_key())) {
+        if (isset($_POST['setting_value']['1']) && strcmp($_POST['setting_value']['1'], app_key())) {
 
           $checkError = false;
           array_push($errors, "Application key does not match with the configuration file");
@@ -214,7 +214,7 @@ class ConfigurationApp
 
      ];
 
-     $size = ( !empty($_POST['setting_value']) ? count($_POST['setting_value']) : null );
+     $size = (!empty($_POST['setting_value']) ? count($_POST['setting_value']) : null);
      
     try {
        
@@ -232,14 +232,14 @@ class ConfigurationApp
           
       }
       
-      if ( isset($_POST['setting_value']['9']) && is_numeric($_POST['setting_value']['9']) === false) {
+      if (isset($_POST['setting_value']['9']) && is_numeric($_POST['setting_value']['9']) === false) {
 
         $checkError = false;
         array_push($errors, 'Invalid post per rss value');
          
       }
 
-      if ( !$checkError) {
+      if (!$checkError) {
 
          $this->setView('reading-setting');
          $this->setPageTitle('Reading Settings');
@@ -281,7 +281,7 @@ class ConfigurationApp
 
     } else {
 
-      if ( ( isset($_SESSION['status']) ) && ( $_SESSION['status'] == 'readingConfigUpdated') ) {
+      if ((isset($_SESSION['status']) ) && ($_SESSION['status'] == 'readingConfigUpdated')) {
 
         $checkStatus = true;
         array_push($status, "Reading setting has been updated");
@@ -330,7 +330,7 @@ class ConfigurationApp
     $checkError = true;
     $checkStatus = false;
 
-    if(!$getPermalinkValue = $this->configEvent->grabSettingByName('permalink_setting')) {
+    if (!$getPermalinkValue = $this->configEvent->grabSettingByName('permalink_setting')) {
 
       $_SESSION['error'] = "permalinkValueNotFound";
       direct_page('index.php?load=option-permalink&error=permalinkValueNotFound', 404);
@@ -350,7 +350,7 @@ class ConfigurationApp
                 'setting_name' => isset($_POST['setting_name']) ? Sanitize::severeSanitizer($_POST['setting_name']) : ""
               ];
     
-    if(isset($_POST['configFormSubmit'])) {
+    if (isset($_POST['configFormSubmit'])) {
 
       try {
         
@@ -361,14 +361,14 @@ class ConfigurationApp
         
         }
 
-        if( false === sanitize_selection_box(distill_post_request($filters)['permalinks'], ['yes', 'no']) ) {
+        if (false === sanitize_selection_box(distill_post_request($filters)['permalinks'], ['yes', 'no'])) {
 
           $checkError = false;
           array_push($errors, "Please choose the available value provided!");
 
         }
 
-        if(!$checkError) {
+        if (!$checkError) {
 
           $this->setView('permalink-setting');
           $this->setPageTitle('Permalink Setting');
@@ -386,19 +386,17 @@ class ConfigurationApp
           $this->configEvent->setConfigName(prevent_injection(distill_post_request($filters)['setting_name']));
 
           $permalink_value = array(
-
             'rewrite' => distill_post_request($filters)['permalinks'],
-            'server_software' => find_webserver_name(),
-      
+            'server_software' => find_webserver_name()
           );
 
           $updated_permalink_value = json_encode($permalink_value);
           
           $this->configEvent->setConfigValue($updated_permalink_value);
 
-          if ( ( $server_software['server_software'] == 'Apache' ) || ( $server_software['server_software'] == 'LiteSpeed' ) ) {
+          if (($server_software['server_software'] == 'Apache') || ($server_software['server_software'] == 'LiteSpeed')) {
 
-            write_htaccess( distill_post_request($filters)['permalinks'], Session::getInstance()->scriptlog_session_level, read_htaccess_config( distill_post_request($filters)['permalinks'] ) );
+            write_htaccess(distill_post_request($filters)['permalinks'], Session::getInstance()->scriptlog_session_level, read_htaccess_config(distill_post_request($filters)['permalinks']));
 
           }
           
@@ -422,7 +420,7 @@ class ConfigurationApp
       
     } else {
 
-      if(isset($_SESSION['error'])) {
+      if (isset($_SESSION['error'])) {
         $checkError = false;
         ($_SESSION['error'] == 'permalinkValueNotFound') ?: array_push($errors, "Error: Permalink value not found!");
         unset($_SESSION['error']);
@@ -457,6 +455,139 @@ class ConfigurationApp
 
   }
   
+  /**
+   * updateTimezoneConfig
+   *
+   */
+  public function updateTimezoneConfig()
+  {
+
+    $errors = array();
+    $status = array();
+    $checkError = true;
+    $checkStatus = false;
+
+    if (!$getTimezoneValue = $this->configEvent->grabSettingByName('timezone_setting')) {
+
+      $_SESSION['error'] = "timezoneValueNotFound";
+      direct_page('index.php?load=option-timezone&error=timezoneValueNotFound', 404);
+      
+    }
+
+    $data_timezone = array(
+      'ID' => $getTimezoneValue['ID'],
+      'setting_name' => $getTimezoneValue['setting_name'],
+      'setting_value' => $getTimezoneValue['setting_value']
+    );
+
+    $timezone_identifier = json_decode($data_timezone['setting_value'], true);
+
+    $filters = [
+      'timezone' => isset($_POST['timezone']) ? Sanitize::severeSanitizer($_POST['timezone']) : "",
+      'setting_id' => FILTER_SANITIZE_NUMBER_INT,
+      'setting_name' => isset($_POST['setting_name']) ? Sanitize::severeSanitizer($_POST['setting_name']) : ""
+    ];
+
+    if (isset($_POST['configFormSubmit'])) {
+
+      try {
+        
+        if (!csrf_check_token('csrfToken', $_POST, 60*10)) {
+
+          header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request", true, 400);
+          throw new AppException("Sorry, unpleasant attempt detected!");
+        
+        }
+
+        if (false === sanitize_selection_box(distill_post_request($filters)['timezone'], print_timezone_list())) {
+
+          $checkError = false;
+          array_push($errors, "Please choose the available value provided!");
+
+        }
+
+        if (!$checkError) {
+
+          $this->setView('timezone-setting');
+          $this->setPageTitle('Timezone Setting');
+          $this->setFormAction(ActionConst::TIMEZONE_CONFIG);
+          $this->view->set('pageTitle', $this->getPageTitle());
+          $this->view->set('timezoneData', $data_timezone);
+          $this->view->set('errors', $errors);
+          $this->view->set('formAction', $this->getFormAction());
+          $this->view->set('timezoneIdentifier', $this->configEvent->timezoneIdentifierDropDown($timezone_identifier['timezone_identifier']));
+          $this->view->set('csrfToken', csrf_generate_token('csrfToken'));
+
+        } else {
+
+          $this->configEvent->setConfigId((int)distill_post_request($filters)['setting_id']);
+
+          $this->configEvent->setConfigName(prevent_injection(distill_post_request($filters)['setting_name']));
+
+          $timezone_value = array(
+            'timezone_identifier' => distill_post_request($filters)['timezone']
+          );
+
+          $updated_timezone_value = json_encode($timezone_value);
+
+          $this->configEvent->setConfigValue($updated_timezone_value);
+
+          $this->configEvent->modifySetting();
+
+          $_SESSION['status'] = "timezoneConfigUpdated";
+          direct_page('index.php?load=option-timezone&status=timezoneConfigUpdated', 200);
+
+        }
+        
+      } catch (\Throwable $th) {
+        
+        LogError::setStatusCode(http_response_code());
+        LogError::exceptionHandler($th);
+
+      } catch (AppException $e) {
+        
+        LogError::setStatusCode(http_response_code());
+        LogError::exceptionHandler($e);
+      }
+
+    } else {
+
+      if (isset($_SESSION['error'])) {
+        $checkError = false;
+        ($_SESSION['error'] == 'timezoneValueNotFound') ?: array_push($errors, "Error: Timezone value not found!");
+        unset($_SESSION['error']);
+      }
+ 
+      if (isset($_SESSION['status'])) {
+        $checkStatus = true;
+        ($_SESSION['status'] == 'timezoneConfigUpdated') ?: array_push($status, "Timezone setting has been updated");
+        unset($_SESSION['status']);
+      }
+
+      $this->setView('timezone-setting');
+      $this->setPageTitle('Timezone Setting');
+      $this->setFormAction(ActionConst::TIMEZONE_CONFIG);
+
+      if (!$checkError) {
+       $this->view->set('errors', $errors);
+      }
+ 
+      if ($checkStatus) {
+       $this->view->set('status', $status);
+      }
+
+      $this->view->set('pageTitle', $this->getPageTitle());
+      $this->view->set('formAction', $this->getFormAction());
+      $this->view->set('timezoneData', $data_timezone);
+      $this->view->set('timezoneIdentifier', $this->configEvent->timezoneIdentifierDropDown($timezone_identifier['timezone_identifier']));
+      $this->view->set('csrfToken', csrf_generate_token('csrfToken'));
+
+    }
+
+    return $this->view->render();
+
+  }
+
   protected function setView($viewName)
   {
     $this->view = new View('admin', 'ui', 'setting', $viewName);
