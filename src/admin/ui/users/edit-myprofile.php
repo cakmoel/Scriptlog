3a8bb0e84afff3345fdf32b55dea02e404e7c027
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-22 13:42:11 +0700

    revamping src/admin/ui/users/edit-myprofile.php

--- src/admin/ui/users/edit-myprofile.php
+++ src/admin/ui/users/edit-myprofile.php
@@ -8,7 +8,7 @@
       <small>Control Panel</small>
     </h1>
     <ol class="breadcrumb">
-      <li><a href="index.php?load=dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li><a href="index.php?load=dashboard"><i class="fa fa-dashboard" aria-hidden="true"></i> Home</a></li>
       <li><a href="index.php?load=users">User</a></li>
       <li class="active">Profile</li>
     </ol>
