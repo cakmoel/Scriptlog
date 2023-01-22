cb07e44da3e7b6d9ab8bfa9179ca403edfccdafa
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-22 08:56:37 +0700

    revamping src/admin/topics.php

--- src/admin/topics.php
+++ src/admin/topics.php
@@ -2,128 +2,110 @@
 
 $action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action'])) : "";
 $topicId = isset($_GET['Id']) ? intval($_GET['Id']) : 0;
-$topicDao = new TopicDao();
-$topicEvent = new TopicEvent($topicDao, $validator, $sanitizer);
-$topicApp = new TopicApp($topicEvent);
+$topicDao = class_exists('TopicDao') ? new TopicDao() : "";
+$topicEvent = class_exists('TopicEvent') ? new TopicEvent($topicDao, $validator, $sanitizer) : "";
+$topicApp = class_exists('TopicApp') ? new TopicApp($topicEvent) : "";
 
 try {
 
     switch ($action) {
-    
+
         case ActionConst::NEWTOPIC:
-            
-            if( false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
-    
-                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-    
+
+            if (false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
+
+                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
             } else {
-    
-                if((!check_integer($topicId)) && (gettype($topicId) != "integer")) {
-    
-                    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request", true, 400);
+
+                if ((!check_integer($topicId)) && (gettype($topicId) != "integer")) {
+
+                    header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request", true, 400);
                     header("Status: 400 Bad Request");
                     throw new AppException("Invalid ID data type");
-    
                 }
-    
+
                 if ($topicId == 0) {
-    
+
                     $topicApp->insert();
-    
                 } else {
-    
+
                     direct_page('index.php?load=dashboard', 302);
-    
                 }
-    
             }
-            
+
             break;
-            
+
         case ActionConst::EDITTOPIC:
-            
-            if( false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
-    
-                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-    
+
+            if (false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
+
+                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
             } else {
-    
+
                 if ((!check_integer($topicId)) && (gettype($topicId) !== "integer")) {
-    
-                    header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request", true, 400);
+
+                    header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request", true, 400);
                     header("Status: 400 Bad Request");
                     throw new AppException("Invalid ID data type");
-    
                 }
-    
+
                 if ($topicDao->checkTopicId($topicId, $sanitizer)) {
-                
+
                     $topicApp->update((int)$topicId);
-                    
                 } else {
-        
-                    direct_page('index.php?load=404&notfound='.notfound_id(), 404);
-                    
+
+                    direct_page('index.php?load=404&notfound=' . notfound_id(), 404);
                 }
-                
             }
-            
+
             break;
-            
+
         case ActionConst::DELETETOPIC:
-    
+
             if ((!check_integer($topicId)) && (gettype($topicId) !== "integer")) {
-    
-                header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request", true, 400);
+
+                header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request", true, 400);
                 header("Status: 400 Bad Request");
                 throw new AppException("Invalid ID data type");
-    
             }
-            
+
             if (false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
-    
-              direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-    
+
+                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
             } else {
-    
-                if($topicDao->checkTopicId($topicId, $sanitizer)) {
-    
-                  $topicApp->remove((int)$topicId);
-    
+
+                if ($topicDao->checkTopicId($topicId, $sanitizer)) {
+
+                    $topicApp->remove((int)$topicId);
                 } else {
-    
-                  direct_page('index.php?load=404&notfound='.notfound_id(), 404);
-    
+
+                    direct_page('index.php?load=404&notfound=' . notfound_id(), 404);
                 }
-                 
             }
-            
+
             break;
-            
+
         default:
-            
-            if(false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
-    
-                direct_page('index.php?load=403&forbidden='.forbidden_id(), 403);
-    
+
+            if (false === $authenticator->userAccessControl(ActionConst::TOPICS)) {
+
+                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
             } else {
-    
+
                 $topicApp->listItems();
-    
             }
-        
+
             break;
-            
     }
+} catch (\Throwable $th) {
 
-} catch (Throwable $th) {
+    if (class_exists('LogError')) {
 
-    LogError::setStatusCode(http_response_code());
-    LogError::exceptionHandler($th);
-
+        LogError::setStatusCode(http_response_code());
+        LogError::exceptionHandler($th);
+    }
 } catch (AppException $e) {
 
     LogError::setStatusCode(http_response_code());
     LogError::exceptionHandler($e);
-    
 }
