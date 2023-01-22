e55ee5dee21cabca9eccea713b0b2a8332d6fbef
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-22 16:04:00 +0700

    append  lib/core/MedooInit.php

---
+++ src/lib/core/MedooInit.php
@@ -0,0 +1,23 @@
+<?php
+use Medoo\Medoo;
+
+/**
+ * Class MedooInit
+ * 
+ * @category Core Class
+ * @author M.Noermoehammad
+ * @license MIT
+ * 
+ */
+class MedooInit
+{
+
+protected static $database;
+
+public static function connect(array $options)
+{
+  self::$database = new Medoo($options);
+  return self::$database;
+}
+
+}
