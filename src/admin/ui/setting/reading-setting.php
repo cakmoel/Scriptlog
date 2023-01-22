f5ad0aab3e50ee4d6477621d8cd3fcaaa60b6657
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-22 13:39:03 +0700

    revamping src/admin/ui/setting/reading-setting.php

--- src/admin/ui/setting/reading-setting.php
+++ src/admin/ui/setting/reading-setting.php
@@ -1,4 +1,4 @@
-<?php ( !defined('SCRIPTLOG') ) ? exit() : ""; ?>
+<?php if (!defined('SCRIPTLOG')) { exit(); } ?>
  <!-- Content Wrapper. Contains page content -->
  <div class="content-wrapper">
  <!-- Content Header (Page header) -->
