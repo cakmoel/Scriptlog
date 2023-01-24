7dde6dadb9eb136465cb2e9ec01e3f9f7fcaf3f5
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-01-24 18:23:41 +0700

    revamping lib/utility/autolink.php

--- src/lib/utility/autolink.php
+++ src/lib/utility/autolink.php
@@ -10,7 +10,7 @@
  */
 function autolink($text) {
     $pattern = '/(((http[s]?:\/\/(.+(:.+)?@)?)|(www\.))[a-z0-9](([-a-z0-9]+\.)*\.[a-z]{2,})?\/?[a-z0-9.,_\/~#&=:;%+!?-]+)/is';
-    $text = preg_replace($pattern, ' <a href="$1">$1</a>', $text);
-    $text = preg_replace('/href="www/', 'href="https://www', $text);
-    return $text;
+    $replace = preg_replace($pattern, ' <a href="$1">$1</a>', $text);
+    return preg_replace('/href="www/', 'href="https://www', $replace);
+   
 }
