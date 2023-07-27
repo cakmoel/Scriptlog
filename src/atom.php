0f2789e685d0459a9186d5ee9e78e1f03a38d0ea
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-07-27 14:22:15 +0700

    revamping src/atom.php

--- src/atom.php
+++ src/atom.php
@@ -11,10 +11,10 @@
  */
 require __DIR__ . '/lib/main.php';
 
-$app_title = app_info()['site_name'];
-$app_link = app_info()['app_url'];
-$app_description = app_info()['site_description'];
-$feed_link = app_url() . DS . 'atom.php';
+$app_title = isset(app_info()['site_name']) ? app_info()['site_name'] : "";
+$app_link = isset(app_info()['app_url']) ? app_info()['app_url'] : "";
+$app_description = isset(app_info()['site_description']) ? app_info()['site_description'] : "";
+$feed_link = current_load_url() . 'atom.php';
 
 $atom_feed = new AtomWriter();
 
