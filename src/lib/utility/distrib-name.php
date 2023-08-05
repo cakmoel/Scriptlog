ae343aba5e7297404d4e7d336627efb59e1c191e
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2023-08-05 20:11:56 +0700

    append distrib-name.php

---
+++ src/lib/utility/distrib-name.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * distrib_name()
+ * 
+ * reading operating system distribution name for linux distro 
+ * within configuration file
+ * 
+ * @category function
+ * @author nirmalakhanza
+ * @license MIT
+ * @version 1.0
+ * @return mixed
+ * 
+ */
+function distrib_name()
+{
+  $config = class_exists('AppConfig') ? AppConfig::readConfiguration(invoke_config()) : "";
+
+  $distrib_name = isset($config['os']['distrib_name']) ? $config['os']['distrib_name'] : "";
+
+  return (!empty($distrib_name)) ? $distrib_name : "";
+}
