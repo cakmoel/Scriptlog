beb5e3dc5acf322b34d110d749651a62fccc0e0f
Nirmala Adiba Khanza <nirmala.adiba.khanza@gmail.com>
2023-09-26 12:56:38 +0700

    revamping lib/utility/app-tagline.php

--- src/lib/utility/app-tagline.php
+++ src/lib/utility/app-tagline.php
@@ -12,6 +12,6 @@
  */
 function app_tagline()
 {
- $tagline = medoo_get_where("tbl_settings", ['ID', 'setting_name', 'setting_value'], ['setting_name' => 'site_tagline']);
+ $tagline = function_exists('medoo_get_where') ? medoo_get_where("tbl_settings", ['ID', 'setting_name', 'setting_value'], ['setting_name' => 'site_tagline']) : "";
  return (is_array($tagline) && isset($tagline['setting_value'])) ? $tagline['setting_value'] : "";
 }
