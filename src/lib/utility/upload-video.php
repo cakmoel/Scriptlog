fcedfbbcedbff78ff9d8393b0dba86f230ea631e
Nirmala Adiba Khanza <nirmala.adiba.khanza@gmail.com>
2023-11-30 16:41:30 +0700

    src/lib/utility/upload-video.php
    revamping upload video

--- src/lib/utility/upload-video.php
+++ src/lib/utility/upload-video.php
@@ -12,7 +12,6 @@
  * @param string $file_size
  * @param string $file_type
  * @param string $file_name
- * @return bool
  * 
  */
 function upload_video($file_location, $file_name)
@@ -21,10 +20,6 @@ function upload_video($file_location, $file_name)
    $video_path = __DIR__ . '/../../'.APP_VIDEO;
    $video_uploaded = $video_path . $file_name;
    
-   if (!move_uploaded_file($file_location, $video_uploaded)) {
-
-      scriptlog_error("Video uploaded failure");
-
-   } 
-
+   return (!move_uploaded_file($file_location, $video_uploaded)) ? scriptlog_error($file_location, $video_uploaded) : true;
+   
 }
