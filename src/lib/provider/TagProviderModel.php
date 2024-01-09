3e6d51e71e732dfff3aded3eef0263f9c3d23454
Nirmala Adiba Khanza <nirmala.adiba.khanza@gmail.com>
2024-01-09 15:17:57 +0700

    revamping lib/provider/TagProviderModel

--- src/lib/provider/TagProviderModel.php
+++ src/lib/provider/TagProviderModel.php
@@ -11,6 +11,10 @@
 class TagProviderModel extends Dao
 {
 
+  private $linkPosts;
+
+  private $pagination;
+
 public function __construct()
 {
   parent::__construct();
@@ -44,12 +48,60 @@ $tag_exploaded = explode(',', strtolower($tags));
 
 foreach ((array) $tag_exploaded as $tag) {
 
-  $html[] = '<a href="'.permalinks($tag)['tag'].'" class="tag" title="'.escape_html($tag).'">#'.escape_html($tag).'</a>';
+  $html[] = '<a href="javascript:void(0)" class="tag" title="'.escape_html($tag).'">#'.escape_html($tag).'</a>';
 
 }
 
 return implode(" ", $html);
 
+}
+
+/**
+ * getPostsPublishedByTag
+ *
+ * @param string $tag
+ * @param object $sanitize
+ * @param Paginator $paginator
+ * @return mixed
+ */
+public function getPostsPublishedByTag($tag, $sanitize, Paginator $perPage)
+{
+  $entries = [];
+
+  $this->linkPosts = $perPage;
+
+  $count_tags = "SELECT tbl_posts.ID, tbl_posts.post_title, tbl_posts.post_content, tbl_posts.post_tags 
+                 FROM tbl_posts WHERE tbl_posts.post_tags LIKE :post_tags ORDER BY tbl_posts.ID DESC";
+
+  $this->setSQL($count_tags);
+
+  $this->linkPosts->set_total($this->checkCountValue([':post_tags' => $tag]));
+
+  $sql = "SELECT tbl_posts.ID, tbl_posts.media_id, tbl_posts.post_author, 
+                 tbl_posts.post_date AS created_at, 
+                 tbl_posts.post_modified AS modified_at,  
+                 tbl_posts.post_title, tbl_posts.post_slug, 
+                 tbl_posts.post_content, tbl_posts.post_summary, 
+                 tbl_posts.post_keyword, tbl_posts.post_tags, tbl_posts.post_status,
+                 tbl_posts.post_type, tbl_posts.comment_status, tbl_users.user_fullname,
+                 tbl_users.user_login, tbl_users.user_level,
+                 tbl_media.media_filename, tbl_media.media_caption
+          FROM tbl_posts, tbl_users, tbl_media
+          WHERE tbl_posts.post_tags LIKE :post_tags 
+          AND tbl_posts.post_author = tbl_users.ID
+          AND tbl_posts.post_status = 'publish' 
+          AND tbl_posts.post_type = 'blog'
+          AND tbl_users.user_banned = '0'
+          AND tbl_posts.media_id = tbl_media.ID
+          ORDER BY tbl_posts.post_date DESC " . $this->linkPosts->get_limit($sanitize);
+
+  $this->setSQL($sql);
+
+  $entries = $this->findAll([':post_tags' => $tag]);
+
+  $this->pagination = $this->linkPosts->page_links($sanitize);
+
+  return (empty($entries)) ?: ['postsByTag' => $entries, 'paginationLink' => $this->pagination];
 }
 
 }
