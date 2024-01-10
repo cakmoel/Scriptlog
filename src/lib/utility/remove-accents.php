a131e04bc061f58687d0064c9b4a3f0c571ad789
Nirmala Adiba Khanza <nirmala.adiba.khanza@gmail.com>
2024-01-10 09:13:56 +0700

    revamping lib/utility/remove-accents

--- src/lib/utility/remove-accents.php
+++ src/lib/utility/remove-accents.php
@@ -3,7 +3,8 @@
  * remove_accents
  * Converts all accent characters to their ASCII equivalents
  *
- * @category Function
+ * @category function
+ * @author Nirmalakhanza <nirmala.adiba.khanza@gmail.com>
  * @uses Util::remove_accents remove_accent
  * @param string $string
  * @return void
@@ -11,7 +12,15 @@
  */
 function remove_accents($string)
 {
- 
- return Util::remove_accents($string);
+    
+ if (class_exists('Transliterator')) {
 
+    $transliterator = Transliterator::createFromRules(':: Any-Latin; :: Latin-ASCII; :: NFD; :: [:Nonspacing Mark:] Remove; :: Lower(); :: NFC;', Transliterator::FORWARD);
+    
+    return $transliterator->transliterate($string);
+
+ } else {
+
+    return class_exists('Util') ? Util::remove_accents($string) : "";
+ }
 }
