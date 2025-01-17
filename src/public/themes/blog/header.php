23b56db4620339eab8ea5a707cd65bacdcb7326c
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2025-01-18 01:31:19 +0700

    updating public/themes/blog/header.php

--- src/public/themes/blog/header.php
+++ src/public/themes/blog/header.php
@@ -8,7 +8,7 @@ require dirname(__FILE__) . '/functions.php';
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <?= function_exists('theme_meta') ? theme_meta()['site_meta_tags'] . PHP_EOL : ""; ?>
-
+<meta property="og:type" content="website" />
 <?php 
  if (function_exists('theme_dir')) :
 ?>
