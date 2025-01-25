2e27796bba9d2309074bf5682ce01adf58dca62e
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2025-01-25 10:12:38 +0700

    append sitemap-generator.php

---
+++ src/lib/utility/sitemap-generator.php
@@ -0,0 +1,24 @@
+<?php 
+declare(strict_types=1);
+
+/**
+ *  fuction sitemap_generator
+ * 
+ * @category function
+ * @author nirmalakhanza nirmala.adiba.khanza@gmail.com 
+ * @version 1.0
+ * @return mixed|bool|array
+ */
+
+function sitemap_generator()
+{
+
+   $items = function_exists('medoo_column') ? medoo_column('tbl_posts', 'post_slug') : "";
+   $sitemap = class_exists('SitemapService') ? new SiteMapService() : "";
+   if (is_array($items)) {
+      $sitemapItems = $sitemap->generateSitemap($items);
+      return (!empty($sitemapItems)) ? true : false;
+   }
+   
+}
+
