e9192c5ebf2f04f131a859ccac214190c2a63f35
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2026-03-23 21:45:44 +0700

    feat: import exception

---
+++ src/lib/core/ImportException.php
@@ -0,0 +1,20 @@
+<?php defined('SCRIPTLOG') || die("Direct access not permitted");
+/**
+ * Class ImportException
+ * 
+ * Custom exception for import operations
+ * 
+ * @category  Core Class
+ * @author    M.Noermoehammad
+ * @license   MIT
+ * @version   1.0
+ * @since     Since Release 1.0
+ *
+ */
+class ImportException extends CoreException
+{
+  public function __construct($message = "", $code = 0, Throwable $previous = null)
+  {
+    parent::__construct($message, $code, $previous);
+  }
+}
