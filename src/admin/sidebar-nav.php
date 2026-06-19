6a0ea4260c0c5effaddce4dc489ad124a67be5b9
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2026-06-19 16:47:27 +0700

    feat: add i18n sidebar locale bootstrap, ARIA attributes, and language indicator widget

--- src/admin/sidebar-nav.php
+++ src/admin/sidebar-nav.php
@@ -1,8 +1,33 @@
 <?php
 function sidebar_navigation($module, $url, $user_id = null, $user_session = null)
 {
+    // Bootstrap admin locale from database default language setting (tbl_settings.lang_default)
+    // Uses static cache to query DB at most once per request for performance
+    static $localeBootstrapped = false;
+    if (!$localeBootstrapped) {
+        if (!isset($_SESSION['admin_locale']) && !isset($_COOKIE['admin_locale'])) {
+            try {
+                $configDao = new ConfigurationDao();
+                $defaultLang = $configDao->findConfigByName('lang_default', new Sanitize());
+                if (!empty($defaultLang['setting_value'])) {
+                    $langCode = $defaultLang['setting_value'];
+                    $availableLocales = ['en', 'ar', 'zh', 'fr', 'ru', 'es', 'id'];
+                    if (in_array($langCode, $availableLocales)) {
+                        admin_set_locale($langCode);
+                    }
+                }
+            } catch (Throwable $e) {
+                // Silently fall back to English
+            }
+        }
+        $localeBootstrapped = true;
+    }
+
+    $currentLocale = admin_get_locale();
+    $locales = admin_get_available_locales();
+    $currentLangName = $locales[$currentLocale] ?? 'English';
     ?>
-<aside class="main-sidebar">
+<aside class="main-sidebar" role="navigation" aria-label="<?= admin_translate('nav.navigation'); ?>">
     <section class="sidebar">
         <div class="user-panel"></div>
 
@@ -10,19 +35,20 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <li class="header"><?= admin_translate('nav.navigation'); ?></li>
 
             <li <?= ($module === 'dashboard') ? 'class="active"' : ''; ?>>
-                <a href="<?= $url . '/' . generate_request('index.php', 'get', ['dashboard'], false)['link']; ?>">
-                    <i class="fa fa-dashboard fa-fw"></i>
+                <a href="<?= $url . '/' . generate_request('index.php', 'get', ['dashboard'], false)['link']; ?>"
+                    <?= ($module === 'dashboard') ? 'aria-current="page"' : ''; ?>>
+                    <i class="fa fa-dashboard fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.dashboard'); ?></span>
                 </a>
             </li>
 
             <?php if (access_control_list(ActionConst::POSTS)) : ?>
             <li <?= ($module === 'posts' || $module === 'topics') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-thumb-tack fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'posts' || $module === 'topics') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-thumb-tack fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.posts'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -51,11 +77,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php if (access_control_list(ActionConst::MEDIALIB)) : ?>
             <li
                 <?= ($module === 'medialib' || $module === 'downloads') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-image fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'medialib' || $module === 'downloads') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-image fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.media'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -83,11 +109,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
 
             <?php if (access_control_list(ActionConst::PAGES)) : ?>
             <li <?= ($module === 'pages') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-file fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'pages') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-file fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.pages'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -110,8 +136,9 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php if (access_control_list(ActionConst::COMMENTS)) : ?>
             <li <?= ($module === 'comments') ? 'class="active"' : ''; ?>>
                 <a
-                    href="<?= $url . '/' . generate_request('index.php', 'get', ['comments', ActionConst::COMMENTS], false)['link']; ?>">
-                    <i class="fa fa-comments"></i>
+                    href="<?= $url . '/' . generate_request('index.php', 'get', ['comments', ActionConst::COMMENTS], false)['link']; ?>"
+                    <?= ($module === 'comments') ? 'aria-current="page"' : ''; ?>>
+                    <i class="fa fa-comments" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.comments'); ?></span>
                 </a>
             </li>
@@ -119,11 +146,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
 
             <?php if (access_control_list(ActionConst::IMPORT) || access_control_list(ActionConst::PRIVACY)) : ?>
             <li <?= ($module === 'import' || $module === 'export') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-wrench fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'import' || $module === 'export') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-wrench fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.tools'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -147,11 +174,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
 
             <?php if (access_control_list(ActionConst::USERS)) : ?>
             <li <?= ($module === 'users') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-user fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'users') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-user fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.users'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -171,8 +198,9 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php else : ?>
             <li <?= ($module === 'users') ? 'class="active"' : ''; ?>>
                 <a
-                    href="<?= generate_request('index.php', 'get', ['users', 'editUser', $user_id, $user_session])['link']; ?>">
-                    <i class="fa fa-user"></i>
+                    href="<?= generate_request('index.php', 'get', ['users', 'editUser', $user_id, $user_session])['link']; ?>"
+                    <?= ($module === 'users') ? 'aria-current="page"' : ''; ?>>
+                    <i class="fa fa-user" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.your_profile'); ?></span>
                 </a>
             </li>
@@ -181,11 +209,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php if (access_control_list(ActionConst::THEMES)) : ?>
             <li
                 <?= ($module === 'templates' || $module === 'menu') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-paint-brush fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'templates' || $module === 'menu') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-paint-brush fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.appearance'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -214,11 +242,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
                 || $module === 'option-api'
                 || $module === 'option-mail'
                 || $module === 'option-downloads') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-sliders fa-fw"></i>
+                <a href="#" aria-expanded="<?= (in_array($module, ['option-general', 'option-permalink', 'option-reading', 'option-timezone', 'option-memberships', 'option-api', 'option-mail', 'option-downloads'])) ? 'true' : 'false'; ?>">
+                    <i class="fa fa-sliders fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.settings'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -253,8 +281,9 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php if (access_control_list(ActionConst::PLUGINS)) : ?>
             <li <?= ($module === 'plugins') ? 'class="active"' : ''; ?>>
                 <a
-                    href="<?= $url . '/' . generate_request('index.php', 'get', ['plugins', ActionConst::PLUGINS], false)['link']; ?>">
-                    <i class="fa fa-plug fa-fw"></i>
+                    href="<?= $url . '/' . generate_request('index.php', 'get', ['plugins', ActionConst::PLUGINS], false)['link']; ?>"
+                    <?= ($module === 'plugins') ? 'aria-current="page"' : ''; ?>>
+                    <i class="fa fa-plug fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.plugins'); ?></span>
                 </a>
             </li>
@@ -263,11 +292,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php if (access_control_list(ActionConst::PRIVACY)) : ?>
             <li
                 <?= ($module === 'privacy' || $module === 'privacy-policy') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-shield fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'privacy' || $module === 'privacy-policy') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-shield fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.privacy'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -290,11 +319,11 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             <?php if (access_control_list(ActionConst::CONFIGURATION)) : ?>
             <li
                 <?= ($module === 'languages' || $module === 'translations' || $module === 'option-language') ? 'class="treeview active"' : 'class="treeview"'; ?>>
-                <a href="#">
-                    <i class="fa fa-globe fa-fw"></i>
+                <a href="#" aria-expanded="<?= ($module === 'languages' || $module === 'translations' || $module === 'option-language') ? 'true' : 'false'; ?>">
+                    <i class="fa fa-globe fa-fw" aria-hidden="true"></i>
                     <span><?= admin_translate('nav.languages'); ?></span>
                     <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
+                        <i class="fa fa-angle-left pull-right" aria-hidden="true"></i>
                     </span>
                 </a>
                 <ul class="treeview-menu">
@@ -320,7 +349,131 @@ function sidebar_navigation($module, $url, $user_id = null, $user_session = null
             </li>
             <?php endif; ?>
         </ul>
+
+        <div class="sidebar-lang-indicator" role="complementary" aria-label="<?= admin_translate('nav.language_settings'); ?>">
+            <a href="<?= $url . '/' . generate_request('index.php', 'get', ['option-language'], false)['link']; ?>"
+               class="sidebar-lang-link"
+               aria-label="<?= admin_translate('nav.language_settings') . ' - ' . $currentLangName; ?>"
+               title="<?= admin_translate('nav.language_settings'); ?>">
+                <span class="sidebar-lang-icon" aria-hidden="true">&#127758;</span>
+                <span class="sidebar-lang-code"><?= safe_html(strtoupper($currentLocale)); ?></span>
+                <span class="sidebar-lang-name"><?= safe_html($currentLangName); ?></span>
+            </a>
+        </div>
     </section>
 </aside>
+
+<style>
+.sidebar-lang-indicator {
+    border-top: 1px solid rgba(255,255,255,0.08);
+    padding: 0;
+    margin-top: 4px;
+    transition: border-color 0.2s ease;
+}
+
+.sidebar-lang-indicator:hover {
+    border-top-color: rgba(255,255,255,0.18);
+}
+
+.sidebar-lang-link {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    padding: 10px 15px 10px 18px;
+    color: rgba(255,255,255,0.55);
+    text-decoration: none;
+    font-size: 12px;
+    transition: all 0.2s ease;
+    border-radius: 0;
+    outline-offset: -2px;
+}
+
+.sidebar-lang-link:hover,
+.sidebar-lang-link:focus {
+    color: rgba(255,255,255,0.9);
+    background: rgba(255,255,255,0.05);
+}
+
+.sidebar-lang-link:focus-visible {
+    outline: 2px solid rgba(255,255,255,0.4);
+    outline-offset: -2px;
+}
+
+.sidebar-lang-icon {
+    font-size: 14px;
+    line-height: 1;
+    flex-shrink: 0;
+}
+
+.sidebar-lang-code {
+    font-weight: 600;
+    letter-spacing: 0.5px;
+    font-size: 11px;
+    text-transform: uppercase;
+    flex-shrink: 0;
+}
+
+.sidebar-lang-name {
+    font-weight: 400;
+    font-size: 12px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    opacity: 0.7;
+    transition: opacity 0.2s ease;
+}
+
+.sidebar-lang-link:hover .sidebar-lang-name {
+    opacity: 1;
+}
+
+/* When sidebar is collapsed (mini mode) - show only icon + code */
+.sidebar-mini.sidebar-collapse .sidebar-lang-name {
+    display: none;
+}
+
+.sidebar-mini.sidebar-collapse .sidebar-lang-link {
+    justify-content: center;
+    padding: 10px 5px;
+}
+
+.sidebar-mini.sidebar-collapse .sidebar-lang-code {
+    display: none;
+}
+
+.sidebar-mini.sidebar-collapse .sidebar-lang-indicator:hover .sidebar-lang-name {
+    display: inline;
+    position: absolute;
+    left: 50px;
+    background: rgba(0,0,0,0.9);
+    padding: 4px 8px;
+    border-radius: 3px;
+    white-space: nowrap;
+    z-index: 1000;
+    opacity: 1;
+}
+
+/* RTL support for language indicator */
+body:not(.sidebar-mini) .sidebar-lang-indicator,
+body:not(.sidebar-collapse) .sidebar-lang-indicator {
+    direction: inherit;
+}
+
+/* Reduced motion preference */
+@media (prefers-reduced-motion: reduce) {
+    .sidebar-lang-link,
+    .sidebar-lang-indicator,
+    .sidebar-lang-name {
+        transition: none;
+    }
+}
+
+/* Dark mode compatibility for sidebar */
+@media (prefers-color-scheme: dark) {
+    .sidebar-lang-indicator {
+        border-top-color: rgba(255,255,255,0.06);
+    }
+}
+</style>
 <?php
 }
