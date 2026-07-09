13f6af0411ec6248e9ce48518043b6e56658b6b6
nirmalakhanza <nirmala.adiba.khanza@gmail.com>
2026-07-09 19:17:33 +0700

    fix: validate filter IDs before passing to filter_input_array in distill_post_request

--- src/lib/utility/distill-post-request.php
+++ src/lib/utility/distill-post-request.php
@@ -17,7 +17,27 @@ function distill_post_request($refine)
 {
 
     if (is_array($refine)) {
-        return filter_input_array(INPUT_POST, $refine);
+
+        static $validFilterIds = null;
+
+        if ($validFilterIds === null) {
+            $validFilterIds = array_map(function ($name) {
+                return filter_id($name);
+            }, filter_list());
+            $validFilterIds = array_flip($validFilterIds);
+        }
+
+        $cleanFilters = [];
+        foreach ($refine as $key => $value) {
+            if (is_int($value) && isset($validFilterIds[$value])) {
+                $cleanFilters[$key] = $value;
+            } elseif (is_array($value)) {
+                $cleanFilters[$key] = $value;
+            } else {
+                $cleanFilters[$key] = FILTER_UNSAFE_RAW;
+            }
+        }
+        return filter_input_array(INPUT_POST, $cleanFilters);
     } else {
         scriptlog_error("can not retrieve external variables, please make sure it is an array!");
     }
