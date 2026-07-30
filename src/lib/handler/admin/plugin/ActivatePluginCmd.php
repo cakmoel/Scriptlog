f2809d86967454b411c26299a0596792e28b8637
cakmoel <alanmoehammad@gmail.com>
2026-07-30 19:33:46 +0700

    feat: add admin plugin activate plugin cmd

---
+++ src/lib/handler/admin/plugin/ActivatePluginCmd.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Scriptlog\Handler\Admin\Plugin;
+
+defined('SCRIPTLOG') || die("Direct access not permitted");
+
+use Scriptlog\Core\ActionConst;
+use Scriptlog\Handler\AdminActionCommand;
+
+/**
+ * Command for activating a plugin.
+ */
+class ActivatePluginCmd implements AdminActionCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(array $context): void
+    {
+        $app = $context['app'];
+        $pluginDao = $context['pluginDao'];
+        $pluginController = $context['pluginController'];
+        $pluginId = (int)$context['id'];
+
+        if (false === $app->authenticator->userAccessControl(ActionConst::PLUGINS)) {
+            direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
+        }
+
+        if ($pluginDao->checkPluginId($pluginId, $app->sanitizer)) {
+            $pluginController->enablePlugin($pluginId);
+        } else {
+            direct_page('index.php?load=404&notfound=' . notfound_id(), 404);
+        }
+    }
+}
