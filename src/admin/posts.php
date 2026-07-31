a6baa7f7a9866701c6f3b7a6f50ea1f9d1ec55d2
cakmoel <alanmoehammad@gmail.com>
2026-07-31 09:24:28 +0700

    refactor: migrate admin pages to command registry

--- src/admin/posts.php
+++ src/admin/posts.php
@@ -12,58 +12,21 @@ $postAppService = class_exists('PostApplicationService') ? new PostApplicationSe
 $postController = class_exists('PostController') ? new PostController($postService, $topicDao, $mediaDao, $postAppService) : "";
 
 try {
-    switch ($action) {
-        case ActionConst::NEWPOST:
-            if (false === $app->authenticator->userAccessControl(ActionConst::POSTS)) {
-                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
-            } else {
-                if ($postId == 0) {
-                    $postController->insert();
-                } else {
-                    direct_page('index.php?load=dashboard', 302);
-                }
-            }
+    $actionKey = empty($action) ? 'default_post' : $action;
 
-            break;
-
-        case ActionConst::EDITPOST:
-            if (false === $app->authenticator->userAccessControl(ActionConst::POSTS)) {
-                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
-            } else {
-                if ($postDao->checkPostId($postId, $app->sanitizer)) {
-                    $postController->update((int)$postId);
-                } else {
-                    direct_page('index.php?load=404&notfound=' . notfound_id(), 404);
-                }
-            }
-
-            break;
-
-        case ActionConst::DELETEPOST:
-            if ($postId <= 0) {
-                header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request", true, 400);
-                header("Status: 400 Bad Request");
-                throw new AppException("Invalid ID data type!");
-            }
-
-            if (false === $app->authenticator->userAccessControl(ActionConst::POSTS)) {
-                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
-            } else {
-                if ($postDao->checkPostId($postId, $app->sanitizer)) {
-                    $postController->remove((int)$postId);
-                } else {
-                    direct_page('index.php?load=404&notfound=' . notfound_id(), 404);
-                }
-            }
-
-            break;
-
-        default:
-            if (false === $app->authenticator->userAccessControl(ActionConst::POSTS)) {
-                direct_page('index.php?load=403&forbidden=' . forbidden_id(), 403);
-            } else {
-                $postController->listItems();
-            }
+    if ($app->adminActionRegistry && $app->adminActionRegistry->has($actionKey)) {
+        $app->adminActionRegistry->execute($actionKey, [
+            'app' => $app,
+            'id' => $postId,
+            'postDao' => $postDao,
+            'topicDao' => $topicDao,
+            'mediaDao' => $mediaDao,
+            'postService' => $postService,
+            'postAppService' => $postAppService,
+            'postController' => $postController,
+        ]);
+    } else {
+        direct_page('index.php?load=404&notfound=' . notfound_id(), 404);
     }
 } catch (\Throwable $th) {
     if (class_exists('LogError')) {
