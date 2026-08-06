b6194edcca76af9181563c6a5c53aa2f5ad237e8
cakmoel <alanmoehammad@gmail.com>
2026-08-06 07:11:23 +0700

    refactor: use view model and partial templates

--- src/public/themes/blog/page.php
+++ src/public/themes/blog/page.php
@@ -2,28 +2,14 @@
 
 $retrieve_page = function_exists('rewrite_status') && rewrite_status() == 'yes' ? retrieve_page(request_path()->param1, 'yes') : retrieve_page(HandleRequest::isQueryStringRequested()['value'], 'no');
 
-$page_img = isset($retrieve_page['media_filename']) ? htmlout($retrieve_page['media_filename']) : "";
-$page_id = isset($retrieve_page['ID']) ? (int)$retrieve_page['ID'] : "";
-$page_title = isset($retrieve_page['post_title']) ? htmlspecialchars($retrieve_page['post_title'], ENT_QUOTES, 'UTF-8', false) : "";
+$page = (function_exists('prepare_page') && !empty($retrieve_page)) ? prepare_page($retrieve_page) : null;
 
-if (isset($retrieve_page['user_fullname'])) {
-    $page_author = htmlout($retrieve_page['user_fullname']);
-}
-
-if (isset($retrieve_page['user_login'])) {
-    $page_author = htmlout($retrieve_page['user_login']);
-}
-
-$img_alt = isset($retrieve_page['media_caption']) ? htmlout($retrieve_page['media_caption']) : "";
-$page_content = isset($retrieve_page['post_content']) ? html_entity_decode(htmLawed(html($retrieve_page['post_content']))) : "";
-
-if (isset($retrieve_page['post_modified'])) {
-    $page_created = htmlout(make_date($retrieve_page['post_modified']));
-}
-
-if (isset($retrieve_page['post_date'])) {
-    $page_created = htmlout(make_date($retrieve_page['post_date']));
-}
+$page_id = ($page !== null && $page->id() !== null) ? (int)$page->id() : "";
+$page_title = ($page !== null && $page->title() !== null) ? $page->title() : "";
+$page_author = ($page !== null && $page->author() !== null) ? $page->author() : "";
+$page_created = ($page !== null && $page->date() !== null) ? $page->date() : "";
+$img_alt = ($page !== null && $page->mediaCaption() !== null) ? $page->mediaCaption() : "";
+$page_content = ($page !== null && $page->content() !== null) ? $page->content() : "";
 
 ?>
 
@@ -32,16 +18,16 @@ if (isset($retrieve_page['post_date'])) {
         <div class="post blog-post col-lg-8">
             <div class="container">
                 <div class="post-single">
-                    <div class="post-thumbnail"><?= isset($page_img) ? invoke_responsive_image($page_img, 'medium', true, isset($img_alt) ? $img_alt : "", 'img-fluid') : '<img src="' . theme_dir() . 'assets/img/placeholder.svg" alt="" width="730" height="486" class="img-fluid" loading="lazy" decoding="async">' ?></div>
+                    <div class="post-thumbnail"><?= ($page !== null && $page->media() !== null && $page->media() !== '') ? invoke_responsive_image($page->media(), 'medium', true, $img_alt, 'img-fluid') : '<img src="' . theme_dir() . 'assets/img/placeholder.svg" alt="" width="730" height="486" class="img-fluid" loading="lazy" decoding="async">' ?></div>
                     <div class="post-details">
                         <div class="post-meta d-flex justify-content-between">
                             <div class="category">
-                                <?= isset($page_id) ? link_topic((int)$page_id) : ""; ?>
+                                <?= ($page_id !== "") ? link_topic((int)$page_id) : ""; ?>
                             </div>
                         </div>
                         <h1>
-                            <?= isset($page_title) ? $page_title : ""; ?>
-                            <a href="<?= isset($page_id) ? permalinks($page_id) : "#"; ?>" title="<?= isset($page_title) ? $page_title : ""; ?>">
+                            <?= $page_title; ?>
+                            <a href="<?= ($page !== null) ? $page->url() : "#"; ?>" title="<?= $page_title; ?>">
                             <i class="fa fa-external-link" aria-hidden="true"></i></a>
                         </h1>
                         <div class="post-footer d-flex align-items-center flex-column flex-sm-row">
@@ -58,7 +44,7 @@ if (isset($retrieve_page['post_date'])) {
                         </div>
 
                         <div class="post-tags">
-                            <?= isset($page_id) ? link_tag($page_id) : ""; ?>
+                            <?= ($page_id !== "") ? link_tag($page_id) : ""; ?>
                         </div>
 
                     </div>
