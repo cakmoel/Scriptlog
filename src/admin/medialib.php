c5cb8768102a9a42f3c79f6b3d17717b9efa695d
cakmoel <alanmoehammad@gmail.com>
2026-08-06 07:11:21 +0700

    fix: harden escaping flags

--- src/admin/medialib.php
+++ src/admin/medialib.php
@@ -2,7 +2,7 @@
 
 defined('SCRIPTLOG') || die("Direct access not permitted");
 
-$action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action'])) : "";
+$action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action']), ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401) : "";
 $mediaId = isset($_GET['Id']) ? intval($_GET['Id']) : 0;
 $mediaDao = class_exists('MediaDao') ? new MediaDao() : "";
 $downloadModel = class_exists('DownloadModel') ? new DownloadModel() : "";
