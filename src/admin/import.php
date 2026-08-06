a12667d49418b56d3d2d891e6e2e64881125da8f
cakmoel <alanmoehammad@gmail.com>
2026-08-06 07:11:21 +0700

    fix: harden escaping flags

--- src/admin/import.php
+++ src/admin/import.php
@@ -2,7 +2,7 @@
 
 defined('SCRIPTLOG') || die("Direct access not permitted");
 
-$action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action'])) : "";
+$action = isset($_GET['action']) ? htmlentities(strip_tags($_GET['action']), ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401) : "";
 
 if (empty($action)) {
     if (isset($_POST['previewSubmit'])) {
